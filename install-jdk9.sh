1f6665f80e1a288c432ad6d90eb36b7eb921b6f5
Christian Stein <sormuras@gmail.com>
2017-06-17 10:26:39 +0200

    Export bash environment variables

--- install-jdk9.sh
+++ install-jdk9.sh
@@ -7,6 +7,6 @@ JDK9_ARCHIVE=jdk-9-ea+${JDK9_BUILD}_linux-x64_bin.tar.gz
 cd ~
 wget http://download.java.net/java/jdk9/archive/${JDK9_BUILD}/binaries/${JDK9_ARCHIVE}
 tar -xzf ${JDK9_ARCHIVE}
-JAVA_HOME=~/jdk-9
-PATH=${JAVA_HOME}/bin:$PATH
+export JAVA_HOME=~/jdk-9
+export PATH=${JAVA_HOME}/bin:$PATH
 cd -
