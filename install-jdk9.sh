3efb9b78107a9123ed4b5b80c03b5951d41b849e
Christian Stein <sormuras@gmail.com>
2017-06-17 10:18:04 +0200

    Made jdk-9 installation shell script executable

--- install-jdk9.sh
+++ install-jdk9.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 JDK9_BUILD=174
-JDK9_TARGZ=jdk-9-ea+${JDK9_BUILD}_linux-x64_bin.tar.gz
+JDK9_ARCHIVE=jdk-9-ea+${JDK9_BUILD}_linux-x64_bin.tar.gz
 
 cd ~
-wget http://download.java.net/java/jdk9/archive/${JDK9_BUILD}/binaries/${JDK9_TARGZ}
-tar -xzf ${JDK9_TARGZ}
+wget http://download.java.net/java/jdk9/archive/${JDK9_BUILD}/binaries/${JDK9_ARCHIVE}
+tar -xzf ${JDK9_ARCHIVE}
 JAVA_HOME=~/jdk-9
 PATH=${JAVA_HOME}/bin:$PATH
 cd -
