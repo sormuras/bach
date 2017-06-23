eb3088ddd3b02a96aef39c5ce7a854f7817b36eb
Christian Stein <sormuras@gmail.com>
2017-06-23 08:30:06 +0200

    Upgrade to jdk-9+175 (no more `early access`)

--- install-jdk9.sh
+++ install-jdk9.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-JDK9_BUILD=174
-JDK9_ARCHIVE=jdk-9-ea+${JDK9_BUILD}_linux-x64_bin.tar.gz
+JDK9_BUILD=175
+JDK9_ARCHIVE=jdk-9+${JDK9_BUILD}_linux-x64_bin.tar.gz
 
 cd ~
 wget http://download.java.net/java/jdk9/archive/${JDK9_BUILD}/binaries/${JDK9_ARCHIVE}
