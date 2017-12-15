fce41b1aef349618539269796ddb6253db70e349
Christian Stein <sormuras@gmail.com>
2017-12-15 12:10:27 +0100

    Support more edge-cases

--- demo/03-modules-first/src/main/java/de/sormuras/bach/modules/GenerateModuleProperties.java
+++ demo/03-modules-first/src/main/java/de/sormuras/bach/modules/GenerateModuleProperties.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URI;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -20,14 +19,20 @@ import java.util.TreeMap;
 import java.util.spi.ToolProvider;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("WeakerAccess")
 public class GenerateModuleProperties {
 
-  private final System.Logger log = System.getLogger(getClass().getName());
   private final URI base;
+  private final URI offset;
   private final Map<String, String> map;
 
-  GenerateModuleProperties(URI base, Map<String, String> map) {
+  GenerateModuleProperties(Map<String, String> map, URI offset) {
+    this(map, URI.create("http://central.maven.org/maven2/"), offset);
+  }
+
+  GenerateModuleProperties(Map<String, String> map, URI base, URI offset) {
     this.base = base;
+    this.offset = offset;
     this.map = map;
   }
 
@@ -36,78 +41,96 @@ public class GenerateModuleProperties {
   }
 
   private void scan(String fragment) {
-    var uri = base.resolve(fragment);
-    System.out.println(uri);
-    read(uri)
-        .ifPresent(
-            source -> {
-              if (source.contains("maven-metadata.xml") && !fragment.isEmpty()) {
-                scanMetadata(uri);
-                return;
-              }
-              scanDirectory(fragment, source);
-            });
+    var uri = base.resolve(offset).resolve(fragment);
+    System.out.println("\nscan(" + fragment + ") // " + uri);
+    var optionalSource = read(uri);
+    if (!optionalSource.isPresent()) {
+      return;
+    }
+    var source = optionalSource.get();
+    if (source.contains("maven-metadata.xml")) {
+      if (scanMetadata(uri)) {
+        return;
+      }
+    }
+    scanGroup(fragment, source);
   }
 
-  private void scanDirectory(String fragment, String source) {
+  private void scanGroup(String fragment, String source) {
     for (var line : source.split("\\R")) {
       if (line.startsWith("<a href=") && line.contains("title")) {
         var name = substring(line, ">", "</a>");
-        if (!name.endsWith("/")) {
-          name = name + "/";
+        if (name.endsWith("/")) {
+          scan(fragment + name);
         }
-        scan(fragment + name);
       }
     }
   }
 
-  private void scanMetadata(URI uri) {
+  private boolean scanMetadata(URI uri) {
     var meta = read(uri.resolve("maven-metadata.xml")).orElseThrow(Error::new);
-    if (meta.contains("<lastUpdated>2017")) { // TODO better date check "2017 or higher"
+    if (meta.contains("<versioning>")) {
+      var updated = substring(meta, "<lastUpdated>", "<");
+      if (updated.length() != 14) {
+        System.out.println("  [!] unexpected <lastUpdated> format: " + updated);
+        return true;
+      }
+      if (updated.compareTo("20170101000000") < 0) {
+        System.out.println("  [!] too old: " + updated);
+        return true;
+      }
       var group = substring(meta, "<groupId>", "<");
       var artifact = substring(meta, "<artifactId>", "<");
-      var version = substring(meta, "<latest>", "<");
-      var jar = ToolProvider.findFirst("jar").orElseThrow(Error::new);
-      var out = new StringWriter(8000);
+      var version = substring(meta, "<release>", "<"); // TODO Consider 'latest'?
       try {
-        var tmp = load(uri.resolve(version + "/" + artifact + "-" + version + ".jar").toURL());
-        jar.run(
-            new PrintWriter(out),
-            new PrintWriter(System.err),
-            "--describe-module",
-            "--file",
-            tmp.toString());
-        var lines = out.toString().split("\\R");
-        var empty = false;
-        for (var line : lines) {
-          if (empty) {
-            var module = line.trim().split("\\s+")[0];
-            if (module.contains("@")) {
-              module = line.substring(0, line.indexOf('@'));
-            }
-            var value = group + ":" + artifact;
-            var old = map.put(module, value);
-            System.out.println(module + " = " + value);
-            if (old == null) {
-              break;
-            }
-            if (old.equals(value)) {
-              log.log(System.Logger.Level.WARNING, "Already mapped {0} to {1}?!", module, value);
-            }
-            log.log(System.Logger.Level.WARNING, "Non-unique module name found: {0}", module);
-            break;
-          }
-          if (line.trim().isEmpty()) {
-            empty = true;
-          }
+        // TODO load .pom and assume "jar" packaging
+        var jar = load(uri.resolve(version + "/" + artifact + "-" + version + ".jar"));
+        if (Files.exists(jar) && Files.size(jar) > 0) {
+          scanJar(jar, group, artifact);
         }
-        Files.delete(tmp);
-      } catch (FileNotFoundException e) {
-        // ignore
+        Files.deleteIfExists(jar);
       } catch (Exception e) {
-        throw new Error("Scanning maven-metadata failed!", e);
+        throw new Error("Scanning maven-metadata.xml failed!", e);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  private void scanJar(Path path, String group, String artifact) {
+    var jar = ToolProvider.findFirst("jar").orElseThrow(Error::new);
+    var out = new StringWriter(8000);
+    jar.run(
+        new PrintWriter(out),
+        new PrintWriter(System.err),
+        "--describe-module",
+        "--file",
+        path.toString());
+    var lines = out.toString().split("\\R");
+    var empty = false;
+    for (var line : lines) {
+      if (empty) {
+        var module = line.trim().split("\\s+")[0];
+        if (module.contains("@")) {
+          module = line.substring(0, line.indexOf('@'));
+        }
+        var value = group + ":" + artifact;
+        var old = map.put(module, value);
+        System.out.println("\n\t" + module + " = " + value + "\n");
+        if (old == null) {
+          return;
+        }
+        if (old.equals(value)) {
+          System.err.println("  [!] already mapped " + module + " to " + value);
+        }
+        System.err.println("  [!] non-unique module name found: " + module);
+        return;
+      }
+      if (line.trim().isEmpty()) {
+        empty = true;
       }
     }
+    System.out.println("  [!] not a modular jar: " + path);
   }
 
   static Optional<String> read(URI uri) {
@@ -121,12 +144,16 @@ public class GenerateModuleProperties {
     }
   }
 
-  static Path load(URL url) throws Exception {
+  static Path load(URI uri) throws Exception {
+    var url = uri.toURL();
     var parts = url.getFile().split("/");
-    var temp = Files.createTempFile("000-", "-" + parts[parts.length - 1]);
+    var temp = Files.createTempFile("", "-" + parts[parts.length - 1]);
     try (var sourceStream = url.openStream();
         var targetStream = Files.newOutputStream(temp)) {
       sourceStream.transferTo(targetStream);
+    } catch (FileNotFoundException e) {
+      System.out.println("  [!] file not found: " + url);
+      Files.deleteIfExists(temp);
     }
     return temp;
   }
@@ -142,24 +169,21 @@ public class GenerateModuleProperties {
   }
 
   public static void main(String... args) throws IOException {
-    Map<String, String> map = new TreeMap<>();
-    // new
-    // GenerateModuleProperties(URI.create("http://central.maven.org/maven2/org/apache/commons/"),
-    // map).run();
-    // new GenerateModuleProperties(URI.create("http://central.maven.org/maven2/org/kordamp/"),
-    // map).run();
-    // new GenerateModuleProperties(URI.create("http://central.maven.org/maven2/org/junit/"),
-    // map).run();
-    // new
-    // GenerateModuleProperties(URI.create("http://central.maven.org/maven2/org/springframework/"),
-    // map).run();
-    new GenerateModuleProperties(URI.create("http://central.maven.org/maven2/org/joda/"), map)
-        .run();
+    var map = new TreeMap<String, String>();
+    // org/apache/commons/
+    // org/kordamp/
+    // org/junit/
+    // org/springframework/
+    new GenerateModuleProperties(map, URI.create("org/apache/commons/")).run();
     var lines =
         map.entrySet()
             .stream()
             .map(e -> e.getKey() + "=" + e.getValue())
             .collect(Collectors.toList());
-    Files.write(Paths.get("module-version-joda.properties"), lines, CREATE, TRUNCATE_EXISTING);
+    Files.write(
+        Paths.get("target/module-maven-apache-commons.properties"),
+        lines,
+        CREATE,
+        TRUNCATE_EXISTING);
   }
 }
