9d260609f01a93f7e312cc16af2d58dd7bd9f54a
Christian Stein <sormuras@gmail.com>
2018-01-17 21:14:05 +0100

    Support installation of Java 10 with GPL or BCL

--- install-jdk.sh
+++ install-jdk.sh
@@ -8,10 +8,22 @@ TMP="${TMP#*Most recent build: jdk-${JDK_FEATURE}-ea+}" # remove everything befo
 TMP="${TMP%%<*}"                                        # remove everything after the number
 JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')"     # remove all whitespace
 
+JDK_LICENSE=GPL
+
+while getopts F:B:L: option
+do
+ case "${option}"
+ in
+ F) JDK_FEATURE=${OPTARG};;
+ B) JDK_BUILD=${OPTARG};;
+ L) JDK_LICENSE=${OPTARG};;
+ esac
+done
+
 JDK_ARCHIVE=jdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
 
 cd ~
-wget http://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/BCL/${JDK_ARCHIVE}
+wget http://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/${JDK_LICENSE}/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
