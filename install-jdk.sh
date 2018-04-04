79426e72ff15acf1abc632bf73a3448f3c890890
Christian Stein <sormuras@gmail.com>
2018-04-04 14:18:03 +0200

    Add `dry-run` mode and allow `?` as feature number

--- install-jdk.sh
+++ install-jdk.sh
@@ -9,9 +9,10 @@
 #
 # Example usage
 #
-#   install-jdk.sh                 | install most recent (early-access) JDK
-#   install-jdk.sh -W /usr/opt     | install most recent (early-access) JDK to /usr/opt
-#   install-jdk.sh -C              | install most recent (early-access) JDK with linked system CA certificates
+#   install-jdk.sh -D              | only gather and print variable values
+#   install-jdk.sh                 | install most recent (early-access) OpenJDK
+#   install-jdk.sh -W /usr/opt     | install most recent (early-access) OpenJDK to /usr/opt
+#   install-jdk.sh -C              | install most recent (early-access) OpenJDK with linked system CA certificates
 #   install-jdk.sh -F 9            | install most recent OpenJDK 9
 #   install-jdk.sh -F 10           | install most recent OpenJDK 10
 #   install-jdk.sh -F 11           | install most recent OpenJDK 11
@@ -19,11 +20,12 @@
 #
 # Options
 #
-#   -F f | Feature number of the JDK release  [9|10|...]
-#   -B b | Build number of the JDK release    [?|1|2...]
+#   -D   | Dry-run, only gather and print variable values
+#   -F f | Feature number of the JDK release  [9|10|...|?]
+#   -B b | Build number of the JDK release    [1|2...|?]
 #   -L l | License of the JDK                 [GPL|BCL]
 #   -W w | Working directory and install path [${HOME}]
-#   -C c | Use system CA certificates (currently only Debian/Ubuntu is supported)
+#   -C   | Use system CA certificates (currently only Debian/Ubuntu is supported)
 #
 # Exported environment variables
 #
@@ -36,35 +38,52 @@
 #
 set -e
 
-VERSION=2018-04-03
+VERSION='2018-04-04'
+DRY_RUN='0'
+LINK_SYSTEM_CACERTS='0'
 
-JDK_FEATURE='11'
+JDK_FEATURE='?'
 JDK_BUILD='?'
 JDK_LICENSE='GPL'
 JDK_WORKSPACE=${HOME}
-JDK_SYSTEM_CACERTS='0'
+JDK_DOWNLOAD='https://download.java.net/java'
+JDK_BASENAME='openjdk'
 
-while getopts F:B:L:W:C option
-do
+echo "install-jdk.sh (${VERSION})"
+
+#
+# Parse command line options
+#
+while getopts ':F:B:L:W:CD' option; do
   case "${option}" in
+    D) DRY_RUN='1';;
+    C) LINK_SYSTEM_CACERTS='1';;
     F) JDK_FEATURE=${OPTARG};;
     B) JDK_BUILD=${OPTARG};;
     L) JDK_LICENSE=${OPTARG};;
     W) JDK_WORKSPACE=${OPTARG};;
-    C) JDK_SYSTEM_CACERTS="1";;
+    :) echo "Option -${OPTARG} requires an argument."; exit 1;;
+   \?) echo "Invalid option: -${OPTARG}"; exit 1;;
  esac
 done
 
-echo "install-jdk.sh (${VERSION})"
-echo "  FEATURE = ${JDK_FEATURE}"
-echo "  LICENSE = ${JDK_LICENSE}"
-echo
+#
+# Determine current early access release
+#
+if [ "${JDK_FEATURE}" == '?' ]; then 
+  COUNTER='11'
+  while [ "${COUNTER}" != '99' ]
+  do
+    CODE=$(curl -s -I "http://jdk.java.net/${COUNTER}" | head -n 1 | cut -d' ' -f2)
+    if [ "${CODE}" -ge '400' ]; then
+      break
+    fi
+    JDK_FEATURE=${COUNTER}
+    COUNTER=$[${COUNTER} +1]
+  done
+fi
 
-#
-# Other constants
-#
-JDK_DOWNLOAD='https://download.java.net/java'
-JDK_BASENAME='openjdk'
+
 if [ "${JDK_LICENSE}" != 'GPL' ]; then
   JDK_BASENAME='jdk'
 fi
@@ -72,10 +91,10 @@ fi
 #
 # 9 or 10
 #
-if [ "${JDK_FEATURE}" == '9' ] || [ "${JDK_FEATURE}" = '10' ]; then
+if [ "${JDK_FEATURE}" == '9' ] || [ "${JDK_FEATURE}" == '10' ]; then
   if [ "${JDK_LICENSE}" == 'GPL' ]; then
     if [ "${JDK_BUILD}" == '?' ]; then
-      TMP=$(curl -L jdk.java.net/${JDK_FEATURE})
+      TMP=$(curl -s -L jdk.java.net/${JDK_FEATURE})
       TMP="${TMP#*<h1>JDK}"                                   # remove everything before the number
       TMP="${TMP%%General-Availability Release*}"             # remove everything after the number
       JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')"     # remove all whitespace
@@ -101,15 +120,29 @@ fi
 #
 if [ "${JDK_FEATURE}" == '11' ]; then
   if [ "${JDK_BUILD}" == '?' ]; then
-    TMP=$(curl -L jdk.java.net/${JDK_FEATURE})
+    TMP=$(curl -s -L jdk.java.net/${JDK_FEATURE})
     TMP="${TMP#*Most recent build: jdk-${JDK_FEATURE}-ea+}" # remove everything before the number
     TMP="${TMP%%<*}"                                        # remove everything after the number
     JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')"     # remove all whitespace
   fi
-
   JDK_ARCHIVE=${JDK_BASENAME}-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
   JDK_URL=${JDK_DOWNLOAD}/early_access/jdk${JDK_FEATURE}/${JDK_BUILD}/${JDK_LICENSE}/${JDK_ARCHIVE}
   JDK_HOME=jdk-${JDK_FEATURE}
+fi
+
+#
+# Print variables and exit if dry-run is active
+#
+echo "  FEATURE = ${JDK_FEATURE}"
+echo "    BUILD = ${JDK_BUILD}"
+echo "  LICENSE = ${JDK_LICENSE}"
+echo
+echo "  ARCHIVE = ${JDK_ARCHIVE}"
+echo "      URL = ${JDK_URL}"
+echo
+
+if [ "${DRY_RUN}" == '1' ]; then
+  exit 0
 fi
 
 #
@@ -129,10 +162,11 @@ export JAVA_HOME=${JDK_WORKSPACE}/${JDK_HOME}
 export PATH=${JAVA_HOME}/bin:$PATH
 
 #
-# Link to system certificates to prevent:
-# SunCertPathBuilderException: unable to find valid certification path to requested target
+# Link to system certificates
+#  - http://openjdk.java.net/jeps/319
+#  - https://bugs.openjdk.java.net/browse/JDK-8196141
 #
-if [ "${JDK_SYSTEM_CACERTS}" == '1' ]; then
+if [ "${LINK_SYSTEM_CACERTS}" == '1' ]; then
   mv "${JAVA_HOME}/lib/security/cacerts" "${JAVA_HOME}/lib/security/cacerts.jdk"
   # TODO: Support for other distros than Debian/Ubuntu could be provided
   ln -s /etc/ssl/certs/java/cacerts "${JAVA_HOME}/lib/security/cacerts"
