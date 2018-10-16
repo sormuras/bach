4bccf63af5db152b07c431fa3846b17ac1ab78c3
Christian Stein <sormuras@gmail.com>
2018-10-16 19:41:54 +0200

    Upgrade to Java SE 11.0.1 LTS (Oracle)

--- install-jdk.sh
+++ install-jdk.sh
@@ -199,7 +199,7 @@ function determine_url() {
        10-GPL) url="${DOWNLOAD}/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_${os}_bin.tar.gz"; return;;
        10-BCL) url="${ORACLE}/10.0.2+13/19aef61b38124481863b1413dce1855f/jdk-10.0.2_${os}_bin.tar.gz"; return;;
        11-GPL) url="${DOWNLOAD}/GA/jdk11/28/GPL/openjdk-11+28_${os}_bin.tar.gz"; return;;
-       11-BCL) url="${ORACLE}/11+28/55eed80b163941c8885ad9298e6d786a/jdk-11_${os}_bin.tar.gz"; return;;
+       11-BCL) url="${ORACLE}/11.0.1+13/90cf5d8f270a4347a95050320eef3fb7/jdk-11.0.1_${os}_bin.tar.gz"; return;;
     esac
 
     # EA or RC build?
