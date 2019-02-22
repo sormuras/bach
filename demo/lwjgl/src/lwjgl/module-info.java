e3da774ed0402bc4300901d95491ba713f3aff3e
Christian Stein <sormuras@gmail.com>
2019-02-22 12:01:03 +0100

    Add missing natives

--- demo/lwjgl/src/lwjgl/module-info.java
+++ demo/lwjgl/src/lwjgl/module-info.java
@@ -2,5 +2,7 @@ module lwjgl {
   requires org.lwjgl;
   requires org.lwjgl.natives;
   requires org.lwjgl.glfw;
+  requires org.lwjgl.glfw.natives;
   requires org.lwjgl.opengl;
+  requires org.lwjgl.opengl.natives;
 }
