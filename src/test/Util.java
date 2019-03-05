a161a489a6fa4b7095654cce250a96365afef176
Christian Stein <sormuras@gmail.com>
2019-03-05 11:18:04 +0100

    Add test helper Util.java

---
+++ src/test/Util.java
@@ -0,0 +1,156 @@
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.AclEntry;
+import java.nio.file.attribute.AclEntryPermission;
+import java.nio.file.attribute.AclEntryType;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.condition.OS;
+
+interface Util {
+
+  /** Platform-agnostic {@code chmod} implementation. */
+  static void chmod(Path path, boolean r, boolean w, boolean x) throws Exception {
+    if (OS.WINDOWS.isCurrentOs()) {
+      var upls = path.getFileSystem().getUserPrincipalLookupService();
+      var user = upls.lookupPrincipalByName(System.getProperty("user.name"));
+      var builder = AclEntry.newBuilder();
+      var permissions =
+          EnumSet.of(
+              // AclEntryPermission.EXECUTE, // "x"
+              // AclEntryPermission.READ_DATA, // "r"
+              AclEntryPermission.READ_ATTRIBUTES,
+              AclEntryPermission.READ_NAMED_ATTRS,
+              // AclEntryPermission.WRITE_DATA, // "w"
+              // AclEntryPermission.APPEND_DATA, // "w"
+              AclEntryPermission.WRITE_ATTRIBUTES,
+              AclEntryPermission.WRITE_NAMED_ATTRS,
+              AclEntryPermission.DELETE_CHILD,
+              AclEntryPermission.DELETE,
+              AclEntryPermission.READ_ACL,
+              AclEntryPermission.WRITE_ACL,
+              AclEntryPermission.WRITE_OWNER,
+              AclEntryPermission.SYNCHRONIZE);
+      if (r) {
+        permissions.add(AclEntryPermission.READ_DATA); // == LIST_DIRECTORY
+      }
+      if (w) {
+        permissions.add(AclEntryPermission.WRITE_DATA); // == ADD_FILE
+        permissions.add(AclEntryPermission.APPEND_DATA); // == ADD_SUBDIRECTORY
+      }
+      if (x) {
+        permissions.add(AclEntryPermission.EXECUTE);
+      }
+      builder.setPermissions(permissions);
+      builder.setPrincipal(user);
+      builder.setType(AclEntryType.ALLOW);
+      var aclAttr = Files.getFileAttributeView(path, AclFileAttributeView.class);
+      aclAttr.setAcl(List.of(builder.build()));
+      return;
+    }
+    var user = (r ? "r" : "-") + (w ? "w" : "-") + (x ? "x" : "-");
+    Files.setPosixFilePermissions(path, PosixFilePermissions.fromString(user + "------"));
+  }
+
+  /** Create {@code count} regular files in the supplied directory. */
+  static List<Path> createFiles(Path directory, int count) throws Exception {
+    var paths = new ArrayList<Path>();
+    for (int i = 0; i < count; i++) {
+      paths.add(Files.createFile(directory.resolve("file-" + i)));
+    }
+    return paths;
+  }
+
+  /** Copy all files and directories from source to target directory. */
+  static void treeCopy(Path source, Path target) throws Exception {
+    treeCopy(source, target, __ -> true);
+  }
+
+  /** Copy selected files and directories from source to target directory. */
+  static void treeCopy(Path source, Path target, Predicate<Path> filter) throws Exception {
+    // debug("treeCopy(source:`%s`, target:`%s`)%n", source, target);
+    if (!Files.exists(source)) {
+      throw new IllegalArgumentException("source must exist: " + source);
+    }
+    if (!Files.isDirectory(source)) {
+      throw new IllegalArgumentException("source must be a directory: " + source);
+    }
+    if (Files.exists(target)) {
+      if (!Files.isDirectory(target)) {
+        throw new IllegalArgumentException("target must be a directory: " + target);
+      }
+      if (target.equals(source)) {
+        return;
+      }
+      if (target.startsWith(source)) {
+        // copy "a/" to "a/b/"...
+        throw new IllegalArgumentException("target must not a child of source");
+      }
+    }
+    try (var stream = Files.walk(source).sorted()) {
+      var paths = stream.collect(Collectors.toList());
+      for (var path : paths) {
+        var destination = target.resolve(source.relativize(path));
+        if (Files.isDirectory(path)) {
+          Files.createDirectories(destination);
+          continue;
+        }
+        if (filter.test(path)) {
+          Files.copy(path, destination, StandardCopyOption.REPLACE_EXISTING);
+        }
+      }
+    }
+  }
+
+  /** Delete all files and directories from the root directory. */
+  static void treeDelete(Path root) throws Exception {
+    treeDelete(root, __ -> true);
+  }
+
+  /** Delete selected files and directories from the root directory. */
+  static void treeDelete(Path root, Predicate<Path> filter) throws Exception {
+    // trivial case: delete existing empty directory or single file
+    try {
+      Files.deleteIfExists(root);
+      return;
+    } catch (DirectoryNotEmptyException ignored) {
+      // fall-through
+    }
+    // default case: walk the tree...
+    try (var stream = Files.walk(root)) {
+      var selected = stream.filter(filter).sorted((p, q) -> -p.compareTo(q));
+      for (var path : selected.collect(Collectors.toList())) {
+        Files.deleteIfExists(path);
+      }
+    }
+  }
+
+  /** Walk directory tree structure. */
+  static List<String> treeWalk(Path root) {
+    var lines = new ArrayList<String>();
+    treeWalk(root, lines::add);
+    return lines;
+  }
+
+  /** Walk directory tree structure. */
+  static void treeWalk(Path root, Consumer<String> out) {
+    try (var stream = Files.walk(root)) {
+      stream
+          .map(root::relativize)
+          .map(path -> path.toString().replace('\\', '/'))
+          .sorted()
+          .filter(Predicate.not(String::isEmpty))
+          .forEach(out);
+    } catch (Exception e) {
+      throw new Error("Walking tree failed: " + root, e);
+    }
+  }
+}
