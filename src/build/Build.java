ec5209d389ae7e7a3a897dd34afd43eb31c5abd4
Christian Stein <sormuras@gmail.com>
2019-03-10 11:57:19 +0100

    Implement test step

--- src/build/Build.java
+++ src/build/Build.java
@@ -15,8 +15,14 @@
  * limitations under the License.
  */
 
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /** OS-agnostic build program. */
 class Build {
@@ -38,6 +44,7 @@ class Build {
   private final Bach bach = new Bach(true, Path.of(""));
   private final Path target = Path.of("target", "build");
   private final Path targetBinMain = target.resolve("bin/main");
+  private final Path targetBinTest = target.resolve("bin/test");
   private final Path targetJavadoc = target.resolve("javadoc");
   private final Path targetJars = target.resolve("jars");
 
@@ -45,6 +52,7 @@ class Build {
     System.out.println("\n[clean]");
 
     Bach.Util.treeDelete(targetBinMain);
+    Bach.Util.treeDelete(targetBinTest);
     Bach.Util.treeDelete(targetJavadoc);
     Bach.Util.treeDelete(targetJars);
   }
@@ -56,14 +64,35 @@ class Build {
   private void compile() {
     System.out.println("\n[compile]");
     bach.run(0, "javac", "-d", targetBinMain, "src/bach/Bach.java");
+    Util.treeWalk(targetBinMain);
   }
 
-  private void test() {
-    System.out.println("\n[test - compile] // TODO");
-    // TODO bach.run(0, "javac", "-d", targetBinTest, "src/test/BachTests.java", ...);
+  private void test() throws Exception {
+    System.out.println("\n[test - download]");
+    var uri = URI.create(bach.get(Bach.Property.TOOL_URI_JUNIT));
+    var junit = Bach.Util.download(System.out::println, target, uri);
 
-    System.out.println("\n[test - run] // TODO");
-    // TODO Start JUnit Platform ConsoleLauncher
+    System.out.println("\n[test - compile]");
+    var javac = new ArrayList<>();
+    javac.add("-d");
+    javac.add(targetBinTest);
+    javac.add("--class-path");
+    javac.add(Bach.Util.join(targetBinMain, junit));
+    javac.addAll(Bach.Util.findJavaFiles(Path.of("src", "test")));
+    bach.run(0, "javac", javac.toArray(Object[]::new));
+    Util.treeCopy(Path.of("src/test-resources"), targetBinTest);
+    Util.treeWalk(targetBinTest);
+
+    System.out.println("\n[test - run]");
+    var launcher = new ArrayList<>();
+    launcher.add("-ea");
+    launcher.add("-Djunit.jupiter.execution.parallel.enabled=true");
+    launcher.add("-Djunit.jupiter.execution.parallel.mode.default=concurrent");
+    launcher.add("--class-path");
+    launcher.add(Bach.Util.join(targetBinTest, targetBinMain, junit));
+    launcher.add("org.junit.platform.console.ConsoleLauncher");
+    launcher.add("--scan-class-path");
+    bach.run(0, "java", launcher.toArray(Object[]::new));
   }
 
   private void document() throws Exception {
@@ -142,5 +171,70 @@ class Build {
         "-C",
         "demo/scaffold",
         ".");
+  }
+
+  static class Util {
+    /** Copy all files and directories from source to target directory. */
+    static void treeCopy(Path source, Path target) throws Exception {
+      treeCopy(source, target, __ -> true);
+    }
+
+    /** Copy selected files and directories from source to target directory. */
+    static void treeCopy(Path source, Path target, Predicate<Path> filter) throws Exception {
+      // debug("treeCopy(source:`%s`, target:`%s`)%n", source, target);
+      if (!Files.exists(source)) {
+        throw new IllegalArgumentException("source must exist: " + source);
+      }
+      if (!Files.isDirectory(source)) {
+        throw new IllegalArgumentException("source must be a directory: " + source);
+      }
+      if (Files.exists(target)) {
+        if (!Files.isDirectory(target)) {
+          throw new IllegalArgumentException("target must be a directory: " + target);
+        }
+        if (target.equals(source)) {
+          return;
+        }
+        if (target.startsWith(source)) {
+          // copy "a/" to "a/b/"...
+          throw new IllegalArgumentException("target must not a child of source");
+        }
+      }
+      try (var stream = Files.walk(source).sorted()) {
+        int counter = 0;
+        var paths = stream.collect(Collectors.toList());
+        for (var path : paths) {
+          var destination = target.resolve(source.relativize(path));
+          if (Files.isDirectory(path)) {
+            Files.createDirectories(destination);
+            continue;
+          }
+          if (filter.test(path)) {
+            Files.copy(path, destination, StandardCopyOption.REPLACE_EXISTING);
+            counter++;
+          }
+        }
+        System.out.printf("Copied %d file(s) of %d elements.%n", counter, paths.size());
+      }
+    }
+
+    /** Walk directory tree structure. */
+    static void treeWalk(Path root) {
+      treeWalk(root, System.out::println);
+    }
+
+    /** Walk directory tree structure. */
+   static void treeWalk(Path root, Consumer<String> out) {
+      try (var stream = Files.walk(root)) {
+        stream
+            .map(root::relativize)
+            .map(path -> path.toString().replace('\\', '/'))
+            .sorted()
+            .filter(Predicate.not(String::isEmpty))
+            .forEach(out);
+      } catch (Exception e) {
+        throw new Error("Walking tree failed: " + root, e);
+      }
+    }
   }
 }
