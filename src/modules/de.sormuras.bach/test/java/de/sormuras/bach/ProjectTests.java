91f8d3862e3d78528107ad904fb5d9dbdc6863d1
Christian Stein <sormuras@gmail.com>
2019-07-05 05:31:14 +0200

    Move help() test to a better place

--- src/modules/de.sormuras.bach/test/java/de/sormuras/bach/ProjectTests.java
+++ src/modules/de.sormuras.bach/test/java/de/sormuras/bach/ProjectTests.java
@@ -1,5 +1,7 @@
 package de.sormuras.bach;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -7,6 +9,7 @@ public class ProjectTests {
   public static void main(String[] args) {
     System.out.println(Project.class + " is in " + Project.class.getModule());
     checkProjectProperties();
+    checkHelp();
   }
 
   private static void checkProjectProperties() {
@@ -16,5 +19,11 @@ public class ProjectTests {
     assert Path.of("").equals(project.paths.home);
     assert Path.of("src/modules").equals(project.paths.sources);
     assert List.of("de.sormuras.bach", "integration").equals(project.options.modules);
+  }
+
+  private static void checkHelp() {
+    var string = new StringWriter();
+    Project.help(new PrintWriter(string, true));
+    assert string.toString().contains("1.0.0-SNAPSHOT");
   }
 }
