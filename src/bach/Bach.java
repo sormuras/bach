605f9e472223673e3a6e2f721f6a1a3674ca1319
Christian Stein <sormuras@gmail.com>
2019-07-08 20:34:17 +0200

    Add Modules helper class

--- src/bach/Bach.java
+++ src/bach/Bach.java
@@ -26,7 +26,10 @@ import static java.lang.System.Logger.Level.WARNING;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UncheckedIOException;
+import java.lang.module.ModuleDescriptor;
+import java.lang.module.ModuleDescriptor.Requires;
 import java.lang.module.ModuleDescriptor.Version;
+import java.lang.module.ModuleFinder;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLConnection;
@@ -46,8 +49,11 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.ServiceLoader;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
 import java.util.spi.ToolProvider;
 import java.util.stream.Collectors;
 
@@ -541,6 +547,75 @@ public class Bach {
 
     int run(Bach bach);
   }
+
+
+  /** Static helpers handling modules. */
+  static class Modules {
+
+    private static final Pattern MODULE_NAME_PATTERN = Pattern.compile("(?:module)\\s+(.+)\\s*\\{.*");
+    private static final Pattern MODULE_REQUIRES_PATTERN =
+        Pattern.compile(
+            "(?:requires)" // key word
+                + "(?:\\s+[\\w.]+)?" // optional modifiers
+                + "\\s+([\\w.]+)" // module name
+                + "(?:\\s*/\\*\\s*([\\w.\\-+]+)\\s*\\*/\\s*)?" // optional version
+                + ";.*"); // end marker
+
+    /** Enumerate all system module names. */
+    static List<String> findSystemModuleNames() {
+      return ModuleFinder.ofSystem().findAll().stream()
+          .map(reference -> reference.descriptor().name())
+          .sorted()
+          .collect(Collectors.toList());
+    }
+
+    /** Calculate external module names. */
+    static Set<String> findExternalModuleNames(Iterable<ModuleDescriptor> descriptors) {
+      var declaredModules = new TreeSet<String>();
+      var requiredModules = new TreeSet<String>();
+      for (var descriptor : descriptors) {
+        declaredModules.add(descriptor.name());
+        descriptor.requires().stream().map(Requires::name).forEach(requiredModules::add);
+      }
+      var externalModules = new TreeSet<>(requiredModules);
+      externalModules.removeAll(declaredModules);
+      externalModules.removeAll(findSystemModuleNames()); // "java.base", "java.logging", ...
+      return Set.copyOf(externalModules);
+    }
+
+    /** Simplistic module declaration parser. */
+    static ModuleDescriptor parseDeclaration(Path path) {
+      if (!Util.isModuleInfo(path)) {
+        throw new IllegalArgumentException("Expected module-info.java path, but got: " + path);
+      }
+      try {
+        return parseDeclaration(Files.readString(path));
+      } catch (IOException e) {
+        throw new UncheckedIOException("Reading module declaration failed: " + path, e);
+      }
+    }
+
+    /** Simplistic module declaration parser. */
+    static ModuleDescriptor parseDeclaration(String source) {
+      var nameMatcher = MODULE_NAME_PATTERN.matcher(source);
+      if (!nameMatcher.find()) {
+        throw new IllegalArgumentException("Expected Java module source unit, but got: " + source);
+      }
+      var name = nameMatcher.group(1).trim();
+      var builder = ModuleDescriptor.newModule(name);
+      var requiresMatcher = MODULE_REQUIRES_PATTERN.matcher(source);
+      while (requiresMatcher.find()) {
+        var requiredName = requiresMatcher.group(1);
+        Optional.ofNullable(requiresMatcher.group(2))
+            .ifPresentOrElse(
+                version ->
+                    builder.requires(Set.of(), requiredName, ModuleDescriptor.Version.parse(version)),
+                () -> builder.requires(requiredName));
+      }
+      return builder.build();
+    }
+  }
+
 
   /** Static helper. */
   static class Util {
---
+++ src/test/ModulesTests.java
@@ -0,0 +1,64 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.module.ModuleDescriptor;
+import java.lang.module.ModuleDescriptor.Requires;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+class ModulesTests {
+
+  @Test
+  void minimalisticModuleDeclaration() {
+    var descriptor = Bach.Modules.parseDeclaration("module a{}");
+    assertEquals("a", descriptor.name());
+    assertEquals("[mandated java.base]", descriptor.requires().toString());
+  }
+
+  @Test
+  void moduleDeclarationWithSingleReadEdge() {
+    var descriptor = Bach.Modules.parseDeclaration("module a{requires b;}");
+    var requires = computeRequiresMap(descriptor);
+    assertEquals("a", descriptor.name());
+    assertEquals(Set.of("b", "java.base"), requires.keySet());
+  }
+
+  @Test
+  void findSystemModuleNames() {
+    var names = Bach.Modules.findSystemModuleNames();
+    assertTrue(names.contains("java.base"));
+    assertTrue(names.contains("java.se"));
+    assertTrue(names.contains("java.xml"));
+  }
+
+  @Test
+  void moduleDeclarationWithRequiresAndVersion() {
+    var descriptor = Bach.Modules.parseDeclaration("module a{requires b/*1.2*/;}");
+    var requires = computeRequiresMap(descriptor);
+    assertEquals("a", descriptor.name());
+    assertEquals(Set.of("b", "java.base"), requires.keySet());
+    assertEquals("1.2", requires.get("b").compiledVersion().orElseThrow().toString());
+  }
+
+  @Test
+  void findExternalModuleNames() {
+    assert Bach.Modules.findExternalModuleNames(Set.of()).isEmpty();
+    var a = Bach.Modules.parseDeclaration("module a { requires java.scripting; }");
+    assertTrue(Bach.Modules.findExternalModuleNames(Set.of(a)).isEmpty());
+    var b = Bach.Modules.parseDeclaration("module b { requires a; }");
+    assertTrue(Bach.Modules.findExternalModuleNames(Set.of(a, b)).isEmpty());
+    assertEquals(Bach.Modules.findExternalModuleNames(Set.of(b)), Set.of("a"));
+    var c = Bach.Modules.parseDeclaration("module c { requires a; \n requires b; }");
+    assertTrue(Bach.Modules.findExternalModuleNames(Set.of(a, b, c)).isEmpty());
+  }
+
+  private static Map<String, Requires> computeRequiresMap(ModuleDescriptor descriptor) {
+    var requires = new HashMap<String, Requires>();
+    for (var dependence : descriptor.requires()) {
+      requires.put(dependence.name(), dependence);
+    }
+    return requires;
+  }
+}
