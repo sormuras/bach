9b7738a5fb865552585c33376a7c17e69356551a
Christian Stein <sormuras@gmail.com>
2019-07-16 14:50:47 +0200

    Launch JUnit Platform "manually" in integration module

--- demo/src/integration/test/java/integration/Main.java
+++ demo/src/integration/test/java/integration/Main.java
@@ -1,7 +1,42 @@
 package integration;
 
+import java.io.PrintWriter;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.core.LauncherConfig;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
 class Main {
   public static void main(String... args) {
     System.out.println(Main.class.getModule().getDescriptor());
+
+    var listener = new SummaryGeneratingListener();
+
+    var launcherConfig =
+        LauncherConfig.builder()
+            // .enableTestEngineAutoRegistration(false)
+            // .enableTestExecutionListenerAutoRegistration(false)
+            // .addTestEngines(new CustomTestEngine())
+            // .addTestExecutionListeners(new LegacyXmlReportGeneratingListener(reportsDir, out))
+            // .addTestExecutionListeners(new CustomTestExecutionListener())
+            .addTestExecutionListeners(listener)
+            .build();
+
+    var launcher = LauncherFactory.create(launcherConfig);
+
+    var request =
+        LauncherDiscoveryRequestBuilder.request()
+            .selectors(DiscoverySelectors.selectModule(Main.class.getModule().getName()))
+            // .filters(TagFilter.includeTags("fast"))
+            .build();
+
+    launcher.execute(request);
+
+    var summary = listener.getSummary();
+    if (summary.getTotalFailureCount() != 0) {
+      summary.printFailuresTo(new PrintWriter(System.err, true));
+      throw new AssertionError(summary.getTotalFailureCount() + " failure(s)...");
+    }
   }
 }
--- demo/src/integration/test/java/module-info.java
+++ demo/src/integration/test/java/module-info.java
@@ -3,4 +3,5 @@ open /*test*/ module integration {
   requires de.sormuras.bach.demo;
   requires de.sormuras.bach.demo.multi;
   requires org.junit.jupiter.api /*5.5.0*/;
+  requires org.junit.platform.launcher /*1.5.0*/;
 }
