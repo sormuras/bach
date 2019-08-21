fd50c1403795f30bc80ce5eab8e0f34349c578bd
Christian Stein <sormuras@gmail.com>
2019-08-21 09:26:12 +0200

    Enhance test module requirements

--- demo/src/integration/test/java/module-info.java
+++ demo/src/integration/test/java/module-info.java
@@ -1,7 +1,11 @@
 // --main-class integration.Main
 open /*test*/ module integration {
+  // modules under test
   requires de.sormuras.bach.demo;
   requires de.sormuras.bach.demo.multi;
+  // /*test*/ compile
   requires org.junit.jupiter.api /*5.5.1*/;
+  // /*test*/ runtime
   requires org.junit.platform.launcher /*1.5.1*/;
+  requires org.junit.jupiter.engine /*5.5.1*/;
 }
