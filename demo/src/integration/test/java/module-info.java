c948ca37d40a445c8cc6b448d4dcf57ab3b249a7
Christian Stein <sormuras@gmail.com>
2019-08-25 10:13:48 +0200

    Require Jupiter aggregator module

--- demo/src/integration/test/java/module-info.java
+++ demo/src/integration/test/java/module-info.java
@@ -3,9 +3,7 @@ open /*test*/ module integration {
   // modules under test
   requires de.sormuras.bach.demo;
   requires de.sormuras.bach.demo.multi;
-  // /*test*/ compile
-  requires org.junit.jupiter.api /*5.5.1*/;
-  // /*test*/ runtime
+  // modules used for testing
+  requires org.junit.jupiter /*5.5.1*/;
   requires org.junit.platform.launcher /*1.5.1*/;
-  requires org.junit.jupiter.engine /*5.5.1*/;
 }
