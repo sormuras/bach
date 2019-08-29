fd2f70bfe987cf3978e225ca26f991ca67d7bd99
Christian Stein <sormuras@gmail.com>
2019-08-29 05:20:24 +0200

    Extract getBanner() method

--- src/modules/de.sormuras.bach/main/java/de/sormuras/bach/Bach.java
+++ src/modules/de.sormuras.bach/main/java/de/sormuras/bach/Bach.java
@@ -1,22 +1,31 @@
 package de.sormuras.bach;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 import java.util.stream.Collectors;
 
 public class Bach {
 
   public static String VERSION = "2-ea";
 
-  public static void main(String... args) throws Exception {
-    try (var stream = Bach.class.getModule().getResourceAsStream("de/sormuras/bach/banner.txt")) {
-      if (stream != null) {
-        var lines = new BufferedReader(new InputStreamReader(stream)).lines();
-        var banner = lines.collect(Collectors.joining(System.lineSeparator()));
-        System.out.println(banner + " " + VERSION);
-      } else {
-        System.out.printf("Bach %s (member of %s)%n", VERSION, Bach.class.getModule());
+  public static void main(String... args) {
+    var bach = new Bach();
+    System.out.println(bach.getBanner());
+  }
+
+  String getBanner() {
+    var module = getClass().getModule();
+    try (var stream = module.getResourceAsStream("de/sormuras/bach/banner.txt")) {
+      if (stream == null) {
+        return String.format("Bach.java %s (member of %s)%n", VERSION, module);
       }
+      var lines = new BufferedReader(new InputStreamReader(stream)).lines();
+      var banner = lines.collect(Collectors.joining(System.lineSeparator()));
+      return banner + " " + VERSION;
+    } catch (IOException e) {
+      throw new UncheckedIOException("loading banner resource failed", e);
     }
   }
 }
