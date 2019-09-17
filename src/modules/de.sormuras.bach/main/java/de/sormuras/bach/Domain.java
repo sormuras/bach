3a06c4ee984b94c60b3559dab1678a094ecd7e04
Christian Stein <sormuras@gmail.com>
2019-09-17 19:34:03 +0200

    Make read-only properties public

--- src/modules/de.sormuras.bach/main/java/de/sormuras/bach/Domain.java
+++ src/modules/de.sormuras.bach/main/java/de/sormuras/bach/Domain.java
@@ -15,15 +15,15 @@ public interface Domain {
   /** Modular project model. */
   class Project {
     /** Name of the project. */
-    final String name;
+    public final String name;
     /** Version of the project. */
-    final Version version;
+    public final Version version;
     /** Library. */
-    final Library library;
+    public final Library library;
     /** List of realms. */
-    final List<Realm> realms;
+    public final List<Realm> realms;
     /** Target directory. */
-    final Path target;
+    public final Path target;
 
     public Project(String name, Version version, Library library, List<Realm> realms, Path target) {
       this.name = name;
@@ -37,9 +37,9 @@ public interface Domain {
   /** Manage external 3rd-party modules. */
   class Library {
     /** List of library paths to external 3rd-party modules. */
-    final List<Path> modulePaths;
+    public final List<Path> modulePaths;
     /** Map external 3rd-party module names to their {@code URI}s. */
-    final Function<String, URI> moduleMapper;
+    public final Function<String, URI> moduleMapper;
 
     public Library(List<Path> modulePaths, Function<String, URI> moduleMapper) {
       this.modulePaths = List.copyOf(modulePaths);
@@ -50,17 +50,17 @@ public interface Domain {
   /** Common base class for main- and test realms. */
   abstract class Realm {
     /** Name of the realm. */
-    final String name;
+    public final String name;
     /** Map of declared module source unit. */
-    final Map<String, ModuleSource> modules;
+    public final Map<String, ModuleSource> modules;
 
     /** Module source path specifies where to find input source files for multiple modules. */
     final String moduleSourcePath;
 
     Realm(String name, String moduleSourcePath, Map<String, ModuleSource> modules) {
       this.name = name;
-      this.modules = modules;
       this.moduleSourcePath = moduleSourcePath;
+      this.modules = Map.copyOf(modules);
     }
   }
 
@@ -74,7 +74,7 @@ public interface Domain {
   /** Test realm. */
   class TestRealm extends Realm {
     /** Main realm reference. */
-    final MainRealm main;
+    public final MainRealm main;
 
     public TestRealm(
         MainRealm main, String moduleSourcePath, Map<String, ModuleSource> declaredModules) {
@@ -86,12 +86,18 @@ public interface Domain {
   /** Describes a Java module source unit. */
   class ModuleSource {
     /** Relative to the backing {@code module-info.java} file. */
-    final Path info;
+    public final Path info;
+    /** Relative path to the resources directory. */
+    public final Path sources;
+    /** Relative path to the resources directory. */
+    public final Path resources;
     /** Associated module descriptor, normally parsed from module {@link #info} file. */
-    final ModuleDescriptor descriptor;
+    public final ModuleDescriptor descriptor;
 
-    public ModuleSource(Path info, ModuleDescriptor descriptor) {
+    public ModuleSource(Path info, Path sources, Path resources, ModuleDescriptor descriptor) {
       this.info = info;
+      this.sources = sources;
+      this.resources = resources;
       this.descriptor = descriptor;
     }
   }
