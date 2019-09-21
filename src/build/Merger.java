2340e86a125a2bd5a6570c5b70ccfe84cb5c47a6
Christian Stein <sormuras@gmail.com>
2019-09-21 06:18:11 +0200

    Include Jigsaw

--- src/build/Merger.java
+++ src/build/Merger.java
@@ -30,7 +30,7 @@ import java.util.spi.ToolProvider;
 class Merger {
   static Path SOURCE = Path.of("src/modules/de.sormuras.bach/main/java/de/sormuras/bach");
   static Path TARGET = Path.of("bin/merged");
-  static List<String> TYPES = List.of("Command", "Project", "Task", "Util");
+  static List<String> TYPES = List.of("Command", "Project", "Jigsaw", "Task", "Util");
 
   public static void main(String[] args) throws Exception {
     System.out.printf("Merging %d types into Bach.java {%n", TYPES.size());
