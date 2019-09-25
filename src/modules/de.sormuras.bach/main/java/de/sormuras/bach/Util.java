bcea56d427da6596bba2b27d7e37d31860d5b094
Christian Stein <sormuras@gmail.com>
2019-09-25 05:26:39 +0200

    Introduce download support

--- src/modules/de.sormuras.bach/main/java/de/sormuras/bach/Util.java
+++ src/modules/de.sormuras.bach/main/java/de/sormuras/bach/Util.java
@@ -18,16 +18,26 @@
 package de.sormuras.bach;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UncheckedIOException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
@@ -200,6 +210,99 @@ import java.util.stream.Stream;
       }
     } catch (IOException e) {
       throw new UncheckedIOException("tree delete failed: " + root, e);
+    }
+  }
+
+  /** File transfer utility. */
+  static class Downloader {
+
+    static class Item {
+
+      static Item of(URI uri, String file) {
+        return new Item(uri, file);
+      }
+
+      private final URI uri;
+      private final String file;
+
+      private Item(URI uri, String file) {
+        this.uri = uri;
+        this.file = file;
+      }
+    }
+
+    private final PrintWriter out, err;
+    private final HttpClient client;
+
+    Downloader(PrintWriter out, PrintWriter err) {
+      this(out, err, HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build());
+    }
+
+    private Downloader(PrintWriter out, PrintWriter err, HttpClient client) {
+      this.out = out;
+      this.err = err;
+      this.client = client;
+    }
+
+    Set<Path> download(Path directory, Collection<Item> items) {
+      Util.treeCreate(directory);
+      return items.stream()
+          .parallel()
+          .map(item -> download(item.uri, directory.resolve(item.file)))
+          .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    Path download(URI uri, Path path) {
+      if ("file".equals(uri.getScheme())) {
+        try {
+          return Files.copy(Path.of(uri), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+          throw new IllegalArgumentException("copy file failed:" + uri, e);
+        }
+      }
+      var request = HttpRequest.newBuilder(uri).GET();
+      if (Files.exists(path)) {
+        try {
+          var etagBytes = (byte[]) Files.getAttribute(path, "user:etag");
+          var etag = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(etagBytes)).toString();
+          request.setHeader("If-None-Match", etag);
+        } catch (Exception e) {
+          err.println("Couldn't get 'user:etag' file attribute: " + e);
+        }
+      }
+      try {
+        var handler = HttpResponse.BodyHandlers.ofFile(path);
+        var response = client.send(request.build(), handler);
+        if (response.statusCode() == 200) {
+          var etagHeader = response.headers().firstValue("etag");
+          if (etagHeader.isPresent()) {
+            try {
+              var etag = etagHeader.get();
+              Files.setAttribute(path, "user:etag", StandardCharsets.UTF_8.encode(etag));
+            } catch (Exception e) {
+              err.println("Couldn't set 'user:etag' file attribute: " + e);
+            }
+          }
+          var lastModifiedHeader = response.headers().firstValue("last-modified");
+          if (lastModifiedHeader.isPresent()) {
+            try {
+              var format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
+              var millis = format.parse(lastModifiedHeader.get()).getTime(); // 0 means "unknown"
+              var fileTime = FileTime.fromMillis(millis == 0 ? System.currentTimeMillis() : millis);
+              Files.setLastModifiedTime(path, fileTime);
+            } catch (Exception e) {
+              err.println("Couldn't set last modified file attribute: " + e);
+            }
+          }
+          synchronized (out) {
+            out.println(path + " <- " + uri);
+          }
+        }
+      } catch (IOException | InterruptedException e) {
+        err.println("Failed to load: " + uri + " -> " + e);
+        e.printStackTrace(err);
+      }
+      return path;
     }
   }
 }
--- src/modules/de.sormuras.bach/test/java/de/sormuras/bach/UtilTests.java
+++ src/modules/de.sormuras.bach/test/java/de/sormuras/bach/UtilTests.java
@@ -18,11 +18,16 @@
 package de.sormuras.bach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.api.io.TempDir;
 
 class UtilTests {
@@ -31,5 +36,39 @@ class UtilTests {
     assertEquals(List.of(), Util.list(temp));
     assertEquals(List.of(), Util.list(temp, Files::isRegularFile));
     assertEquals(List.of(), Util.list(temp, Files::isDirectory));
+  }
+
+  @Test
+  void downloadFilesUsingFileScheme(@TempDir Path temp, TestReporter reporter) throws Exception {
+    var a = Files.writeString(temp.resolve("a.txt"), "a");
+    var b = Files.writeString(temp.resolve("b.txt"), "b");
+    var c = Files.writeString(temp.resolve("c.txt"), "c");
+
+    var download = temp.resolve("download");
+    var a2 = download.resolve("a2.txt");
+    var b2 = download.resolve("b2.txt");
+    var c2 = download.resolve("c2.txt");
+
+    var items =
+        List.of(
+            Util.Downloader.Item.of(a.toUri(), a2.getFileName().toString()),
+            Util.Downloader.Item.of(b.toUri(), b2.getFileName().toString()),
+            Util.Downloader.Item.of(c.toUri(), c2.getFileName().toString()));
+
+    var out = new StringWriter();
+    var err = new StringWriter();
+    var transfer = new Util.Downloader(new PrintWriter(out), new PrintWriter(err));
+    var paths = transfer.download(download, items);
+    reporter.publishEntry("err", err.toString());
+
+    assertEquals(Set.of(a2, b2, c2), paths);
+    assertLinesMatch(Files.readAllLines(a), Files.readAllLines(a2));
+    assertLinesMatch(Files.readAllLines(b), Files.readAllLines(b2));
+    assertLinesMatch(Files.readAllLines(c), Files.readAllLines(c2));
+
+    var d = Files.writeString(temp.resolve("d.txt"), "d");
+    var d2 = download.resolve("d2.txt");
+    transfer.download(d.toUri(), d2);
+    assertLinesMatch(Files.readAllLines(d), Files.readAllLines(d2));
   }
 }
