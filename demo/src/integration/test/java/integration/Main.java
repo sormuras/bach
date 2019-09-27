ef64b46706fc9fddf2928619f4632cfa88e6541d
Christian Stein <sormuras@gmail.com>
2019-09-27 16:46:56 +0200

    Implement ToolProvider interface

--- demo/src/integration/test/java/integration/Main.java
+++ demo/src/integration/test/java/integration/Main.java
@@ -1,42 +1,58 @@
 package integration;
 
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectModule;
+
 import java.io.PrintWriter;
-import org.junit.platform.engine.discovery.DiscoverySelectors;
-import org.junit.platform.launcher.core.LauncherConfig;
+import java.util.List;
+import java.util.spi.ToolProvider;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 
-class Main {
+public class Main implements ToolProvider {
+
   public static void main(String... args) {
-    System.out.println(Main.class.getModule().getDescriptor());
-
-    var listener = new SummaryGeneratingListener();
-
-    var launcherConfig =
-        LauncherConfig.builder()
-            // .enableTestEngineAutoRegistration(false)
-            // .enableTestExecutionListenerAutoRegistration(false)
-            // .addTestEngines(new CustomTestEngine())
-            // .addTestExecutionListeners(new LegacyXmlReportGeneratingListener(reportsDir, out))
-            // .addTestExecutionListeners(new CustomTestExecutionListener())
-            .addTestExecutionListeners(listener)
-            .build();
-
-    var launcher = LauncherFactory.create(launcherConfig);
-
-    var request =
-        LauncherDiscoveryRequestBuilder.request()
-            .selectors(DiscoverySelectors.selectModule(Main.class.getModule().getName()))
-            // .filters(TagFilter.includeTags("fast"))
-            .build();
-
-    launcher.execute(request);
-
-    var summary = listener.getSummary();
-    if (summary.getTotalFailureCount() != 0) {
-      summary.printFailuresTo(new PrintWriter(System.err, true));
-      throw new AssertionError(summary.getTotalFailureCount() + " failure(s)...");
+    var out = new PrintWriter(System.out, true);
+    var err = new PrintWriter(System.err, true);
+    try {
+      var code = new Main().run(out, err, args);
+      if (code != 0) {
+        throw new AssertionError("run() return non-zero exit code: " + code);
+      }
+    } catch (Throwable t) {
+      throw new Error("run() failed", t);
     }
+  }
+
+  private final String moduleName = getClass().getModule().getName();
+
+  @Override
+  public String name() {
+    return moduleName;
+  }
+
+  @Override
+  public int run(PrintWriter out, PrintWriter err, String... args) {
+    var arguments = List.of(args);
+    out.printf("Running tests in: %s%n", arguments);
+    var launcher = LauncherFactory.create();
+    var request = LauncherDiscoveryRequestBuilder.request();
+    arguments.forEach(arg -> request.selectors(selectModule(arg)));
+    var summaryGeneratingListener = new SummaryGeneratingListener();
+
+    launcher.execute(request.build(), summaryGeneratingListener);
+
+    var summary = summaryGeneratingListener.getSummary();
+    if (summary.getTotalFailureCount() != 0) {
+      summary.printFailuresTo(err);
+      summary.printTo(err);
+      return 1;
+    }
+    if (summary.getTestsFoundCount() == 0) {
+      err.printf("No tests found in: %s%n", arguments);
+      return 2;
+    }
+    summary.printTo(out);
+    return 0;
   }
 }
--- demo/src/integration/test/java/module-info.java
+++ demo/src/integration/test/java/module-info.java
@@ -1,9 +1,14 @@
 // --main-class integration.Main
 open /*test*/ module integration {
+
   // modules under test
   requires de.sormuras.bach.demo;
   requires de.sormuras.bach.demo.multi;
+
   // modules used for testing
-  requires org.junit.jupiter /*5.5.1*/;
-  requires org.junit.platform.launcher /*1.5.1*/;
+  requires org.junit.jupiter;
+  requires org.junit.platform.launcher;
+
+  provides java.util.spi.ToolProvider with
+      integration.Main;
 }
