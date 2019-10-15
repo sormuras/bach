de6d93928dd244b948512c9f8c72204b7dfe1bec
Christian Stein <sormuras@gmail.com>
2019-10-15 17:38:08 +0200

    Introduce Maven

---
+++ src/modules/de.sormuras.bach/main/java/de/sormuras/bach/Maven.java
@@ -0,0 +1,75 @@
+package de.sormuras.bach;
+
+import java.net.URI;
+import java.util.Properties;
+
+/*BODY*/
+/** Maven 2 repository support. */
+public /*STATIC*/ class Maven {
+
+  private final Log log;
+  private final Resources resources;
+  private final Properties moduleMavenProperties;
+  private final Properties moduleVersionProperties;
+
+  public Maven(
+      Log log,
+      Resources resources,
+      Properties moduleMavenProperties,
+      Properties moduleVersionProperties) {
+    this.log = log;
+    this.resources = resources;
+    this.moduleMavenProperties = moduleMavenProperties;
+    this.moduleVersionProperties = moduleVersionProperties;
+  }
+
+  public String lookup(String module) {
+    return lookup(module, moduleVersionProperties.getProperty(module));
+  }
+
+  public String lookup(String module, String version) {
+    return moduleMavenProperties.getProperty(module) + ':' + version;
+  }
+
+  public URI toUri(String group, String artifact, String version) {
+    var repository =
+        version.endsWith("SNAPSHOT")
+            ? "https://oss.sonatype.org/content/repositories/snapshots"
+            : "https://repo1.maven.org/maven2";
+    return toUri(repository, group, artifact, version, "", "jar");
+  }
+
+  public URI toUri(
+      String repository,
+      String group,
+      String artifact,
+      String version,
+      String classifier,
+      String type) {
+    var versionAndClassifier = classifier.isBlank() ? version : version + '-' + classifier;
+    var file = artifact + '-' + versionAndClassifier + '.' + type;
+    if (version.endsWith("SNAPSHOT")) {
+      var base = String.join("/", repository, group.replace('.', '/'), artifact, version);
+      var xml = URI.create(base + "/maven-metadata.xml");
+      try {
+        var meta = resources.read(xml);
+        var timestamp = substring(meta, "<timestamp>", "<");
+        var buildNumber = substring(meta, "<buildNumber>", "<");
+        var replacement = timestamp + '-' + buildNumber;
+        log.debug("%s:%s:%s -> %s", group, artifact, version, replacement);
+        file = file.replace("SNAPSHOT", replacement);
+      } catch (Exception e) {
+        log.warn("Maven metadata extraction from %s failed: %s", xml, e);
+      }
+    }
+    var uri = String.join("/", repository, group.replace('.', '/'), artifact, version, file);
+    return URI.create(uri);
+  }
+
+  /** Extract substring between begin and end tags. */
+  static String substring(String string, String beginTag, String endTag) {
+    int beginIndex = string.indexOf(beginTag) + beginTag.length();
+    int endIndex = string.indexOf(endTag, beginIndex);
+    return string.substring(beginIndex, endIndex).trim();
+  }
+}
