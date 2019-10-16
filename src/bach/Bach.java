294ea2f2a1b5b5b1a217640706c27a07a4900476
GitHub Action <action@github.com>
2019-10-16 14:09:50 +0000

    Back to development of version 1.9-ea

--- src/bach/Bach.java
+++ src/bach/Bach.java
@@ -82,7 +82,7 @@ import javax.lang.model.SourceVersion;
 
 public class Bach {
 
-  public static String VERSION = "1.9.8";
+  public static String VERSION = "1.9-ea";
 
   /**
    * Main entry-point.
