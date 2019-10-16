608d41b9fac427a93d6a93193c550c6fdd3d7b82
Christian Stein <sormuras@gmail.com>
2019-10-16 14:53:35 +0200

    Delete superseded Util.Downloader class

--- src/modules/it/test/java/it/ResourcesTests.java
+++ src/modules/it/test/java/it/ResourcesTests.java
@@ -31,4 +31,43 @@ class ResourcesTests {
   void headRequestOfHttpsGoogleComUsingSystemResourcesIsRespondedWithStatus200() throws Exception {
     assertEquals(200, Resources.ofSystem().head(URI.create("https://google.com")).statusCode());
   }
+  /*
+
+  @Test
+  void downloadFilesUsingFileScheme(@TempDir Path temp, TestReporter reporter) throws Exception {
+    var a = Files.writeString(temp.resolve("a.txt"), "a");
+    var b = Files.writeString(temp.resolve("b.txt"), "b");
+    var c = Files.writeString(temp.resolve("c.txt"), "c");
+
+    var download = temp.resolve("download");
+    var a2 = download.resolve("a2.txt");
+    var b2 = download.resolve("b2.txt");
+    var c2 = download.resolve("c2.txt");
+
+    var items =
+        List.of(
+            Util.Downloader.Item.of(a.toUri(), a2.getFileName().toString()),
+            Util.Downloader.Item.of(b.toUri(), b2.getFileName().toString()),
+            Util.Downloader.Item.of(c.toUri(), c2.getFileName().toString()));
+
+    var out = new StringWriter();
+    var err = new StringWriter();
+    var transfer = new Util.Downloader(new PrintWriter(out), new PrintWriter(err));
+    var paths = transfer.download(download, items);
+    var errors = err.toString();
+    if (!errors.isBlank()) {
+      reporter.publishEntry("errors", errors);
+    }
+
+    assertEquals(Set.of(a2, b2, c2), paths);
+    assertLinesMatch(Files.readAllLines(a), Files.readAllLines(a2));
+    assertLinesMatch(Files.readAllLines(b), Files.readAllLines(b2));
+    assertLinesMatch(Files.readAllLines(c), Files.readAllLines(c2));
+
+    var d = Files.writeString(temp.resolve("d.txt"), "d");
+    var d2 = download.resolve("d2.txt");
+    transfer.download(d.toUri(), d2);
+    assertLinesMatch(Files.readAllLines(d), Files.readAllLines(d2));
+  }
+   */
 }
