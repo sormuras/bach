3eb23c7a7a6048930ead75a8b49bab915f2cc4b2
GitHub Action <action@github.com>
2019-10-17 19:35:43 +0000

    Release 1.9.9

--- src/bach/Bach.java
+++ src/bach/Bach.java
@@ -82,7 +82,7 @@ import javax.lang.model.SourceVersion;
 
 public class Bach {
 
-  public static String VERSION = "1.9-ea";
+  public static String VERSION = "1.9.9";
 
   /**
    * Main entry-point.
