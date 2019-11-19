cb23d0eb0669e611e2b8df0ae61a1c64f035689b
Christian Stein <sormuras@gmail.com>
2019-11-19 16:54:26 +0100

    Enhance readability even more

--- src/de.sormuras.bach/main/java/de/sormuras/bach/task/Resolver.java
+++ src/de.sormuras.bach/main/java/de/sormuras/bach/task/Resolver.java
@@ -59,40 +59,17 @@ class Resolver {
     var systemModulesSurvey = Modules.Survey.of(ModuleFinder.ofSystem());
     var missing = findMissingModules(systemModulesSurvey);
     if (missing.isEmpty()) {
+      log.debug("All required modules are locatable.");
       return;
     }
 
-    var http = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
-    var uris = new Uris(log, http);
-    var maven = createMaven(uris);
-
-    var moduleUriProperties = load(new Properties(), lib.resolve("module-uri.properties"));
+    var downloader = new Downloader();
     do {
-      log.debug("Loading missing modules: %s", missing);
-      for (var entry : missing.entrySet()) {
-        var module = entry.getKey();
-        var direct = moduleUriProperties.getProperty(module);
-        if (direct != null) {
-          var uri = URI.create(direct);
-          var jar = lib.resolve(module + ".jar");
-          uris.copy(uri, jar, StandardCopyOption.COPY_ATTRIBUTES);
-          continue;
-        }
-        var versions = entry.getValue();
-        var version = singleton(versions).map(Object::toString).orElse(maven.version(module));
-        var ga = maven.lookup(module, version).split(":");
-        var group = ga[0];
-        var artifact = ga[1];
-        var repository = library.mavenRepositoryMapper().apply(group, version);
-        uris.copy(
-            maven.toUri(repository, group, artifact, version),
-            lib.resolve(module + '-' + version + ".jar"),
-            StandardCopyOption.COPY_ATTRIBUTES);
-      }
+      downloader.loadMissingModules(missing);
       missing.clear();
-      var library2 = Modules.Survey.of(ModuleFinder.of(lib));
-      library2.putAllRequiresTo(missing);
-      library2.declaredModules().forEach(missing::remove);
+      var libraryModulesSurvey = Modules.Survey.of(ModuleFinder.of(lib));
+      libraryModulesSurvey.putAllRequiresTo(missing);
+      libraryModulesSurvey.declaredModules().forEach(missing::remove);
       systemModulesSurvey.declaredModules().forEach(missing::remove);
     } while (!missing.isEmpty());
   }
@@ -121,62 +98,99 @@ class Resolver {
     return missing;
   }
 
-  Maven createMaven(Uris uris) throws Exception {
-    var cache =
-        Files.createDirectories(Path.of(System.getProperty("user.home")).resolve(".bach/modules"));
-    var artifactPath =
+  private class Downloader {
+    HttpClient http = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
+    Uris uris = new Uris(log, http);
+    Maven maven = createMaven(uris);
+    Properties moduleUriProperties = load(new Properties(), lib.resolve("module-uri.properties"));
+
+    Downloader() throws Exception {}
+
+    void loadMissingModules(Map<String, Set<Version>> missing) throws Exception {
+      log.debug("Loading modules: %s", missing);
+      for (var entry : missing.entrySet()) {
+        var module = entry.getKey();
+        var direct = moduleUriProperties.getProperty(module);
+        if (direct != null) {
+          var uri = URI.create(direct);
+          var jar = lib.resolve(module + ".jar");
+          uris.copy(uri, jar, StandardCopyOption.COPY_ATTRIBUTES);
+          continue;
+        }
+        var versions = entry.getValue();
+        var version = singleton(versions).map(Object::toString).orElse(maven.version(module));
+        var ga = maven.lookup(module, version).split(":");
+        var group = ga[0];
+        var artifact = ga[1];
+        var repository = library.mavenRepositoryMapper().apply(group, version);
         uris.copy(
-            URI.create("https://github.com/sormuras/modules/raw/master/module-maven.properties"),
-            cache.resolve("module-maven.properties"),
+            maven.toUri(repository, group, artifact, version),
+            lib.resolve(module + '-' + version + ".jar"),
             StandardCopyOption.COPY_ATTRIBUTES);
-
-    var artifactLookup =
-        new Maven.Lookup(
-            library.mavenGroupColonArtifactMapper(),
-            map(load(new Properties(), lib.resolve("module-maven.properties"))),
-            map(load(new Properties(), artifactPath)));
-
-    var versionPath =
-        uris.copy(
-            URI.create("https://github.com/sormuras/modules/raw/master/module-version.properties"),
-            cache.resolve("module-version.properties"),
-            StandardCopyOption.COPY_ATTRIBUTES);
-
-    var versionLookup =
-        new Maven.Lookup(
-            library.mavenVersionMapper(),
-            map(load(new Properties(), lib.resolve("module-version.properties"))),
-            map(load(new Properties(), versionPath)));
-    return new Maven(log, uris, artifactLookup, versionLookup);
-  }
-
-  static Properties load(Properties properties, Path path) {
-    if (Files.isRegularFile(path)) {
-      try (var reader = Files.newBufferedReader(path)) {
-        properties.load(reader);
-      } catch (Exception e) {
-        throw new RuntimeException("Reading properties failed: " + path, e);
       }
     }
-    return properties;
-  }
 
-  /** Convert all {@link String}-based properties in an instance of {@code Map<String, String>}. */
-  static Map<String, String> map(Properties properties) {
-    var map = new HashMap<String, String>();
-    for (var name : properties.stringPropertyNames()) {
-      map.put(name, properties.getProperty(name));
-    }
-    return Map.copyOf(map);
-  }
+    Maven createMaven(Uris uris) throws Exception {
+      var cache =
+          Files.createDirectories(
+              Path.of(System.getProperty("user.home")).resolve(".bach/modules"));
+      var artifactPath =
+          uris.copy(
+              URI.create("https://github.com/sormuras/modules/raw/master/module-maven.properties"),
+              cache.resolve("module-maven.properties"),
+              StandardCopyOption.COPY_ATTRIBUTES);
 
-  static <T> Optional<T> singleton(Collection<T> collection) {
-    if (collection.isEmpty()) {
-      return Optional.empty();
+      var artifactLookup =
+          new Maven.Lookup(
+              library.mavenGroupColonArtifactMapper(),
+              map(load(new Properties(), lib.resolve("module-maven.properties"))),
+              map(load(new Properties(), artifactPath)));
+
+      var versionPath =
+          uris.copy(
+              URI.create(
+                  "https://github.com/sormuras/modules/raw/master/module-version.properties"),
+              cache.resolve("module-version.properties"),
+              StandardCopyOption.COPY_ATTRIBUTES);
+
+      var versionLookup =
+          new Maven.Lookup(
+              library.mavenVersionMapper(),
+              map(load(new Properties(), lib.resolve("module-version.properties"))),
+              map(load(new Properties(), versionPath)));
+      return new Maven(log, uris, artifactLookup, versionLookup);
     }
-    if (collection.size() != 1) {
-      throw new IllegalStateException("Too many elements: " + collection);
+
+    Properties load(Properties properties, Path path) {
+      if (Files.isRegularFile(path)) {
+        try (var reader = Files.newBufferedReader(path)) {
+          properties.load(reader);
+        } catch (Exception e) {
+          throw new RuntimeException("Reading properties failed: " + path, e);
+        }
+      }
+      return properties;
     }
-    return Optional.of(collection.iterator().next());
+
+    /**
+     * Convert all {@link String}-based properties in an instance of {@code Map<String, String>}.
+     */
+    Map<String, String> map(Properties properties) {
+      var map = new HashMap<String, String>();
+      for (var name : properties.stringPropertyNames()) {
+        map.put(name, properties.getProperty(name));
+      }
+      return Map.copyOf(map);
+    }
+
+    <T> Optional<T> singleton(Collection<T> collection) {
+      if (collection.isEmpty()) {
+        return Optional.empty();
+      }
+      if (collection.size() != 1) {
+        throw new IllegalStateException("Too many elements: " + collection);
+      }
+      return Optional.of(collection.iterator().next());
+    }
   }
 }
