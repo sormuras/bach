7b1bf9781bb9b0f21f12b7d9ec15168798e11cd3
Christian Stein <sormuras@gmail.com>
2019-11-22 05:58:02 +0100

    Add missing test module declaration

---
+++ src/de.sormuras.bach/test/module/module-info.java
@@ -0,0 +1,32 @@
+/*
+ * Bach - Java Shell Builder
+ * Copyright (C) 2019 Christian Stein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// --main-class de.sormuras.bach.Bach
+open /*test*/ module de.sormuras.bach /*extends "main" module*/ {
+  requires org.junit.jupiter;
+
+  // ---
+
+  requires java.compiler;
+  requires java.net.http;
+
+  // exports de.sormuras.bach;
+  // exports de.sormuras.bach.project;
+  // exports de.sormuras.bach.project;
+
+  uses java.util.spi.ToolProvider;
+}
