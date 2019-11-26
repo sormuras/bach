913e478ed3d41368ba1dd03059682c16ac561d9d
Christian Stein <sormuras@gmail.com>
2019-11-26 11:40:00 +0100

    Only compile normal modules and fix javadoc call

--- src/de.sormuras.bach/main/java/de/sormuras/bach/task/Jigsaw.java
+++ src/de.sormuras.bach/main/java/de/sormuras/bach/task/Jigsaw.java
@@ -24,6 +24,7 @@ import de.sormuras.bach.project.Realm;
 import de.sormuras.bach.project.Source;
 import de.sormuras.bach.project.Unit;
 import de.sormuras.bach.util.Paths;
+import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Predicate;
@@ -34,15 +35,15 @@ class Jigsaw {
   private final Bach bach;
   private final Realm realm;
   private final Folder folder;
-  private final Path classes;
-  private final Path javadoc;
+  private final Path classesDirectory;
+  private final Path javadocDirectory;
 
   Jigsaw(Bach bach, Realm realm) {
     this.bach = bach;
     this.realm = realm;
     this.folder = bach.getProject().folder();
-    this.classes = folder.realm(realm.name()).resolve("classes/jigsaw");
-    this.javadoc = folder.realm(realm.name()).resolve("javadoc/jigsaw");
+    this.classesDirectory = folder.realm(realm.name()).resolve("classes/jigsaw");
+    this.javadocDirectory = folder.realm(realm.name()).resolve("javadoc/jigsaw");
   }
 
   void compile(List<Unit> units) {
@@ -52,44 +53,53 @@ class Jigsaw {
             .map(Unit::name)
             .collect(Collectors.joining(","));
     var modulePaths = Paths.filterExisting(realm.modulePaths());
-    bach.execute(
-        new Call("javac")
-            .add("-d", classes)
-            .add("--module", normalNames)
-            // .addEach(realm.toolArguments.javac)
-            // .iff(realm.preview(), c -> c.add("--enable-preview"))
-            // .iff(realm.release() != 0, c -> c.add("--release", realm.release()))
-            .add("--module-source-path", realm.moduleSourcePath())
-            .forEach(units, this::patchModule)
-            .add("--module-path", modulePaths)
-            .add("--module-version", bach.getProject().version()));
+    if (!normalNames.isBlank()) {
+      bach.execute(
+          new Call("javac")
+              .add("-d", classesDirectory)
+              .add("--module", normalNames)
+              // .addEach(realm.toolArguments.javac)
+              // .iff(realm.preview(), c -> c.add("--enable-preview"))
+              // .iff(realm.release() != 0, c -> c.add("--release", realm.release()))
+              .add("--module-source-path", realm.moduleSourcePath())
+              .forEach(units, this::patchModule)
+              .add("--module-path", modulePaths)
+              .add("--module-version", bach.getProject().version()));
+    }
 
     if (realm.isDeployRealm()) {
-      // TODO var allModuleNames = units.stream().map(Unit::name).collect(Collectors.joining(","));
-      bach.execute(
+      var allModuleNames = units.stream().map(Unit::name).collect(Collectors.joining(","));
+      var allModuleSourcePaths =
+          units.stream()
+              .map(unit -> unit.info().getParent().toString().replace(unit.name(), "*"))
+              .distinct()
+              .collect(Collectors.joining(File.pathSeparator));
+      var javadoc =
           new Call("javadoc")
-              .add("-d", Paths.createDirectories(javadoc))
-              .add("--module", normalNames)
+              .add("-d", Paths.createDirectories(javadocDirectory))
+              .add("--module", allModuleNames)
               .add("-encoding", "UTF-8")
               .add("-locale", "en")
               .iff(!bach.isVerbose(), c -> c.add("-quiet"))
               .add("-Xdoclint:-missing")
               .add("--module-path", modulePaths)
-              .add("--module-source-path", realm.moduleSourcePath()));
-      // for (var unit : realm.units(Unit::isMultiRelease)) {
-      //   var base = unit.sources.get(0);
-      //   if (!unit.info.path.startsWith(base.path)) {
-      //     call.add("--patch-module", unit.name() + "=" + base.path);
-      //   }
-      // }
-
+              .add("--module-source-path", allModuleSourcePaths);
+      for (var unit : units) {
+        if (unit.isMultiRelease()) {
+          var base = unit.sources().get(0);
+          if (!unit.info().startsWith(base.path())) {
+            javadoc.add("--patch-module", unit.name() + "=" + base.path());
+          }
+        }
+      }
+      bach.execute(javadoc);
       bach.execute(
           new Call("jar")
               .add("--create")
               .add("--file", bach.getProject().javadocJar(realm))
               .iff(bach.isVerbose(), c -> c.add("--verbose"))
               .add("--no-manifest")
-              .add("-C", javadoc)
+              .add("-C", javadocDirectory)
               .add("."));
     }
 
@@ -118,7 +128,7 @@ class Jigsaw {
             .iff(bach.isVerbose(), c -> c.add("--verbose"))
             .iff(unit.descriptor().version(), (c, v) -> c.add("--module-version", v.toString()))
             .iff(unit.descriptor().mainClass(), (c, m) -> c.add("--main-class", m))
-            .add("-C", classes.resolve(unit.name()))
+            .add("-C", classesDirectory.resolve(unit.name()))
             .add(".")
             .forEach(resources, (cmd, path) -> cmd.add("-C", path).add(".")));
     if (bach.isVerbose()) {
