680a0e5bbfcdcbf00221b853200502f39171cc86
Christian Stein <sormuras@gmail.com>
2020-01-06 09:59:41 +0100

    Try to build javadoc with Gradle...

--- doc/comparison/minimal/gradle/build.gradle.kts
+++ doc/comparison/minimal/gradle/build.gradle.kts
@@ -8,3 +8,29 @@ java {
 }
 
 version = "0"
+
+tasks {
+
+    javadoc {
+        isFailOnError = false
+        (options as StandardJavadocDocletOptions).addStringOption("-module-source-path", "src/main/java")
+        (options as StandardJavadocDocletOptions).addStringOption("-module", "de.sormuras.bach.doc.minimal")
+    }
+
+    val sourcesJar by creating(Jar::class) {
+        dependsOn(JavaPlugin.CLASSES_TASK_NAME)
+        classifier = "sources"
+        from(sourceSets["main"].allJava)
+    }
+
+    val javadocJar by creating(Jar::class) {
+        dependsOn(JavaPlugin.JAVADOC_TASK_NAME)
+        classifier = "javadoc"
+        from(javadoc)
+    }
+
+    artifacts {
+        add("archives", sourcesJar)
+        add("archives", javadocJar)
+    }
+}
