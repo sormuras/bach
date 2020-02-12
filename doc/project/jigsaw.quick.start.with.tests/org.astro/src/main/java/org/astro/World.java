c58fd8df484f6c375edf544b86b2c4dacf2b96a8
Christian Stein <sormuras@gmail.com>
2020-02-12 17:23:29 +0100

    Introduce "jigsaw.quick.start.with.tests" demo project

---
+++ doc/project/jigsaw.quick.start.with.tests/org.astro/src/main/java/org/astro/World.java
@@ -0,0 +1,10 @@
+package org.astro;
+
+public class World {
+
+  static final String PACKAGE_PRIVATE_FIELD = "World";
+
+  public static String name() {
+    return PACKAGE_PRIVATE_FIELD;
+  }
+}
---
+++ doc/project/jigsaw.quick.start.with.tests/test.base/src/test/java/module-info.java
@@ -0,0 +1,6 @@
+module test.base {
+  requires com.greetings;
+  requires org.astro;
+
+  requires org.junit.jupiter;
+}
