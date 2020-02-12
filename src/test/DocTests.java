c0c9767983e0738df523e184a9bd37d9f39d9ae8
Christian Stein <sormuras@gmail.com>
2020-02-12 19:45:48 +0100

    Introduce integration tests that build documentation projects

---
+++ src/test/DocTests.java
@@ -0,0 +1,24 @@
+// default package
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.nio.file.Path;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DocTests {
+
+  @ParameterizedTest
+  @ValueSource(
+      strings = {"doc/project/jigsaw.quick.start", "doc/project/jigsaw.quick.start.with.tests"})
+  void project(Path base, @TempDir Path temp) {
+    var log = new Log();
+    var bach = new Bach(log, log, true);
+    var paths = new Bach.Project.Paths(base, temp.resolve("out"));
+    var summary = bach.build(paths, project -> project.version("0-ea"));
+    assertDoesNotThrow(summary::assertSuccessful);
+    // log.lines().forEach(System.out::println);
+    // summary.toMarkdown().forEach(System.out::println);
+  }
+}
