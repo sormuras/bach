6660f8a0102439d929355e7adc9a349438cc57fe
Christian Stein <sormuras@gmail.com>
2020-02-13 09:38:49 +0100

    Add unit tests for new utilities

--- src/test/UtilTests.java
+++ src/test/UtilTests.java
@@ -4,7 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
+import java.io.File;
+import java.lang.module.ModuleDescriptor;
+import java.lang.module.ModuleDescriptor.Version;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -33,6 +38,93 @@ class UtilTests {
               .forEach(List.of('a', 'b', 'c'), Bach.Util.Args::add);
       assertLinesMatch(
           List.of("1", "key", "value", "first", "second", "more", "a", "b", "c"), args.list());
+    }
+  }
+
+  @Nested
+  class Printable {
+
+    @Test
+    void object() {
+      assertLinesMatch(List.of("Object"), Bach.Util.Printable.print(new Object()));
+    }
+
+    @Test
+    void record() {
+
+      @SuppressWarnings({"unused", "FieldCanBeLocal"})
+      class Record implements Bach.Util.Printable {
+        private final boolean b = true;
+        private final char c = 'c';
+        private final int[] integers = {1, 2, 3};
+        private final String[] strings = {"s", "t"};
+
+        public boolean b() {
+          return b;
+        }
+
+        public char c() {
+          return c;
+        }
+
+        public int[] integers() {
+          return integers;
+        }
+
+        public String[] strings() {
+          return strings;
+        }
+      }
+
+      assertLinesMatch(
+          List.of(
+              "Record",
+              "  b = true",
+              "  c = c",
+              "  integers = [1, 2, 3]",
+              "  strings = [\"s\", \"t\"]"),
+          new Record().print());
+    }
+  }
+
+  @Nested
+  class Modules {
+    @Test
+    void minimalisticModuleDeclaration() {
+      var actual = Bach.Util.Modules.describe("module a{}");
+      assertEquals(ModuleDescriptor.newModule("a").build(), actual);
+    }
+
+    @Test
+    void moduleDeclarationWithRequires() {
+      var actual = Bach.Util.Modules.describe("module a{requires b;}");
+      assertEquals(ModuleDescriptor.newModule("a").requires("b").build(), actual);
+    }
+
+    @Test
+    void moduleDeclarationWithRequiresAndVersion() {
+      var actual = Bach.Util.Modules.describe("module a{requires b/*1.2*/;}");
+      assertEquals(
+          ModuleDescriptor.newModule("a").requires(Set.of(), "b", Version.parse("1.2")).build(),
+          actual);
+    }
+
+    @Test
+    void moduleDeclarationWithComments() {
+      var actual = Bach.Util.Modules.describe("open /*test*/ module a /*extends a*/ {}");
+      assertEquals(ModuleDescriptor.newModule("a").build(), actual);
+    }
+
+    @Test
+    void moduleSourcePathWithModuleNameAtTheEnd() {
+      var actual = Bach.Util.Modules.moduleSourcePath(Path.of("src/main/a.b.c"), "a.b.c");
+      assertEquals(Path.of("src/main").toString(), actual);
+    }
+
+    @Test
+    void moduleSourcePathWithNestedModuleName() {
+      var actual = Bach.Util.Modules.moduleSourcePath(Path.of("src/a.b.c/main/java"), "a.b.c");
+      assertEquals(String.join(File.separator, "src", "*", "main", "java"), actual);
     }
   }
 }
