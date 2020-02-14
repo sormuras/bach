9f49ff29857aa97f6a863c5176f653b4a8311af7
Christian Stein <sormuras@gmail.com>
2020-02-14 21:46:30 +0100

    Add more assertions

--- src/test/DocTests.java
+++ src/test/DocTests.java
@@ -2,6 +2,7 @@
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
@@ -21,6 +22,14 @@ class DocTests {
     assertEquals(name, project.descriptor().name());
     var units = project.units();
     assertEquals(1, units.size());
+    var greetings = units.get(0);
+    assertEquals("com.greetings", greetings.name());
+    assertEquals(base.resolve("com.greetings/module-info.java"), greetings.path());
+    assertEquals(1, greetings.sources().size());
+    assertEquals(0, greetings.resources().size());
+    assertFalse(greetings.isMainClassPresent()); // TODO should be true!
+    assertFalse(greetings.isMultiRelease());
+    System.out.println(greetings);
   }
 
   @Test
