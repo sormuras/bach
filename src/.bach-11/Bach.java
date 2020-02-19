1d39f084e1108f2e9844b17d9564ba025e3fcc1f
Christian Stein <sormuras@gmail.com>
2020-02-19 10:46:48 +0100

    Introduce URI-based download helper

--- src/.bach-11/Bach.java
+++ src/.bach-11/Bach.java
@@ -33,8 +33,17 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayDeque;
@@ -48,6 +57,7 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalInt;
@@ -2055,6 +2065,94 @@ public class Bach {
         } catch (IOException e) {
           throw new UncheckedIOException("Read all content from file failed: " + path, e);
         }
+      }
+    }
+
+    /** Uniform Resource Identifier ({@link URI}) read and download support. */
+    class Uris {
+
+      private final Logger logger;
+      private final HttpClient http;
+
+      public Uris() {
+        this(LOGGER, HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build());
+      }
+
+      public Uris(Logger logger, HttpClient http) {
+        this.logger = logger;
+        this.http = http;
+      }
+
+      /** Request head-only from the specified uri. */
+      public HttpResponse<Void> head(URI uri, int timeout) throws Exception {
+        var nobody = HttpRequest.BodyPublishers.noBody();
+        var duration = Duration.ofSeconds(timeout);
+        var request = HttpRequest.newBuilder(uri).method("HEAD", nobody).timeout(duration).build();
+        return http.send(request, HttpResponse.BodyHandlers.discarding());
+      }
+
+      /** Copy all content from a uri to a target file. */
+      public Path copy(URI uri, Path path, CopyOption... options) throws Exception {
+        logger.log(Level.DEBUG, "Copy {0} to {1}", uri, path);
+        Files.createDirectories(path.getParent());
+        if ("file".equals(uri.getScheme())) {
+          try {
+            return Files.copy(Path.of(uri), path, options);
+          } catch (Exception e) {
+            throw new IllegalArgumentException("copy file failed:" + uri, e);
+          }
+        }
+        var request = HttpRequest.newBuilder(uri).GET();
+        if (Files.exists(path) && Files.getFileStore(path).supportsFileAttributeView("user")) {
+          try {
+            var etagBytes = (byte[]) Files.getAttribute(path, "user:etag");
+            var etag = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(etagBytes)).toString();
+            request.setHeader("If-None-Match", etag);
+          } catch (Exception e) {
+            logger.log(Level.WARNING, "Couldn't get 'user:etag' file attribute: {0}", e);
+          }
+        }
+        var handler = HttpResponse.BodyHandlers.ofFile(path);
+        var response = http.send(request.build(), handler);
+        if (response.statusCode() == 200) {
+          if (Set.of(options).contains(StandardCopyOption.COPY_ATTRIBUTES)) {
+            var etagHeader = response.headers().firstValue("etag");
+            if (etagHeader.isPresent()) {
+              if (Files.getFileStore(path).supportsFileAttributeView("user")) {
+                try {
+                  var etag = etagHeader.get();
+                  Files.setAttribute(path, "user:etag", StandardCharsets.UTF_8.encode(etag));
+                } catch (Exception e) {
+                  logger.log(Level.WARNING, "Couldn't set 'user:etag' file attribute: {0}", e);
+                }
+              }
+            } else logger.log(Level.WARNING, "No etag provided in response: {0}", response);
+
+            var lastModifiedHeader = response.headers().firstValue("last-modified");
+            if (lastModifiedHeader.isPresent()) {
+              try {
+                @SuppressWarnings("SpellCheckingInspection")
+                var format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
+                var current = System.currentTimeMillis();
+                var millis = format.parse(lastModifiedHeader.get()).getTime(); // 0 means "unknown"
+                var fileTime = FileTime.fromMillis(millis == 0 ? current : millis);
+                Files.setLastModifiedTime(path, fileTime);
+              } catch (Exception e) {
+                logger.log(Level.WARNING, "Couldn't set last modified file attribute: {0}", e);
+              }
+            }
+          }
+          logger.log(Level.DEBUG, "{0} <- {1}", path, uri);
+        }
+        return path;
+      }
+
+      /** Read all content from a uri into a string. */
+      public String read(URI uri) throws Exception {
+        // log.debug("Read %s", uri);
+        if ("file".equals(uri.getScheme())) return Files.readString(Path.of(uri));
+        var request = HttpRequest.newBuilder(uri).GET();
+        return http.send(request.build(), HttpResponse.BodyHandlers.ofString()).body();
       }
     }
   }
