3c73772a81599c6f67df93304a162de8f4025e99
Christian Stein <sormuras@gmail.com>
2020-02-23 09:51:00 +0100

    Configure display name generation w/o junit-platform.properties

--- doc/project/jigsaw.quick.start.with.junit/src/test.base/test/java/test/base/PrependModuleNameTests.java
+++ doc/project/jigsaw.quick.start.with.junit/src/test.base/test/java/test/base/PrependModuleNameTests.java
@@ -2,8 +2,10 @@ package test.base;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(PrependModuleName.class)
 class PrependModuleNameTests {
 
   final PrependModuleName generator = new PrependModuleName();
--- doc/project/jigsaw.quick.start.with.junit/src/test.modules/test/java/test/modules/ModulesTests.java
+++ doc/project/jigsaw.quick.start.with.junit/src/test.modules/test/java/test/modules/ModulesTests.java
@@ -1,7 +1,10 @@
 package test.modules;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
+import test.base.PrependModuleName;
 
+@DisplayNameGeneration(PrependModuleName.class)
 class ModulesTests {
   @Test
   void test() {}
