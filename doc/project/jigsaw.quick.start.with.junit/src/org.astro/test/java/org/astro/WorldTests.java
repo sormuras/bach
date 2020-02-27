19b7cd065aa1d220a0646fee083893847091858e
Christian Stein <sormuras@gmail.com>
2020-02-27 17:47:55 +0100

    Transform into JUnit Jupiter test

--- doc/project/jigsaw.quick.start.with.junit/src/org.astro/test/java/org/astro/WorldTests.java
+++ doc/project/jigsaw.quick.start.with.junit/src/org.astro/test/java/org/astro/WorldTests.java
@@ -1,8 +1,17 @@
 package org.astro;
 
-public class WorldTests {
-  static {
-    if (!World.PACKAGE_PRIVATE_FIELD.equals("World")) throw new AssertionError(")-:");
-    new test.base.Tester();
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+import test.base.PrependModuleName;
+
+@DisplayNameGeneration(PrependModuleName.class)
+class WorldTests {
+  @Test
+  void test() {
+    assertEquals("World", World.PACKAGE_PRIVATE_FIELD);
+    assertNotNull(new test.base.Tester().toString());
   }
 }
