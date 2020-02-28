069e43c3efc65c4287dc79130d03bd3f5a67c0b3
Christian Stein <sormuras@gmail.com>
2020-02-28 10:36:25 +0100

    Use new accessor name

--- doc/project/jigsaw.quick.start.with.junit/src/com.greetings/main/java/com/greetings/Main.java
+++ doc/project/jigsaw.quick.start.with.junit/src/com.greetings/main/java/com/greetings/Main.java
@@ -5,7 +5,7 @@ import org.astro.World;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.format("Greetings %s! (%s)%n", World.name(), Main.class.getModule());
+    System.out.format("Greetings %s! (%s)%n", World.publicName(), Main.class.getModule());
     System.out.format("World from %s%n", World.class.getModule());
   }
 }
