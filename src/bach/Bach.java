66c99018472a04dac7fd81f16395fe382ee84a1e
Christian Stein <sormuras@gmail.com>
2020-03-02 06:41:00 +0100

    Update generated Bach.java

--- src/bach/Bach.java
+++ src/bach/Bach.java
@@ -14,25 +14,102 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.System.Logger.Level;
 import java.lang.module.ModuleDescriptor.Version;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.function.Consumer;
 /** Bach - Java Shell Builder. */
 public class Bach {
-  /** Version of Bach. */
+  /** Version of the Java Shell Builder. */
   public static Version VERSION = Version.parse("11.0-ea");
+  /** Default line printer instance delegates to {@link System#out}. */
+  private static final Consumer<String> PRINTER = System.out::println;
+  /** Default verbosity flag, including {@code -Debug} support. */
+  private static final boolean VERBOSE =
+      Boolean.getBoolean("verbose") // -D verbose=true
+          || Boolean.getBoolean("ebug") // -Debug=true
+          || "".equals(System.getProperty("ebug")); // -Debug
   /** Main entry-point. */
   public static void main(String... args) {
     Main.main(args);
   }
+  /** Line-based message printing consumer. */
+  private final Consumer<String> printer;
+  /** Verbosity flag. */
+  private final boolean verbose;
+  /** Initialize this instance with the specified line printer and verbosity flag. */
+  public Bach() {
+    this(PRINTER, VERBOSE);
+  }
+  /** Initialize this instance with the specified line printer and verbosity flag. */
+  public Bach(Consumer<String> printer, boolean verbose) {
+    this.printer = printer;
+    this.verbose = verbose;
+    print(Level.TRACE, "Bach initialized");
+  }
+  /** Verbosity flag. */
+  public boolean verbose() {
+    return verbose;
+  }
+  /** Line printer. */
+  private Consumer<String> printer() {
+    return printer;
+  }
+  /** Print a message at information level. */
+  public String print(String format, Object... args) {
+    return print(Level.INFO, format, args);
+  }
+  /** Print a message at specified level. */
+  public String print(Level level, String format, Object... args) {
+    var message = String.format(format, args);
+    if (verbose() || level.getSeverity() >= Level.INFO.getSeverity()) printer().accept(message);
+    return message;
+  }
   /** Build default project potentially modified by the passed project builder consumer. */
-  public Object build(Consumer<ProjectBuilder> projectBuilderConsumer) {
+  public Summary build(Consumer<ProjectBuilder> projectBuilderConsumer) {
     return build(project(projectBuilderConsumer));
   }
   /** Build the specified project. */
-  public Object build(Project project) {
-    return project;
+  public Summary build(Project project) {
+    return new Summary(project);
+  }
+  /** Run the given task and its attached child tasks. */
+  void execute(Task task, Summary summary) {
+    var markdown = task.toMarkdown();
+    print(Level.DEBUG, markdown);
+    summary.executionBegin(task);
+    var result = task.execute(new ExecutionContext(this));
+    if (verbose()) {
+      result.out().lines().forEach(printer());
+      result.err().lines().forEach(printer());
+    }
+    if (result.code() != 0) {
+      result.err().lines().forEach(printer);
+      summary.executionEnd(task, result);
+      var message = markdown + ": non-zero result code: " + result.code();
+      throw new RuntimeException(message);
+    }
+    var children = task.children();
+    if (!children.isEmpty()) {
+      try {
+        var tasks = task.parallel() ? children.parallelStream() : children.stream();
+        tasks.forEach(child -> execute(child, summary));
+      } catch (RuntimeException e) {
+        summary.error().addSuppressed(e);
+      }
+    }
+    summary.executionEnd(task, result);
   }
   /** Create new default project potentially modified by the passed project builder consumer. */
   Project project(Consumer<ProjectBuilder> projectBuilderConsumer) {
@@ -41,12 +118,119 @@ public class Bach {
     projectBuilderConsumer.accept(projectBuilder);
     return projectBuilder.build();
   }
+  // src/de.sormuras.bach/main/java/de/sormuras/bach/execution/ExecutionContext.java
+  /** Task execution context passed {@link Task#execute(ExecutionContext)}. */
+  public static final class ExecutionContext {
+    private final Bach bach;
+    private final Instant start;
+    private final StringWriter out;
+    private final StringWriter err;
+    public ExecutionContext(Bach bach) {
+      this.bach = bach;
+      this.start = Instant.now();
+      this.out = new StringWriter();
+      this.err = new StringWriter();
+    }
+    public Bach bach() {
+      return bach;
+    }
+    public Instant start() {
+      return start;
+    }
+    /** Print message if verbose flag is set. */
+    public void print(Level level, String format, Object... args) {
+      if (bach().verbose() || level.getSeverity() >= Level.INFO.getSeverity()) {
+        var writer = level.getSeverity() <= Level.INFO.getSeverity() ? out : err;
+        writer.write(String.format(format, args));
+        writer.write(System.lineSeparator());
+      }
+    }
+    /** Create result with code zero and empty output strings. */
+    public ExecutionResult ok() {
+      var duration = Duration.between(start(), Instant.now());
+      return new ExecutionResult(0, duration, out.toString(), err.toString(), null);
+    }
+    /** Create result with error code one and append throwable's message to the error string. */
+    public ExecutionResult failed(Throwable throwable) {
+      var duration = Duration.between(start(), Instant.now());
+      return new ExecutionResult(1, duration, out.toString(), err.toString(), throwable);
+    }
+  }
+  // src/de.sormuras.bach/main/java/de/sormuras/bach/execution/ExecutionResult.java
+  /** Task execution result record returned by {@link Task#execute(ExecutionContext)}. */
+  public static final class ExecutionResult {
+    private final int code;
+    private final Duration duration;
+    private final String out;
+    private final String err;
+    private final Throwable throwable;
+    public ExecutionResult(int code, Duration duration, String out, String err, Throwable throwable) {
+      this.code = code;
+      this.duration = duration;
+      this.out = out;
+      this.err = err;
+      this.throwable = throwable;
+    }
+    public int code() {
+      return code;
+    }
+    public Duration duration() {
+      return duration;
+    }
+    public String out() {
+      return out;
+    }
+    public String err() {
+      return err;
+    }
+    public Throwable throwable() {
+      return throwable;
+    }
+  }
+  // src/de.sormuras.bach/main/java/de/sormuras/bach/execution/Task.java
+  /** Executable task definition. */
+  public static class Task {
+    /** Group the given tasks for parallel execution. */
+    public static Task parallel(String title, Task... tasks) {
+      return new Task(title, true, List.of(tasks));
+    }
+    /** Group the given tasks for sequential execution. */
+    public static Task sequence(String title, Task... tasks) {
+      return new Task(title, false, List.of(tasks));
+    }
+    private final String title;
+    private final boolean parallel;
+    private final List<Task> children;
+    /** Initialize a task instance. */
+    public Task(String title, boolean parallel, List<Task> children) {
+      this.title = Objects.requireNonNull(title, "title");
+      this.parallel = parallel;
+      this.children = List.copyOf(children);
+    }
+    public String title() {
+      return title;
+    }
+    public boolean parallel() {
+      return parallel;
+    }
+    public List<Task> children() {
+      return children;
+    }
+    /** Default computation called before executing child tasks. */
+    public ExecutionResult execute(ExecutionContext execution) {
+      return execution.ok();
+    }
+    /** Return markdown representation of this task instance. */
+    public String toMarkdown() {
+      return title();
+    }
+  }
   // src/de.sormuras.bach/main/java/de/sormuras/bach/Main.java
   /** Bach's main program. */
   static class Main {
     public static void main(String... args) {
       System.out.println("Bach.java " + Bach.VERSION);
-      new Bach().build(project -> project.name("project")).toString();
+      new Bach().build(project -> project.name("project")).assertSuccessful();
     }
   }
   // src/de.sormuras.bach/main/java/de/sormuras/bach/model/Paths.java
@@ -144,8 +328,11 @@ public class Bach {
       this.paths = paths;
       return this;
     }
+    public ProjectBuilder paths(Path base) {
+      return paths(Paths.of(base));
+    }
     public ProjectBuilder paths(String base) {
-      return paths(Paths.of(Path.of(base)));
+      return paths(Path.of(base));
     }
   }
   // src/de.sormuras.bach/main/java/de/sormuras/bach/model/Structure.java
@@ -157,6 +344,196 @@ public class Bach {
     }
     public Paths paths() {
       return paths;
+    }
+  }
+  // src/de.sormuras.bach/main/java/de/sormuras/bach/Summary.java
+  /** Build summary. */
+  public static final class Summary {
+    private final Project project;
+    private final Deque<String> executions = new ConcurrentLinkedDeque<>();
+    private final Deque<Detail> details = new ConcurrentLinkedDeque<>();
+    private final AssertionError error = new AssertionError("Build failed");
+    public Summary(Project project) {
+      this.project = project;
+    }
+    public Project project() {
+      return project;
+    }
+    public AssertionError error() {
+      return error;
+    }
+    public void assertSuccessful() {
+      var exceptions = error.getSuppressed();
+      if (exceptions.length == 0) return;
+      var one = exceptions[0]; // first suppressed exception
+      if (exceptions.length == 1 && one instanceof RuntimeException) throw (RuntimeException) one;
+      throw error;
+    }
+    public int countedChildlessTasks() {
+      return details.size();
+    }
+    public int countedExecutionEvents() {
+      return executions.size();
+    }
+    /** Task execution is about to begin callback. */
+    void executionBegin(Task task) {
+      if (task.children().isEmpty()) return;
+      var format = "|   +|%6X|        | %s";
+      var thread = Thread.currentThread().getId();
+      var text = task.title();
+      executions.add(String.format(format, thread, text));
+    }
+    /** Task execution ended callback. */
+    void executionEnd(Task task, ExecutionResult result) {
+      var format = "|%4c|%6X|%8d| %s";
+      var children = task.children();
+      var kind = children.isEmpty() ? result.code() == 0 ? ' ' : 'X' : '=';
+      var thread = Thread.currentThread().getId();
+      var millis = result.duration().toMillis();
+      var title = children.isEmpty() ? "**" + task.title() + "**" : task.title();
+      var row = String.format(format, kind, thread, millis, title);
+      if (children.isEmpty()) {
+        var hash = Integer.toHexString(System.identityHashCode(task));
+        var detail = new Detail("Task Execution Details " + hash, task, result);
+        executions.add(row + " [...](#task-execution-details-" + hash + ")");
+        details.add(detail);
+      } else {
+        executions.add(row);
+      }
+    }
+    public List<String> toMarkdown() {
+      var md = new ArrayList<String>();
+      md.add("# Summary");
+      md.addAll(projectDescription());
+      md.addAll(taskExecutionOverview());
+      md.addAll(taskExecutionDetails());
+      md.addAll(exceptionDetails());
+      md.addAll(systemProperties());
+      return md;
+    }
+    private List<String> projectDescription() {
+      var md = new ArrayList<String>();
+      var version = Optional.ofNullable(project.version());
+      md.add("");
+      md.add("## Project");
+      md.add("- name: " + project.name());
+      md.add("- version: " + version.map(Object::toString).orElse("_none_"));
+      md.add("");
+      md.add("```text");
+      md.add(project.toString());
+      md.add("```");
+      return md;
+    }
+    private List<String> taskExecutionOverview() {
+      var md = new ArrayList<String>();
+      md.add("");
+      md.add("## Task Execution Overview");
+      md.add("|    |Thread|Duration|Caption");
+      md.add("|----|-----:|-------:|-------");
+      md.addAll(executions);
+      md.add("");
+      md.add("Legend");
+      md.add(" - A row starting with `+` denotes the start of a task container.");
+      md.add(" - A blank row start (` `) is a normal task execution. Its caption is emphasized.");
+      md.add(" - A row starting with `X` marks an erroneous task execution.");
+      md.add(" - A row starting with `=` marks the end (sum) of a task container.");
+      md.add(" - The Thread column shows the thread identifier, with `1` denoting main thread.");
+      md.add(" - Duration is measured in milliseconds.");
+      return md;
+    }
+    private List<String> taskExecutionDetails() {
+      if (details.isEmpty()) return List.of();
+      var md = new ArrayList<String>();
+      md.add("");
+      md.add("## Task Execution Details");
+      md.add("");
+      for (var detail : details) {
+        var result = detail.result;
+        md.add("### " + detail.caption);
+        md.add(" - Command = " + detail.task.toMarkdown());
+        md.add(" - Code = " + result.code());
+        md.add(" - Duration = " + result.duration());
+        md.add("");
+        if (!result.out().isBlank()) {
+          md.add("Normal (expected) output");
+          md.add("```");
+          md.add(result.out().strip());
+          md.add("```");
+        }
+        if (!result.err().isBlank()) {
+          md.add("Error output");
+          md.add("```");
+          md.add(result.err().strip());
+          md.add("```");
+        }
+        if (result.throwable() != null) {
+          var stackTrace = new StringWriter();
+          result.throwable().printStackTrace(new PrintWriter(stackTrace));
+          md.add("Throwable");
+          md.add("```");
+          stackTrace.toString().lines().forEach(md::add);
+          md.add("```");
+        }
+      }
+      return md;
+    }
+    private List<String> exceptionDetails() {
+      var exceptions = error.getSuppressed();
+      if (exceptions.length == 0) return List.of();
+      var md = new ArrayList<String>();
+      md.add("");
+      md.add("## Exception Details");
+      md.add("");
+      md.add("- Caught " + exceptions.length + " exception(s).");
+      md.add("");
+      for (var exception : exceptions) {
+        var stackTrace = new StringWriter();
+        exception.printStackTrace(new PrintWriter(stackTrace));
+        md.add("### " + exception.getMessage());
+        md.add("```text");
+        stackTrace.toString().lines().forEach(md::add);
+        md.add("```");
+      }
+      return md;
+    }
+    private List<String> systemProperties() {
+      var md = new ArrayList<String>();
+      md.add("");
+      md.add("## System Properties");
+      System.getProperties().stringPropertyNames().stream()
+          .sorted()
+          .forEach(key -> md.add(String.format("- `%s`: `%s`", key, systemProperty(key))));
+      return md;
+    }
+    private String systemProperty(String systemPropertyKey) {
+      var value = System.getProperty(systemPropertyKey);
+      if (!"line.separator".equals(systemPropertyKey)) return value;
+      var build = new StringBuilder();
+      for (char c : value.toCharArray()) {
+        build.append("0x").append(Integer.toHexString(c).toUpperCase());
+      }
+      return build.toString();
+    }
+    public Path write() {
+      var markdown = toMarkdown();
+      try {
+        var directory = project.paths().out();
+        Files.createDirectories(directory);
+        return Files.write(directory.resolve("summary.md"), markdown);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+    /** Task and its result tuple. */
+    private static final class Detail {
+      private final String caption;
+      private final Task task;
+      private final ExecutionResult result;
+      private Detail(String caption, Task task, ExecutionResult result) {
+        this.caption = caption;
+        this.task = task;
+        this.result = result;
+      }
     }
   }
 }
