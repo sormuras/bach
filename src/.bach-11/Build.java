84a0e5a29d9d0a6b5ef15a4340c9aeafaf8c1c61
Christian Stein <sormuras@gmail.com>
2020-03-26 13:12:58 +0100

    Remove superseded Bach.java implementation

--- src/.bach-11/Build.java
+++
@@ -1,9 +0,0 @@
-// default package
-
-/** Custom build program using {@code Bach.java} API. */
-class Build {
-
-  public static void main(String... args) {
-    new Bach().build(project -> project.version("1-ea")).assertSuccessful();
-  }
-}
