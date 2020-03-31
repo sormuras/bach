342e9a7bc41b16369c2942f5d678a45cb8f8e3e4
Christian Stein <sormuras@gmail.com>
2020-03-31 17:07:09 +0200

    Introduce realm

---
+++ src/de.sormuras.bach/main/java/de/sormuras/bach/api/Realm.java
@@ -0,0 +1,63 @@
+/*
+ * Bach - Java Shell Builder
+ * Copyright (C) 2020 Christian Stein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.sormuras.bach.api;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+/** A modular realm description. */
+public /*static*/ class Realm {
+
+  private final String name;
+  private final int release;
+  private final boolean preview;
+  private final List<Unit> units;
+
+  public Realm(String name, int release, boolean preview, List<Unit> units) {
+    this.name = name;
+    this.release = release;
+    this.preview = preview;
+    this.units = units;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public int release() {
+    return release;
+  }
+
+  public boolean preview() {
+    return preview;
+  }
+
+  public List<Unit> units() {
+    return units;
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", Realm.class.getSimpleName() + "[", "]")
+        .add("name='" + name + "'")
+        .add("release=" + release)
+        .add("preview=" + preview)
+        .add("units=" + units)
+        .toString();
+  }
+}
