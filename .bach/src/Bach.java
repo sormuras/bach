b5ec5e1c7c4fcd68b5d796509b61767d2924f50d
Christian Stein <sormuras@gmail.com>
2020-04-01 08:12:52 +0200

    Update generated Bach.java

--- .bach/src/Bach.java
+++ .bach/src/Bach.java
@@ -153,6 +153,10 @@ public class Bach {
           .add("directories=" + directories)
           .toString();
     }
+    public List<String> toRequiresNames() {
+      var names = descriptor.requires().stream().map(ModuleDescriptor.Requires::name);
+      return names.sorted().collect(Collectors.toList());
+    }
   }
   public static class Project {
     private final String name;
@@ -182,6 +186,28 @@ public class Bach {
     public String toNameAndVersion() {
       return name + ' ' + version;
     }
+    public List<String> toStrings() {
+      var strings = new ArrayList<String>();
+      strings.add("Project " + toNameAndVersion());
+      strings.add("\tModule Collections: " + structure.toCollectionNames());
+      for (var collection : structure.collections()) {
+        strings.add("\t\tModule Collection \"" + collection.name() + '"');
+        strings.add("\t\t\trelease=" + collection.release());
+        strings.add("\t\t\tpreview=" + collection.preview());
+        strings.add("\t\t\tModule Descriptions: [" + collection.modules().size() + ']');
+        for (var module : collection.modules()) {
+          strings.add("\t\t\t\tModule " + module.descriptor().toNameAndVersion());
+          strings.add("\t\t\t\t\tmainClass=" + module.descriptor().mainClass().orElse("<empty>"));
+          strings.add("\t\t\t\t\trequires=" + module.toRequiresNames());
+          strings.add("\t\t\t\t\tDirectories: [" + module.directories().size() + ']');
+          for (var directory : module.directories()) {
+            strings.add("\t\t\t\t\t\tpath=" + directory.path());
+            strings.add("\t\t\t\t\t\trelease=" + directory.release());
+          }
+        }
+      }
+      return List.copyOf(strings);
+    }
   }
   public static class Structure {
     private final List<ModuleCollection> collections;
@@ -195,6 +221,9 @@ public class Bach {
       return new StringJoiner(", ", Structure.class.getSimpleName() + "[", "]")
           .add("collections=" + collections)
           .toString();
+    }
+    public List<String> toCollectionNames() {
+      return collections.stream().map(ModuleCollection::name).collect(Collectors.toList());
     }
   }
   public interface Convention {
