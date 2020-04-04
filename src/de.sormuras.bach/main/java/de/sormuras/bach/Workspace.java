498ce767598354ba41be961e91159f5472649b7c
Christian Stein <sormuras@gmail.com>
2020-04-04 09:31:35 +0200

    Introduce workspace

---
+++ src/de.sormuras.bach/main/java/de/sormuras/bach/Workspace.java
@@ -0,0 +1,70 @@
+/*
+ * Bach - Java Shell Builder
+ * Copyright (C) 2020 Christian Stein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.sormuras.bach;
+
+import java.nio.file.Path;
+import java.util.StringJoiner;
+
+/** Well-known paths. */
+public /*static*/ final class Workspace {
+
+  public static Workspace of() {
+    return of(Path.of(""));
+  }
+
+  public static Workspace of(Path base) {
+    return new Workspace(base, base.resolve(".bach/workspace"));
+  }
+
+  private final Path base;
+  private final Path workspace;
+
+  public Workspace(Path base, Path workspace) {
+    this.base = base;
+    this.workspace = workspace;
+  }
+
+  public Path base() {
+    return base;
+  }
+
+  public Path workspace() {
+    return workspace;
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", Workspace.class.getSimpleName() + "[", "]")
+        .add("base=" + base)
+        .add("workspace=" + workspace)
+        .toString();
+  }
+
+  public Path workspace(String first, String... more) {
+    return workspace.resolve(Path.of(first, more));
+  }
+
+  public Path classes(String realm, int release) {
+    var version = "" + (release == 0 ? Runtime.version().feature() : release);
+    return workspace.resolve("classes").resolve(realm).resolve(version);
+  }
+
+  public Path modules(String realm) {
+    return workspace.resolve("modules").resolve(realm);
+  }
+}
---
+++ src/de.sormuras.bach/test/java/de/sormuras/bach/WorkspaceTests.java
@@ -0,0 +1,65 @@
+/*
+ * Bach - Java Shell Builder
+ * Copyright (C) 2020 Christian Stein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.sormuras.bach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.lang.reflect.Field;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+class WorkspaceTests {
+
+  @Test
+  void defaultWorkspace() {
+    var N = Runtime.version().feature();
+    var workspace = Workspace.of();
+    assertEquals(Path.of(""), workspace.base());
+    assertEquals(Path.of(".bach/workspace"), workspace.workspace());
+    assertEquals(Path.of(".bach/workspace", "first"), workspace.workspace("first"));
+    assertEquals(Path.of(".bach/workspace", "first/more"), workspace.workspace("first", "more"));
+    assertEquals(Path.of(".bach/workspace", "classes/realm/9"), workspace.classes("realm", 9));
+    assertEquals(Path.of(".bach/workspace", "classes/realm/" + N), workspace.classes("realm", 0));
+    assertEquals(Path.of(".bach/workspace", "modules/realm"), workspace.modules("realm"));
+  }
+
+  @TestFactory
+  Stream<DynamicTest> stringRepresentationContainsAllComponentNames() {
+    var string = Workspace.of().toString();
+    return componentNames(Workspace.class)
+        .map(name -> dynamicTest(name, () -> assertTrue(string.contains(name))));
+  }
+
+  private Stream<String> componentNames(Class<?> type) {
+    return Stream.of(type.getDeclaredFields()).filter(this::isComponent).map(Field::getName);
+  }
+
+  private boolean isComponent(Field field) {
+    try {
+      var method = field.getDeclaringClass().getMethod(field.getName());
+      return method.getReturnType() == field.getType();
+    } catch (NoSuchMethodException e) {
+      return false;
+    }
+  }
+}
