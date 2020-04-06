3b850c71aeeb6c16b1d74eff19f99a44f7285509
Christian Stein <sormuras@gmail.com>
2020-04-06 15:17:27 +0200

    Introduce virtual project examples for testing

---
+++ src/de.sormuras.bach/test/java/de/sormuras/bach/BuildTests.java
@@ -0,0 +1,51 @@
+/*
+ * Bach - Java Shell Builder
+ * Copyright (C) 2020 Christian Stein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.sormuras.bach;
+
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+
+import java.lang.System.Logger.Level;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import test.base.Log;
+import test.base.Tree;
+
+class BuildTests {
+
+  @Test
+  void buildJigsawQuickStartGreetings(@TempDir Path temp) throws Exception {
+    var example = Projects.exampleOfJigsawQuickStartGreetings();
+    var base = example.deploy(temp.resolve("greetings"));
+    assertLinesMatch(
+        List.of(
+            "src",
+            ">> PATH>>",
+            "src/com.greetings/com/greetings/Main.java",
+            "src/com.greetings/module-info.java"),
+        Tree.walk(base));
+
+    var log = new Log();
+    var bach = new Bach(new Printer.Default(log, Level.ALL), Workspace.of(base));
+    bach.build(example.project());
+    assertLinesMatch(
+        List.of(".bach", ">> PATHS >>", ".bach/workspace/summary.md", ">> PATHS >>"),
+        Tree.walk(base));
+  }
+}
---
+++ src/de.sormuras.bach/test/java/de/sormuras/bach/Projects.java
@@ -0,0 +1,86 @@
+/*
+ * Bach - Java Shell Builder
+ * Copyright (C) 2020 Christian Stein
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.sormuras.bach;
+
+import de.sormuras.bach.project.Structure;
+import de.sormuras.bach.project.structure.Directory;
+import de.sormuras.bach.project.structure.Realm;
+import de.sormuras.bach.project.structure.Unit;
+import java.lang.module.ModuleDescriptor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+/** Projects. */
+public interface Projects {
+
+  final class Example {
+
+    private final Project project;
+    private final Map<Path, String> files;
+
+    public Example(Project project, Map<Path, String> files) {
+      this.project = project;
+      this.files = files;
+    }
+
+    public Project project() {
+      return project;
+    }
+
+    Path deploy(Path base) throws Exception {
+      if (Files.exists(base)) throw new IllegalArgumentException("base directory must not exist");
+      Files.createDirectories(base);
+      for (var entry : files.entrySet()) {
+        var path = base.resolve(entry.getKey());
+        var string = path.toString();
+        var isDirectory = string.endsWith("/") || string.endsWith("\\");
+        if (isDirectory) Files.createDirectories(path);
+        else {
+          Files.createDirectories(path.getParent());
+          Files.writeString(path, entry.getValue());
+        }
+      }
+      return base;
+    }
+  }
+
+  static Example exampleOfJigsawQuickStartGreetings() {
+    var module = ModuleDescriptor.newModule("com.greetings").mainClass("com.greetings.Main");
+    var directory = new Directory(Path.of("src/com.greetings"), Directory.Type.SOURCE, 0);
+    var unit = new Unit(module.build(), List.of(directory));
+    var realm = new Realm("", 0, false, List.of(unit));
+    return new Example(
+        new Project(
+            "Jigsaw Quick Start: Greetings",
+            // https://openjdk.java.net/projects/jigsaw/quick-start
+            ModuleDescriptor.Version.parse("1"),
+            new Structure(List.of(realm))),
+        Map.of(
+            Path.of("src/com.greetings", "module-info.java"),
+            "module com.greetings {}\n",
+            Path.of("src/com.greetings/com/greetings", "Main.java"),
+            "package com.greetings;\n"
+                + "public class Main {\n"
+                + "  public static void main(String[] args) {\n"
+                + "    System.out.println(\"Greetings!\");\n"
+                + "  }\n"
+                + "}\n"));
+  }
+}
