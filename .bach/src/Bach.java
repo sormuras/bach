5998aa96e97c2c609d2530e8e5ec0116bf8ba9fb
Christian Stein <sormuras@gmail.com>
2020-04-07 08:12:22 +0200

    Update generated Bach.java

--- .bach/src/Bach.java
+++ .bach/src/Bach.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -32,11 +33,13 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.Deque;
 import java.util.EnumSet;
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.StringJoiner;
@@ -89,7 +92,7 @@ public class Bach {
   public void execute(Task task) {
     execute(new Task.Executor(this, null), task).assertSuccessful();
   }
-  Task.Executor.Summary execute(Task.Executor executor, Task task) {
+  private Task.Executor.Summary execute(Task.Executor executor, Task task) {
     printer.print(Level.DEBUG, "", "Execute task: " + task.name());
     var summary = executor.execute(task);
     printer.print(Level.DEBUG, "", "Executed tasks: " + summary.getTaskCount());
@@ -769,6 +772,217 @@ public class Bach {
     public void execute(Execution execution) {
       var base = execution.getBach().getWorkspace().base();
       if (Paths.isEmpty(base)) execution.print(Level.WARNING, "Empty base directory " + base.toUri());
+    }
+  }
+  public interface Tool {
+    static Any of(String name, Object... arguments) {
+      return new Any(name, arguments);
+    }
+    static JavaCompiler javac() {
+      return new JavaCompiler();
+    }
+    static String join(Collection<Path> paths) {
+      return paths.stream()
+          .map(Path::toString) // TODO Move replace() to after join()
+          .map(string -> string.replace("{MODULE}", "*"))
+          .collect(Collectors.joining(File.pathSeparator));
+    }
+    String name();
+    String[] args();
+    class Any implements Tool {
+      private final String name;
+      private final List<String> args = new ArrayList<>();
+      private Any(String name, Object... arguments) {
+        this.name = name;
+        addAll(arguments);
+      }
+      public String name() {
+        return name;
+      }
+      public Any add(Object argument) {
+        args.add(argument.toString());
+        return this;
+      }
+      public Any add(String key, Object value) {
+        return add(key).add(value);
+      }
+      public Any add(String key, Object first, Object second) {
+        return add(key).add(first).add(second);
+      }
+      public Any add(boolean predicate, Object first, Object... more) {
+        return predicate ? add(first).addAll(more) : this;
+      }
+      public Any addAll(Object... arguments) {
+        for (var argument : arguments) add(argument);
+        return this;
+      }
+      public <T> Any forEach(Iterable<T> iterable, BiConsumer<Any, T> visitor) {
+        iterable.forEach(argument -> visitor.accept(this, argument));
+        return this;
+      }
+      public String[] args() {
+        return args.toArray(String[]::new);
+      }
+    }
+    class JavaCompiler implements Tool {
+      private List<String> compileModulesCheckingTimestamps;
+      private Version versionOfModulesThatAreBeingCompiled;
+      private List<Path> pathsWhereToFindSourceFilesForModules;
+      private List<Path> pathsWhereToFindApplicationModules;
+      private Map<String, List<Path>> pathsWhereToFindMoreAssetsPerModule;
+      private String characterEncodingUsedBySourceFiles;
+      private int compileForVirtualMachineVersion;
+      private boolean enablePreviewLanguageFeatures;
+      private boolean generateMetadataForMethodParameters;
+      private boolean outputMessagesAboutWhatTheCompilerIsDoing;
+      private boolean outputSourceLocationsOfDeprecatedUsages;
+      private boolean terminateCompilationIfWarningsOccur;
+      private Path destinationDirectory;
+      private JavaCompiler() {}
+      public String name() {
+        return "javac";
+      }
+      public String[] args() {
+        var args = new ArrayList<String>();
+        if (isAssigned(getCompileModulesCheckingTimestamps())) {
+          args.add("--module");
+          args.add(String.join(",", getCompileModulesCheckingTimestamps()));
+        }
+        if (isAssigned(getVersionOfModulesThatAreBeingCompiled())) {
+          args.add("--module-version");
+          args.add(String.valueOf(getVersionOfModulesThatAreBeingCompiled()));
+        }
+        if (isAssigned(getPathsWhereToFindSourceFilesForModules())) {
+          args.add("--module-source-path");
+          args.add(join(getPathsWhereToFindSourceFilesForModules()));
+        }
+        if (isAssigned(getPathsWhereToFindApplicationModules())) {
+          args.add("--module-path");
+          args.add(join(getPathsWhereToFindApplicationModules()));
+        }
+        if (isAssigned(getPathsWhereToFindMoreAssetsPerModule())) {
+          for (var patch : getPathsWhereToFindMoreAssetsPerModule().entrySet()) {
+            args.add("--patch-module");
+            args.add(patch.getKey() + '=' + join(patch.getValue()));
+          }
+        }
+        if (isAssigned(getCompileForVirtualMachineVersion())) {
+          args.add("--release");
+          args.add(String.valueOf(getCompileForVirtualMachineVersion()));
+        }
+        if (isEnablePreviewLanguageFeatures()) args.add("--enable-preview");
+        if (isGenerateMetadataForMethodParameters()) args.add("-parameters");
+        if (isOutputSourceLocationsOfDeprecatedUsages()) args.add("-deprecation");
+        if (isOutputMessagesAboutWhatTheCompilerIsDoing()) args.add("-verbose");
+        if (isTerminateCompilationIfWarningsOccur()) args.add("-Werror");
+        if (isAssigned(getCharacterEncodingUsedBySourceFiles())) {
+          args.add("-encoding");
+          args.add(getCharacterEncodingUsedBySourceFiles());
+        }
+        if (isAssigned(getDestinationDirectory())) {
+          args.add("-d");
+          args.add(String.valueOf(getDestinationDirectory()));
+        }
+        return args.toArray(String[]::new);
+      }
+      public JavaCompiler setCompileModulesCheckingTimestamps(List<String> modules) {
+        this.compileModulesCheckingTimestamps = modules;
+        return this;
+      }
+      public List<String> getCompileModulesCheckingTimestamps() {
+        return compileModulesCheckingTimestamps;
+      }
+      public JavaCompiler setPathsWhereToFindSourceFilesForModules(List<Path> moduleSourcePath) {
+        this.pathsWhereToFindSourceFilesForModules = moduleSourcePath;
+        return this;
+      }
+      public List<Path> getPathsWhereToFindSourceFilesForModules() {
+        return pathsWhereToFindSourceFilesForModules;
+      }
+      public JavaCompiler setPathsWhereToFindApplicationModules(List<Path> modulePath) {
+        this.pathsWhereToFindApplicationModules = modulePath;
+        return this;
+      }
+      public List<Path> getPathsWhereToFindApplicationModules() {
+        return pathsWhereToFindApplicationModules;
+      }
+      public JavaCompiler setDestinationDirectory(Path destinationDirectory) {
+        this.destinationDirectory = destinationDirectory;
+        return this;
+      }
+      public Path getDestinationDirectory() {
+        return destinationDirectory;
+      }
+      public JavaCompiler setPathsWhereToFindMoreAssetsPerModule(Map<String, List<Path>> patches) {
+        this.pathsWhereToFindMoreAssetsPerModule = patches;
+        return this;
+      }
+      public Map<String, List<Path>> getPathsWhereToFindMoreAssetsPerModule() {
+        return pathsWhereToFindMoreAssetsPerModule;
+      }
+      public JavaCompiler setCharacterEncodingUsedBySourceFiles(String encoding) {
+        this.characterEncodingUsedBySourceFiles = encoding;
+        return this;
+      }
+      public String getCharacterEncodingUsedBySourceFiles() {
+        return characterEncodingUsedBySourceFiles;
+      }
+      public JavaCompiler setCompileForVirtualMachineVersion(int release) {
+        this.compileForVirtualMachineVersion = release;
+        return this;
+      }
+      public int getCompileForVirtualMachineVersion() {
+        return compileForVirtualMachineVersion;
+      }
+      public JavaCompiler setEnablePreviewLanguageFeatures(boolean enablePreview) {
+        this.enablePreviewLanguageFeatures = enablePreview;
+        return this;
+      }
+      public boolean isEnablePreviewLanguageFeatures() {
+        return enablePreviewLanguageFeatures;
+      }
+      public JavaCompiler setGenerateMetadataForMethodParameters(boolean parameters) {
+        this.generateMetadataForMethodParameters = parameters;
+        return this;
+      }
+      public boolean isGenerateMetadataForMethodParameters() {
+        return generateMetadataForMethodParameters;
+      }
+      public JavaCompiler setOutputSourceLocationsOfDeprecatedUsages(boolean deprecation) {
+        this.outputSourceLocationsOfDeprecatedUsages = deprecation;
+        return this;
+      }
+      public boolean isOutputSourceLocationsOfDeprecatedUsages() {
+        return outputSourceLocationsOfDeprecatedUsages;
+      }
+      public JavaCompiler setOutputMessagesAboutWhatTheCompilerIsDoing(boolean verbose) {
+        this.outputMessagesAboutWhatTheCompilerIsDoing = verbose;
+        return this;
+      }
+      public boolean isOutputMessagesAboutWhatTheCompilerIsDoing() {
+        return outputMessagesAboutWhatTheCompilerIsDoing;
+      }
+      public JavaCompiler setTerminateCompilationIfWarningsOccur(boolean warningsAreErrors) {
+        this.terminateCompilationIfWarningsOccur = warningsAreErrors;
+        return this;
+      }
+      public boolean isTerminateCompilationIfWarningsOccur() {
+        return terminateCompilationIfWarningsOccur;
+      }
+      public JavaCompiler setVersionOfModulesThatAreBeingCompiled(Version moduleVersion) {
+        this.versionOfModulesThatAreBeingCompiled = moduleVersion;
+        return this;
+      }
+      public Version getVersionOfModulesThatAreBeingCompiled() {
+        return versionOfModulesThatAreBeingCompiled;
+      }
+    }
+    private static boolean isAssigned(Object object) {
+      if (object == null) return false;
+      if (object instanceof Number) return ((Number) object).intValue() != 0;
+      if (object instanceof Optional) return ((Optional<?>) object).isPresent();
+      if (object instanceof Collection) return !((Collection<?>) object).isEmpty();
+      return true;
     }
   }
   public static class Paths {
