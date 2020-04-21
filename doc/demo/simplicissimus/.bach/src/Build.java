fbb3b4ea867b9109b28d7a2e5253d26796ac460a
Christian Stein <sormuras@gmail.com>
2020-04-21 06:19:21 +0200

    Remove demo project for the time being

--- doc/demo/simplicissimus/.bach/src/Build.java
+++
@@ -1,9 +0,0 @@
-import java.nio.file.Path;
-
-class Build {
-  public static void main(String[] args) {
-    System.out.println("Build Simplicissimus");
-    System.out.println(Path.of("").toUri());
-    Bach.main(args);
-  }
-}
--- doc/demo/simplicissimus/README.md
+++
@@ -1,3 +0,0 @@
-# Simplicissimus Demo Project 
-
-Demonstrates most features of Bach.java with using only tools and modules provided by the JDK.
