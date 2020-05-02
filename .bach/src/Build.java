00f2b0e7d6734e45c5427c8378c59a7f0809bbdf
Christian Stein <sormuras@gmail.com>
2020-05-02 06:10:09 +0200

    Update build program

--- .bach/src/Build.java
+++ .bach/src/Build.java
@@ -15,6 +15,11 @@
  * limitations under the License.
  */
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Bach's own build program.
  *
@@ -23,7 +28,77 @@
 class Build {
 
   public static void main(String... args) {
-    var bach = Bach.of(project -> project.version(Bach.VERSION));
-    System.out.println(bach.toString());
+    var base = Bach.Project.Base.of();
+    var realms = List.of(mainRealm(base));
+    var project =
+        new Bach.Project(
+            Bach.Project.Base.of(),
+            new Bach.Project.Info(
+                "\uD83C\uDFBC Java Shell Builder - Build modular Java projects with JDK tools",
+                Bach.VERSION),
+            new Bach.Project.Structure(realms, List.of()));
+    var bach = Bach.of(project);
+    bach.build();
+  }
+
+  static Bach.Project.Realm mainRealm(Bach.Project.Base base) {
+    var units =
+        List.of(
+            new Bach.Project.Unit(
+                Bach.Modules.describe(Path.of("src/de.sormuras.bach/main/java/module-info.java")),
+                List.of(
+                    new Bach.Task.CreateJar(
+                        base.modules("main").resolve("de.sormuras.bach.jar"),
+                        base.classes("main", "de.sormuras.bach")))));
+    var moduleNames = units.stream().map(Bach.Project.Unit::name).collect(Collectors.joining(","));
+    var moduleSourcePath = "src/*/main/java".replace('/', File.separatorChar);
+    var javac =
+        Bach.Task.runTool(
+            "javac",
+            "--module",
+            moduleNames,
+            "--module-source-path",
+            moduleSourcePath,
+            "-d",
+            base.classes("main"));
+    var javadoc =
+        Bach.Task.sequence(
+            "Create API documentation",
+            List.of(
+                new Bach.Task.CreateDirectories(base.api()),
+                Bach.Task.runTool(
+                    "javadoc",
+                    "--module",
+                    moduleNames,
+                    "--module-source-path",
+                    moduleSourcePath,
+                    "-quiet",
+                    "-encoding",
+                    "UTF-8",
+                    "-locale",
+                    "en",
+                    "-d",
+                    base.api())));
+    var jlink =
+        Bach.Task.sequence(
+            "Create custom runtime image",
+            List.of(
+                new Bach.Task.DeleteDirectories(base.image()),
+                Bach.Task.runTool(
+                    "jlink",
+                    "--launcher",
+                    "bach=de.sormuras.bach/de.sormuras.bach.Main",
+                    "--add-modules",
+                    moduleNames,
+                    "--module-path",
+                    base.modules("main"),
+                    "--compress",
+                    "2",
+                    "--strip-debug",
+                    "--no-header-files",
+                    "--no-man-pages",
+                    "--output",
+                    base.image())));
+    return new Bach.Project.Realm("main", units, javac, List.of(javadoc, jlink));
   }
 }
