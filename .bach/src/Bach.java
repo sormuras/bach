487f4b5aa601dd923d152536eae0fa29d26f4845
Christian Stein <sormuras@gmail.com>
2020-05-04 14:18:58 +0200

    Update generated Bach.java

--- .bach/src/Bach.java
+++ .bach/src/Bach.java
@@ -314,75 +314,7 @@ public class Bach {
     }
     public static final class Library {
       public static Library of() {
-        var map = new TreeMap<String, String>();
-        map.put("junit", "https://repo.maven.apache.org/maven2/junit/junit/4.13/junit-4.13.jar");
-        map.put(
-            "net.bytebuddy",
-            "https://repo.maven.apache.org/maven2/net/bytebuddy/byte-buddy/1.10.9/byte-buddy-1.10.9.jar");
-        map.put(
-            "net.bytebuddy.agent",
-            "https://repo.maven.apache.org/maven2/net/bytebuddy/byte-buddy-agent/1.10.9/byte-buddy-agent-1.10.9.jar");
-        map.put(
-            "org.apiguardian.api",
-            "https://repo.maven.apache.org/maven2/org/apiguardian/apiguardian-api/1.1.0/apiguardian-api-1.1.0.jar");
-        map.put(
-            "org.assertj.core",
-            "https://repo.maven.apache.org/maven2/org/assertj/assertj-core/3.15.0/assertj-core-3.15.0.jar");
-        map.put(
-            "org.hamcrest",
-            "https://repo.maven.apache.org/maven2/org/hamcrest/hamcrest/2.2/hamcrest-2.2.jar");
-        map.put(
-            "org.junit.jupiter",
-            "https://repo.maven.apache.org/maven2/org/junit/jupiter/junit-jupiter/5.7.0-M1/junit-jupiter-5.7.0-M1.jar");
-        map.put(
-            "org.junit.jupiter.api",
-            "https://repo.maven.apache.org/maven2/org/junit/jupiter/junit-jupiter-api/5.7.0-M1/junit-jupiter-api-5.7.0-M1.jar");
-        map.put(
-            "org.junit.jupiter.engine",
-            "https://repo.maven.apache.org/maven2/org/junit/jupiter/junit-jupiter-engine/5.7.0-M1/junit-jupiter-engine-5.7.0-M1.jar");
-        map.put(
-            "org.junit.jupiter.params",
-            "https://repo.maven.apache.org/maven2/org/junit/jupiter/junit-jupiter-params/5.7.0-M1/junit-jupiter-params-5.7.0-M1.jar");
-        map.put(
-            "org.junit.platform.commons",
-            "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-commons/1.7.0-M1/junit-platform-commons-1.7.0-M1.jar");
-        map.put(
-            "org.junit.platform.console",
-            "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-console/1.7.0-M1/junit-platform-console-1.7.0-M1.jar");
-        map.put(
-            "org.junit.platform.engine",
-            "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-engine/1.7.0-M1/junit-platform-engine-1.7.0-M1.jar");
-        map.put(
-            "org.junit.platform.launcher",
-            "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-launcher/1.7.0-M1/junit-platform-launcher-1.7.0-M1.jar");
-        map.put(
-            "org.junit.platform.reporting",
-            "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-reporting/1.7.0-M1/junit-platform-reporting-1.7.0-M1.jar");
-        map.put(
-            "org.junit.platform.testkit",
-            "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-testkit/1.7.0-M1/junit-platform-testkit-1.7.0-M1.jar");
-        map.put(
-            "org.junit.vintage.engine",
-            "https://repo.maven.apache.org/maven2/org/junit/vintage/junit-vintage-engine/5.7.0-M1/junit-vintage-engine-5.7.0-M1.jar");
-        map.put(
-            "org.objectweb.asm",
-            "https://repo.maven.apache.org/maven2/org/ow2/asm/asm/8.0.1/asm-8.0.1.jar");
-        map.put(
-            "org.objectweb.asm.commons",
-            "https://repo.maven.apache.org/maven2/org/ow2/asm/asm-commons/8.0.1/asm-commons-8.0.1.jar");
-        map.put(
-            "org.objectweb.asm.tree",
-            "https://repo.maven.apache.org/maven2/org/ow2/asm/asm-tree/8.0.1/asm-tree-8.0.1.jar");
-        map.put(
-            "org.objectweb.asm.tree.analysis",
-            "https://repo.maven.apache.org/maven2/org/ow2/asm/asm-analysis/8.0.1/asm-analysis-8.0.1.jar");
-        map.put(
-            "org.objectweb.asm.util",
-            "https://repo.maven.apache.org/maven2/org/ow2/asm/asm-util/8.0.1/asm-util-8.0.1.jar");
-        map.put(
-            "org.opentest4j",
-            "https://repo.maven.apache.org/maven2/org/opentest4j/opentest4j/1.2.0/opentest4j-1.2.0.jar");
-        return of(map);
+        return of(new ModulesMap());
       }
       public static Library of(Map<String, String> map) {
         return new Library(Set.of(), map::get);
@@ -871,6 +803,78 @@ public class Bach {
           .collect(Collectors.toCollection(TreeSet::new));
     }
     private Modules() {}
+  }
+  public static class ModulesMap extends TreeMap<String, String> {
+    private static final long serialVersionUID = -7978021121082640440L;
+    public ModulesMap() {
+      put("junit", "https://repo.maven.apache.org/maven2/junit/junit/4.13/junit-4.13.jar");
+      put(
+          "net.bytebuddy",
+          "https://repo.maven.apache.org/maven2/net/bytebuddy/byte-buddy/1.10.9/byte-buddy-1.10.9.jar");
+      put(
+          "net.bytebuddy.agent",
+          "https://repo.maven.apache.org/maven2/net/bytebuddy/byte-buddy-agent/1.10.9/byte-buddy-agent-1.10.9.jar");
+      put(
+          "org.apiguardian.api",
+          "https://repo.maven.apache.org/maven2/org/apiguardian/apiguardian-api/1.1.0/apiguardian-api-1.1.0.jar");
+      put(
+          "org.assertj.core",
+          "https://repo.maven.apache.org/maven2/org/assertj/assertj-core/3.15.0/assertj-core-3.15.0.jar");
+      put(
+          "org.hamcrest",
+          "https://repo.maven.apache.org/maven2/org/hamcrest/hamcrest/2.2/hamcrest-2.2.jar");
+      put(
+          "org.junit.jupiter",
+          "https://repo.maven.apache.org/maven2/org/junit/jupiter/junit-jupiter/5.7.0-M1/junit-jupiter-5.7.0-M1.jar");
+      put(
+          "org.junit.jupiter.api",
+          "https://repo.maven.apache.org/maven2/org/junit/jupiter/junit-jupiter-api/5.7.0-M1/junit-jupiter-api-5.7.0-M1.jar");
+      put(
+          "org.junit.jupiter.engine",
+          "https://repo.maven.apache.org/maven2/org/junit/jupiter/junit-jupiter-engine/5.7.0-M1/junit-jupiter-engine-5.7.0-M1.jar");
+      put(
+          "org.junit.jupiter.params",
+          "https://repo.maven.apache.org/maven2/org/junit/jupiter/junit-jupiter-params/5.7.0-M1/junit-jupiter-params-5.7.0-M1.jar");
+      put(
+          "org.junit.platform.commons",
+          "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-commons/1.7.0-M1/junit-platform-commons-1.7.0-M1.jar");
+      put(
+          "org.junit.platform.console",
+          "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-console/1.7.0-M1/junit-platform-console-1.7.0-M1.jar");
+      put(
+          "org.junit.platform.engine",
+          "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-engine/1.7.0-M1/junit-platform-engine-1.7.0-M1.jar");
+      put(
+          "org.junit.platform.launcher",
+          "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-launcher/1.7.0-M1/junit-platform-launcher-1.7.0-M1.jar");
+      put(
+          "org.junit.platform.reporting",
+          "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-reporting/1.7.0-M1/junit-platform-reporting-1.7.0-M1.jar");
+      put(
+          "org.junit.platform.testkit",
+          "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-testkit/1.7.0-M1/junit-platform-testkit-1.7.0-M1.jar");
+      put(
+          "org.junit.vintage.engine",
+          "https://repo.maven.apache.org/maven2/org/junit/vintage/junit-vintage-engine/5.7.0-M1/junit-vintage-engine-5.7.0-M1.jar");
+      put(
+          "org.objectweb.asm",
+          "https://repo.maven.apache.org/maven2/org/ow2/asm/asm/8.0.1/asm-8.0.1.jar");
+      put(
+          "org.objectweb.asm.commons",
+          "https://repo.maven.apache.org/maven2/org/ow2/asm/asm-commons/8.0.1/asm-commons-8.0.1.jar");
+      put(
+          "org.objectweb.asm.tree",
+          "https://repo.maven.apache.org/maven2/org/ow2/asm/asm-tree/8.0.1/asm-tree-8.0.1.jar");
+      put(
+          "org.objectweb.asm.tree.analysis",
+          "https://repo.maven.apache.org/maven2/org/ow2/asm/asm-analysis/8.0.1/asm-analysis-8.0.1.jar");
+      put(
+          "org.objectweb.asm.util",
+          "https://repo.maven.apache.org/maven2/org/ow2/asm/asm-util/8.0.1/asm-util-8.0.1.jar");
+      put(
+          "org.opentest4j",
+          "https://repo.maven.apache.org/maven2/org/opentest4j/opentest4j/1.2.0/opentest4j-1.2.0.jar");
+    }
   }
   public static class ModulesResolver {
     private final Path[] paths;
