204c1cd7b53c96913fc364c442e3ebb9bf5106a7
Christian Stein <sormuras@gmail.com>
2020-06-26 18:54:15 +0200

    Next generation API playground

--- src/test.preview/test-preview/java/test/preview/RecordBasedApiTests.java
+++ src/test.preview/test-preview/java/test/preview/RecordBasedApiTests.java
@@ -1,64 +1,47 @@
 package test.preview;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.lang.module.ModuleDescriptor.Version;
-import java.util.Set;
-import java.util.TreeSet;
 import org.junit.jupiter.api.Test;
 
 public class RecordBasedApiTests {
 
   @Test
   void check() {
-    assertTitle("<untitled>", Project.newProject());
-    assertTitle("Title", Project.newProject().title("Title"));
-    assertVersion("47.11", Project.newProject().version("47.11"));
-    assertFalse(Project
-        .newProject()
-        .requires("foo", "bar")
-        .library()
-        .requires()
-        .isEmpty());
+    assertEquals("1-ea",
+        Project.of().version().toString());
+
+    assertEquals("javac --module-version 1-ea",
+        Project.of().getJavacForMainSources());
+
+    assertEquals("javac --module-version 17.01",
+        Project.of().with(Version.parse("17.01")).getJavacForMainSources());
+
+    assertEquals("javac --module-version 47.11",
+        Project.of().with(new MainSources("javac --module-version 47.11")).getJavacForMainSources());
   }
 
-  static void assertTitle(String expectedTitle, Project project) {
-    assertEquals(expectedTitle, project.title());
-  }
+  record Project(Version version, MainSources main) {
 
-  static void assertVersion(String expectedVersion, Project project) {
-    assertEquals(Version.parse(expectedVersion), project.version());
-  }
-
-  record Project(String title, Version version, Library library)  {
-
-    static Project newProject() {
-      return new Project("<untitled>", Version.parse("1-ea"), Library.newLibrary());
+    public static Project of() {
+      return new Project(Version.parse("1-ea"), new MainSources(""));
     }
 
-    public Project title(String title) {
-      return new Project(title, version, library);
+    public Project with(Version version) {
+      return new Project(version, main);
     }
-    public Project version(String version) {
-      return new Project(title, Version.parse(version), library);
+
+    public Project with(MainSources main) {
+      return new Project(version, main);
     }
-    public Project requires(String module, String... more) {
-      return new Project(title, version, library.requires(module, more));
+
+    public String getJavacForMainSources() {
+      var javac = main().customJavac();
+      return javac.isEmpty() ? "javac --module-version " + version() : javac;
     }
   }
 
-  record Library(Set<String> requires)  {
+  record MainSources(String customJavac) {}
 
-    public static Library newLibrary() {
-      return new Library(Set.of());
-    }
-
-    public Library requires(String module, String... more) {
-      var set = new TreeSet<>(requires);
-      set.add(module);
-      if (more.length > 0) set.addAll(Set.of(more));
-      return new Library(set);
-    }
-  }
 }
