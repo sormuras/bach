8c72fd85527c176fe9ee98caa75c82d933824371
Christian Stein <sormuras@gmail.com>
2020-06-27 05:49:12 +0200

    Test-drive record Bach

--- src/test.preview/test-preview/java/test/preview/RecordBasedApiTests.java
+++ src/test.preview/test-preview/java/test/preview/RecordBasedApiTests.java
@@ -2,6 +2,7 @@ package test.preview;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.lang.System.Logger.Level;
 import java.lang.module.ModuleDescriptor.Version;
 import org.junit.jupiter.api.Test;
 
@@ -9,18 +10,21 @@ public class RecordBasedApiTests {
 
   @Test
   void check() {
+    var bach = Bach.of().with(Level.DEBUG);
+
     var project = Project.of()
+        .with(new MainSources(bach.defaultRelease()))
         .with(Version.parse("17.01"))
         .with(new MainSources(8));
 
     assertEquals("17.01", project.version().toString());
     assertEquals(8, project.main().release());
 
-    project.build();
+    bach.build(project);
 
-    class CustomBuilder extends Builder {
-      CustomBuilder() {
-        super(project);
+    class CustomWorkflow extends Workflow {
+      CustomWorkflow(Bach bach, Project project) {
+        super(bach, project);
       }
 
       @Override
@@ -29,7 +33,34 @@ public class RecordBasedApiTests {
       }
     }
 
-    new CustomBuilder().build();
+    bach.with(CustomWorkflow::new).build(project);
+  }
+
+  record Bach(Level threshold, Workflow.Constructor workflowConstructor) {
+
+    public static Bach of() {
+      return new Bach(Level.INFO, Workflow::new);
+    }
+
+    Bach with(Level threshold) {
+      return new Bach(threshold, workflowConstructor);
+    }
+
+    Bach with(Workflow.Constructor workflowConstructor) {
+      return new Bach(threshold, workflowConstructor);
+    }
+
+    void build(Project project) {
+      workflowConstructor.newWorkflow(this, project).build();
+    }
+
+    boolean log(Level level) {
+      return threshold == Level.ALL || level.getSeverity() >= threshold.getSeverity();
+    }
+
+    int defaultRelease() {
+      return Runtime.version().feature();
+    }
   }
 
   record Project(Version version, MainSources main) {
@@ -45,23 +76,27 @@ public class RecordBasedApiTests {
     public Project with(MainSources main) {
       return new Project(version, main);
     }
-
-    public void build() {
-      new Builder(this).build();
-    }
   }
 
   record MainSources(int release) {}
 
-  static class Builder {
+  static class Workflow {
+
+    @FunctionalInterface
+    interface Constructor {
+      Workflow newWorkflow(Bach bach, Project project);
+    }
+
+    private final Bach bach;
     private final Project project;
 
-    Builder(Project project) {
+    Workflow(Bach bach, Project project) {
+      this.bach = bach;
       this.project = project;
     }
 
     void build() {
-      System.out.println("\n" + getClass());
+      if (bach.log(Level.DEBUG)) System.out.println("\n" + getClass());
       System.out.println(computeJavac(project.main()));
     }
 
