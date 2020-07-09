0033883e762c720ed345751b83d7ef54cc76ce50
Christian Stein <sormuras@gmail.com>
2020-07-09 11:38:58 +0200

    Clean up MainSources's API and member order

--- src/de.sormuras.bach/main/java/de/sormuras/bach/project/MainSources.java
+++ src/de.sormuras.bach/main/java/de/sormuras/bach/project/MainSources.java
@@ -17,29 +17,16 @@
 
 package de.sormuras.bach.project;
 
+import de.sormuras.bach.internal.Factory;
+import de.sormuras.bach.internal.Factory.Kind;
+
 /** Source set of {@code main} modules. */
 public final class MainSources {
 
-  public static MainSources of() {
-    return new MainSources(JavaRelease.ofRuntime(), SourceUnits.of());
-  }
-
-  public MainSources with(JavaRelease release) {
-    return new MainSources(release, units);
-  }
-
-  public MainSources with(SourceUnits units) {
-    return new MainSources(release, units);
-  }
-
-  public MainSources with(SourceUnit unit) {
-    return with(units.with(unit));
-  }
-
   private final JavaRelease release;
-  private final SourceUnits units;
+  private final SourceUnitMap units;
 
-  public MainSources(JavaRelease release, SourceUnits units) {
+  public MainSources(JavaRelease release, SourceUnitMap units) {
     this.release = release;
     this.units = units;
   }
@@ -48,7 +35,36 @@ public final class MainSources {
     return release;
   }
 
-  public SourceUnits units() {
+  public SourceUnitMap units() {
     return units;
+  }
+
+  //
+  // Configuration API
+  //
+
+  @Factory
+  public static MainSources of() {
+    return new MainSources(JavaRelease.ofRuntime(), SourceUnitMap.of());
+  }
+
+  @Factory(Kind.SETTER)
+  public MainSources release(JavaRelease release) {
+    return new MainSources(release, units);
+  }
+
+  @Factory(Kind.SETTER)
+  public MainSources release(int feature) {
+    return release(JavaRelease.of(feature));
+  }
+
+  @Factory(Kind.SETTER)
+  public MainSources units(SourceUnitMap units) {
+    return new MainSources(release, units);
+  }
+
+  @Factory(Kind.OPERATOR)
+  public MainSources with(SourceUnit... moreUnits) {
+    return units(units.with(moreUnits));
   }
 }
