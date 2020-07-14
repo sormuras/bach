fdc7d8f8f8323fd5be07501c75bc67b5e6e8f745
GitHub Action <action@github.com>
2020-07-14 07:35:31 +0000

    Back to version 11-ea

--- src/de.sormuras.bach/main/java/de/sormuras/bach/Bach.java
+++ src/de.sormuras.bach/main/java/de/sormuras/bach/Bach.java
@@ -88,7 +88,7 @@ import java.util.function.UnaryOperator;
 public class Bach {
 
   /** Version of the Java Shell Builder. */
-  public static final Version VERSION = Version.parse("11.4");
+  public static final Version VERSION = Version.parse("11-ea");
 
   /**
    * Main entry-point.
