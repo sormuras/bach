d219dcdf23a114b28c2a814fbbc786df68a81fe9
GitHub Action <action@github.com>
2020-08-05 07:08:54 +0000

    Release Bach.java 11.7

--- src/de.sormuras.bach/main/java/de/sormuras/bach/Bach.java
+++ src/de.sormuras.bach/main/java/de/sormuras/bach/Bach.java
@@ -70,7 +70,7 @@ import java.util.function.UnaryOperator;
 public class Bach {
 
   /** Version of the Java Shell Builder. */
-  public static final Version VERSION = Version.parse("11-ea");
+  public static final Version VERSION = Version.parse("11.7");
 
   /**
    * Main entry-point.
