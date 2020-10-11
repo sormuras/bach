f4581410cb878a1835b4cd1b2ce30b08dae9adff
Christian Stein <sormuras@gmail.com>
2020-10-11 07:13:43 +0200

    Delete workspace on bootstrap

--- .bach/bootstrap/Bootstrap.java
+++ .bach/bootstrap/Bootstrap.java
@@ -3,6 +3,8 @@ import java.lang.module.FindException;
 import java.lang.module.ModuleDescriptor;
 import java.lang.module.ModuleFinder;
 import java.lang.module.ResolutionException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.time.ZoneId;
@@ -16,7 +18,9 @@ class Bootstrap {
   public static void main(String[] args) {
     System.out.println("Bootstrap Bach");
 
-    var bootstrap = Path.of(".bach/workspace/classes/bootstrap");
+    System.out.println("Delete workspace");
+    var workspace = deleteDirectories(Path.of(".bach/workspace"));
+    var bootstrap = workspace.resolve("classes/bootstrap");
     var pattern = DateTimeFormatter.ofPattern("yyyy.MM.dd.HHmm").withZone(ZoneId.of("UTC"));
     var version = ModuleDescriptor.Version.parse(pattern.format(Instant.now()));
 
@@ -33,6 +37,14 @@ class Bootstrap {
         bootstrap.toString());
 
     run(tool(ModuleFinder.of(bootstrap), "build"));
+
+    System.out.println("END.");
+  }
+
+  static void run(ToolProvider tool, String... args) {
+    System.out.println(">> " + tool.name() + " " + String.join(" ", args));
+    var code = tool.run(System.out, System.err, args);
+    if (code != 0) throw new Error("Non-zero exit code: " + code);
   }
 
   static ToolProvider tool(ModuleFinder finder, String name) {
@@ -49,13 +61,25 @@ class Bootstrap {
           .findFirst()
           .orElseThrow();
     } catch (FindException | ResolutionException exception) {
-      throw new Error("Build program failed!", exception);
+      throw new Error("No such tool found: " + name, exception);
     }
   }
 
-  static void run(ToolProvider tool, String... args) {
-    System.out.println(">> " + tool.name() + " " + String.join(" ", args));
-    var code = tool.run(System.out, System.err, args);
-    if (code != 0) throw new Error("Non-zero exit code: " + code);
+  static Path deleteDirectories(Path directory) {
+    try { // trivial case: delete existing empty directory or single file
+      Files.deleteIfExists(directory);
+      return directory;
+    } catch (DirectoryNotEmptyException ignored) {
+      // fall-through
+    } catch (Exception exception) {
+      throw new Error("Delete directories failed: " + directory, exception);
+    }
+    try (var stream = Files.walk(directory)) { // default case: walk the tree...
+      var selected = stream.sorted((p, q) -> -p.compareTo(q));
+      for (var path : selected.toArray(Path[]::new)) Files.deleteIfExists(path);
+    } catch (Exception exception) {
+      throw new Error("Delete directories failed: " + directory, exception);
+    }
+    return directory;
   }
 }
