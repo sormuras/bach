b14a91f81b036404ef8ff5120ba03b6ab00c2e0c
Christian Stein <sormuras@gmail.com>
2020-10-20 06:38:45 +0200

    Add static version accessor

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/Bach.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/Bach.java
@@ -1,4 +1,15 @@
 package com.github.sormuras.bach;
 
 /** Java Shell Builder. */
-public class Bach {}
+public class Bach {
+  /**
+   * Returns the version.
+   *
+   * @return the version or {@code "?"} if the version is unknown at runtime
+   */
+  public static String version() {
+    var descriptor = Bach.class.getModule().getDescriptor();
+    if (descriptor == null) return "?";
+    return descriptor.version().map(Object::toString).orElse("?");
+  }
+}
