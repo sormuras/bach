713a0fce6cdf4c7be01734a46fda9dec11dbdeec
Christian Stein <sormuras@gmail.com>
2020-10-27 10:56:44 +0100

    Extend module-related utilities with declared and required methods

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/Modules.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/Modules.java
@@ -1,11 +1,39 @@
 package com.github.sormuras.bach.internal;
 
+import java.lang.module.ModuleDescriptor;
+import java.lang.module.ModuleDescriptor.Requires;
 import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /** Module-related utilities. */
 public class Modules {
+
+  public static Set<String> declared(ModuleFinder finder) {
+    return declared(finder.findAll().stream().map(ModuleReference::descriptor));
+  }
+
+  public static Set<String> declared(Stream<ModuleDescriptor> descriptors) {
+    return descriptors.map(ModuleDescriptor::name).collect(Collectors.toCollection(TreeSet::new));
+  }
+
+  public static Set<String> required(ModuleFinder finder) {
+    return required(finder.findAll().stream().map(ModuleReference::descriptor));
+  }
+
+  public static Set<String> required(Stream<ModuleDescriptor> descriptors) {
+    return descriptors
+        .map(ModuleDescriptor::requires)
+        .flatMap(Set::stream)
+        .filter(requires -> !requires.modifiers().contains(Requires.Modifier.MANDATED))
+        .filter(requires -> !requires.modifiers().contains(Requires.Modifier.SYNTHETIC))
+        .map(Requires::name)
+        .collect(Collectors.toCollection(TreeSet::new));
+  }
 
   public static ModuleLayer layer(ModuleFinder finder) {
     var boot = ModuleLayer.boot();
