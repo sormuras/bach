3c7a8cf2f2b984fa68b0f537c961f61c5c5b9cbe
Christian Stein <sormuras@gmail.com>
2020-10-31 15:17:34 +0100

    Add more (unused) links

--- .bach/build/module-info.java
+++ .bach/build/module-info.java
@@ -4,7 +4,13 @@ import com.github.sormuras.bach.Project.Link;
 @Project(
     name = "bach",
     java = 16,
-    links = {@Link(module = "junit", target = "junit:junit:4.13.1")})
+    links = {
+      @Link(module = "junit", target = "junit:junit:4.13.1"),
+      @Link(module = "org.junit.jupiter", target = "org.junit.jupiter:junit-jupiter:5.7.0"),
+      @Link(module = "org.junit.jupiter.api", target = "org.junit.jupiter:junit-jupiter-api:5.7.0"),
+      @Link(module = "org.junit.jupiter.engine", target = "org.junit.jupiter:junit-jupiter-engine:5.7.0"),
+      @Link(module = "org.junit.jupiter.params", target = "org.junit.jupiter:junit-jupiter-params:5.7.0"),
+    })
 module build {
   requires com.github.sormuras.bach;
 
