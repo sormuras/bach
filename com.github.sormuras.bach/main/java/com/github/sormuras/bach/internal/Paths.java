c910c48513c2dda9390f50ea666e462f375fbfcf
Christian Stein <sormuras@gmail.com>
2020-10-31 19:02:24 +0100

    Add delete directory tree support

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/Paths.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/Paths.java
@@ -1,7 +1,9 @@
 package com.github.sormuras.bach.internal;
 
+import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.Predicate;
 
 /** Internal {@link Path}-related utilities. */
 public class Paths {
@@ -14,9 +16,37 @@ public class Paths {
     }
   }
 
-  public static boolean deleteIfExists(Path path) {
+  public static Path deleteDirectories(Path directory) {
+    return deleteDirectories(directory, __ -> true);
+  }
+
+  public static Path deleteDirectories(Path directory, Predicate<Path> filter) {
+    // trivial case: delete existing empty directory or single file
     try {
-      return Files.deleteIfExists(path);
+      Files.deleteIfExists(directory);
+      return directory;
+    } catch (DirectoryNotEmptyException ignored) {
+      // fall-through
+    } catch (Exception e) {
+      throw new RuntimeException("Delete directories failed: " + directory, e);
+    }
+    // default case: walk the tree...
+    try (var stream = Files.walk(directory)) {
+      var selected = stream.filter(filter).sorted((p, q) -> -p.compareTo(q));
+      for (var path : selected.toArray(Path[]::new)) Files.deleteIfExists(path);
+    } catch (Exception e) {
+      throw new RuntimeException("Delete directories failed: " + directory, e);
+    }
+    return directory;
+  }
+
+  public static boolean deleteIfExists(Path path) {
+    if (Files.notExists(path)) return false;
+    try {
+      if (Files.isDirectory(path)) {
+        Paths.deleteDirectories(path, __ -> true);
+        return true;
+      } else return Files.deleteIfExists(path);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
