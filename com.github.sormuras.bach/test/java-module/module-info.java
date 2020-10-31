8883636d6b035abc94c896fe1cbafc0af8c00a4e
Christian Stein <sormuras@gmail.com>
2020-10-31 19:02:56 +0100

    Introduce internal tests

---
+++ com.github.sormuras.bach/test/java-module/module-info.java
@@ -0,0 +1,15 @@
+open /*test*/ module com.github.sormuras.bach {
+  exports com.github.sormuras.bach;
+  exports com.github.sormuras.bach.module;
+  exports com.github.sormuras.bach.tool;
+
+  requires transitive java.net.http;
+  requires jdk.compiler;
+  requires jdk.jartool;
+  requires jdk.javadoc;
+  requires jdk.jdeps;
+  requires jdk.jlink;
+  requires org.junit.jupiter;
+
+  uses java.util.spi.ToolProvider;
+}
