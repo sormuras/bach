8c04b6248b014820ed07968db6c1aee7cb5ae492
Christian Stein <sormuras@gmail.com>
2020-11-02 14:35:43 +0100

    Add Bach's main program

---
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/Main.java
@@ -0,0 +1,26 @@
+package com.github.sormuras.bach;
+
+import static java.util.Arrays.copyOfRange;
+
+/** Bach's main program. */
+public class Main {
+  /**
+   * Main entry-point.
+   *
+   * @param args the command line arguments
+   */
+  public static void main(String... args) {
+    if (args.length == 0) {
+      System.out.println("No argument, no action.");
+      return;
+    }
+    switch (args[0]) {
+      case "build" -> ProjectBuilder.build(copyOfRange(args, 1, args.length));
+      case "help", "usage" -> System.out.println("Usage: bach ACTION [ARGS...]");
+      default -> System.out.println("Unsupported action: " + args[0]);
+    }
+  }
+
+  /** Hidden default constructor. */
+  private Main() {}
+}
