6ba699737b1d5a0833e0ccd7f46730eb62eb6800
Christian Stein <sormuras@gmail.com>
2020-11-03 09:52:03 +0100

    Check Bach's default components

--- test.modules/test/java/test/modules/BachTests.java
+++ test.modules/test/java/test/modules/BachTests.java
@@ -2,12 +2,21 @@ package test.modules;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 import com.github.sormuras.bach.Bach;
 import java.lang.module.ModuleDescriptor;
+import java.net.http.HttpClient;
 import org.junit.jupiter.api.Test;
 
 class BachTests {
+  @Test
+  void defaults() {
+    var bach = Bach.ofSystem();
+    assertSame(System.out, bach.printStream());
+    assertEquals(HttpClient.Redirect.NORMAL, bach.httpClient().followRedirects());
+  }
+
   @Test
   void versionIsNotNull() {
     assertNotNull(Bach.version());
