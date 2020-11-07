2c418834261f04c9ec004763c70b0491448c717e
Christian Stein <sormuras@gmail.com>
2020-11-07 13:44:52 +0100

    Add canonical test project

--- test.modules/test/java/test/modules/ProjectBuilderTests.java
+++ test.modules/test/java/test/modules/ProjectBuilderTests.java
@@ -18,6 +18,8 @@ import java.nio.file.Path;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ProjectBuilderTests {
   @Test
@@ -33,7 +35,7 @@ class ProjectBuilderTests {
     assertTrue(project.main().modules().isEmpty());
     assertFalse(project.main().isPresent());
     assertEquals(Runtime.version().feature(), project.main().release());
-    assertEquals(List.of("./*/main/java", "./*/main/java-module"), project.main().moduleSourcePaths());
+    assertEquals(List.of("*/main/java", "*/main/java-module"), project.main().moduleSourcePaths());
     assertEquals(List.of("-encoding", "UTF-8"), project.main().tweaks().get("javac"));
 
     var builder = new ProjectBuilder(bach, project);
@@ -42,5 +44,22 @@ class ProjectBuilderTests {
     assertLinesMatch("""
         Work on Project.+
         """.lines(), baos.toString().lines());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"canonical"})
+  void build(String name) {
+    var baos = new ByteArrayOutputStream();
+    var log = new PrintStream(baos, true, StandardCharsets.UTF_8);
+    var bach = new Bach(log, Http::newClient);
+
+    var base = Base.of("test.projects", name);
+    var project = Project.of(base);
+
+    assertEquals(name, project.name());
+    assertEquals(List.of("foo"), project.main().modules(), project.toString());
+
+    var builder = new ProjectBuilder(bach, project);
+    assertDoesNotThrow(builder::build);
   }
 }
