2ae4e6691bc02b9fb5d216683d9f3942820f6d8a
Christian Stein <sormuras@gmail.com>
2020-11-19 17:34:08 +0100

    Move project instantiation logic into a dedicated factory class

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/Project.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/Project.java
@@ -1,22 +1,11 @@
 package com.github.sormuras.bach;
 
-import com.github.sormuras.bach.internal.Modules;
 import com.github.sormuras.bach.internal.Paths;
-import com.github.sormuras.bach.module.ModuleInfoFinder;
-import com.github.sormuras.bach.module.ModuleInfoReference;
-import com.github.sormuras.bach.module.ModuleLink;
 import com.github.sormuras.bach.project.Library;
 import com.github.sormuras.bach.project.MainSpace;
 import com.github.sormuras.bach.project.TestSpace;
 import java.lang.module.ModuleDescriptor.Version;
-import java.lang.module.ModuleFinder;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.stream.Stream;
 
 /**
@@ -85,7 +74,7 @@ public record Project(String name, Version version, Library library, MainSpace m
   public static Project of() {
     var name = Paths.name(Path.of(""), "unnamed");
     var info = Bach.class.getModule().getAnnotation(ProjectInfo.class);
-    return of(name, info);
+    return new ProjectFactory(name, info).newProject();
   }
 
   /**
@@ -95,113 +84,6 @@ public record Project(String name, Version version, Library library, MainSpace m
    * @return a project model
    */
   public static Project of(ProjectInfo info) {
-    return of(info.name(), info);
-  }
-
-  static Project of(String name, ProjectInfo info) {
-    var version = version(info.version());
-
-    var main = info.main();
-    var mainModuleInfoFinder = mainModuleInfoFinder(main.moduleSourcePaths());
-    var test = info.test();
-    var testModuleInfoFinder = ModuleInfoFinder.of(test.moduleSourcePaths());
-
-    return new Project(
-        name(name),
-        version,
-        library(info, ModuleFinder.compose(mainModuleInfoFinder, testModuleInfoFinder)),
-        new MainSpace(
-            modules(main.modules(), mainModuleInfoFinder),
-            mainModuleInfoFinder.moduleSourcePaths(),
-            List.of(main.modulePaths()),
-            release(main.release()),
-            jarslug(version),
-            main.generateApiDocumentation(),
-            main.generateCustomRuntimeImage(),
-            main.generateApplicationPackage(),
-            tweaks(main.tweaks())),
-        new TestSpace(
-            modules(test.modules(), testModuleInfoFinder),
-            testModuleInfoFinder.moduleSourcePaths(),
-            List.of(test.modulePaths()),
-            tweaks(test.tweaks())));
-  }
-
-  static String name(String name) {
-    return System.getProperty("bach.project.name", name);
-  }
-
-  static Version version(String version) {
-    return Version.parse(System.getProperty("bach.project.version", version));
-  }
-
-  static Library library(ProjectInfo info, ModuleFinder finder) {
-    var requires = new TreeSet<>(List.of(info.library().requires()));
-    requires.addAll(Modules.required(finder));
-    requires.removeAll(Modules.declared(ModuleFinder.ofSystem()));
-    requires.removeAll(Modules.declared(finder));
-    var links = new TreeMap<String, String>();
-    var binding = ProjectInfo.Library.Binding.ofSystem();
-    for (var link : info.library().links()) links.put(link.module(), link(link, binding).uri());
-    return new Library(requires, links);
-  }
-
-  static ModuleLink link(ProjectInfo.Library.Link link, Map<String, String> binding) {
-    var module = link.module();
-    var target = replace(link.target(), binding);
-
-    return switch (link.type()) {
-      case AUTO -> ModuleLink.link(module).to(target);
-      case URI -> ModuleLink.link(module).toUri(target);
-      case MAVEN -> ModuleLink.link(module).toMaven(link.mavenRepository(), target);
-    };
-  }
-
-  static String replace(String template, Map<String, String> binding) {
-    if (template.indexOf('{') < 0) return template;
-    var replaced = template;
-    for (var entry : binding.entrySet()) {
-      var placeholder = entry.getKey();
-      var replacement = entry.getValue();
-      replaced = replaced.replace(placeholder, replacement);
-    }
-    return replaced;
-  }
-
-  static int release(int release) {
-    try {
-      return Integer.parseInt(System.getProperty("bach.project.main.release"));
-    } catch (RuntimeException ignore) {
-      return release != 0 ? release : Runtime.version().feature();
-    }
-  }
-
-  static String jarslug(Version version) {
-    return System.getProperty("bach.project.main.jarslug", version.toString());
-  }
-
-  static ModuleInfoFinder mainModuleInfoFinder(String[] moduleSourcePaths) {
-    // try configured or default module source paths, usually patterns
-    var finder = ModuleInfoFinder.of(moduleSourcePaths);
-    // no module declaration found
-    if (finder.findAll().isEmpty()) {
-      // try single module declaration or what is "simplicissimus"?
-      var info = Path.of("module-info.java");
-      if (Files.exists(info)) return ModuleInfoFinder.of(ModuleInfoReference.of(info));
-      // assume modules are declared in directories named like modules
-      return ModuleInfoFinder.of(".");
-    }
-    return finder;
-  }
-
-  static List<String> modules(String[] modules, ModuleFinder finder) {
-    var all = modules.length == 1 && modules[0].equals("*");
-    return all ? List.copyOf(Modules.declared(finder)) : List.of(modules);
-  }
-
-  static Map<String, List<String>> tweaks(ProjectInfo.Tweak... tweaks) {
-    var map = new HashMap<String, List<String>>();
-    for (var tweak : tweaks) map.put(tweak.tool(), List.of(tweak.args()));
-    return Map.copyOf(map);
+    return new ProjectFactory(info.name(), info).newProject();
   }
 }
