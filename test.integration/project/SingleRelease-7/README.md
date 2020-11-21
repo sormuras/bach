a181adafbbc33300a42a9e88076c9e71cb6750c1
Christian Stein <sormuras@gmail.com>
2020-11-21 07:04:25 +0100

    Add single-release with base 7 integration test project

---
+++ test.integration/project/SingleRelease-7/README.md
@@ -0,0 +1,4 @@
+# Project `SingleRelease-7`
+
+- release: 7
+- module: 9
--- test.integration/test/java/test/project/ProjectBuildTests.java
+++ test.integration/test/java/test/project/ProjectBuildTests.java
@@ -82,6 +82,46 @@ class ProjectBuildTests {
   }
 
   @Test
+  void buildSingleRelease7(@TempDir Path temp) throws Exception {
+    var context = new Context("SingleRelease-7", temp);
+    var output = context.build("-Dbach.project.main.release=7");
+
+    assertLinesMatch(
+        """
+        Build project SingleRelease-7 0-ea
+        Compile main modules
+        >> TOOL CALLS >>
+        Build took .+
+        Logbook written to %s
+        """
+            .formatted(context.workspace("logbook.md").toUri())
+            .lines(),
+        output.lines());
+
+    var foo = context.newModuleFinder().find("foo").orElseThrow();
+    assertEquals("foo", foo.descriptor().name());
+    var path = Path.of(foo.location().orElseThrow());
+    try (var jar = new JarFile(path.toFile())) {
+      assertFalse(jar.isMultiRelease(), "A multi-release JAR file?! -> " + path);
+      var names = new ArrayList<String>();
+      jar.entries().asIterator().forEachRemaining(e -> names.add(e.getName()));
+      assertLinesMatch(
+          """
+          META-INF/
+          META-INF/MANIFEST.MF
+          module-info.class
+          foo/
+          foo/Foo.class
+          """
+              .lines(),
+          names.stream());
+    }
+
+    assertEquals(9, Classes.feature(context.workspace("classes-main/7/foo","module-info.class")));
+    assertEquals(7, Classes.feature(context.workspace("classes-main/7/foo", "foo/Foo.class")));
+  }
+
+  @Test
   void buildSingleRelease8(@TempDir Path temp) throws Exception {
     var context = new Context("SingleRelease-8", temp);
     var output = context.build("-Dbach.project.main.release=8");
