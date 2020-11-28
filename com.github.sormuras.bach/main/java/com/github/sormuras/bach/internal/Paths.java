86b4314b9f9d32a8379e0f61705f3fc358c87fc5
Christian Stein <sormuras@gmail.com>
2020-11-28 21:47:18 +0100

    Update internal Path-related utilities

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/Paths.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/Paths.java
@@ -6,8 +6,11 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayDeque;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.List;
 import java.util.Optional;
 import java.util.TreeSet;
@@ -61,6 +64,12 @@ public class Paths {
       throw new RuntimeException(e);
     }
   }
+  /** Convert path element names of the given unit into a reversed deque. */
+  public static Deque<String> deque(Path path) {
+    var deque = new ArrayDeque<String>();
+    path.forEach(name -> deque.addFirst(name.toString()));
+    return deque;
+  }
 
   /** List content of specified directory in natural order with the given filter applied. */
   public static List<Path> list(Path directory, DirectoryStream.Filter<? super Path> filter) {
@@ -85,6 +94,25 @@ public class Paths {
     }
   }
 
+  /** Walk all trees to find matching paths the given filter starting at given root paths. */
+  public static List<Path> find(Collection<Path> roots, int maxDepth, Predicate<Path> filter) {
+    var paths = new TreeSet<>(Comparator.comparing(Path::toString));
+    for (var root : roots) {
+      try (var stream = Files.walk(root, maxDepth)) {
+        stream.filter(filter).forEach(paths::add);
+      } catch (Exception e) {
+        throw new Error("Walk directory '" + root + "' failed: " + e, e);
+      }
+    }
+    return List.copyOf(paths);
+  }
+
+  public static List<Path> findModuleInfoJavaFiles(Path directory, int limit) {
+    if (isRoot(directory)) throw new IllegalStateException("Root directory: " + directory.toUri());
+    var files = find(List.of(directory), limit, Paths::isModuleInfoJavaFile);
+    return List.copyOf(files);
+  }
+
   /** Test supplied path for pointing to a Java Archive file. */
   public static boolean isJarFile(Path path) {
     return Files.isRegularFile(path) && name(path).endsWith(".jar");
@@ -98,6 +126,11 @@ public class Paths {
   /** Test supplied path for pointing to a Java module declaration compilation unit. */
   public static boolean isModuleInfoJavaFile(Path path) {
     return isJavaFile(path) && name(path).equals("module-info.java");
+  }
+
+  /** Test supplied path for pointing to a Java module declaration for a given space name. */
+  public static boolean isModuleInfoJavaFileForCodeSpace(Path info, String space) {
+    return isModuleInfoJavaFile(info) && Collections.frequency(deque(info), space) == 1;
   }
 
   /** Test for a path pointing to a file system root like {@code /} or {@code C:\}. */
@@ -128,9 +161,7 @@ public class Paths {
     return paths.stream().map(Path::toString).collect(Collectors.joining(File.pathSeparator));
   }
 
-  /**
-   * @return the file name of the path as a string
-   */
+  /** @return the file name of the path as a string */
   public static String name(Path path) {
     return name(path, null);
   }
@@ -146,14 +177,6 @@ public class Paths {
       return Files.size(path);
     } catch (Exception e) {
       throw new Error("Size of file failed: " + e, e);
-    }
-  }
-
-  public static Optional<String> readString(Path path) {
-    try {
-      return Optional.of(Files.readString(path));
-    } catch (Exception exception) {
-      return Optional.empty();
     }
   }
 
