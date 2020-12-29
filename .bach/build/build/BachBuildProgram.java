7481a3670fff9600d127562e37a0eb38d2ac21e7
Christian Stein <sormuras@gmail.com>
2020-12-29 17:51:31 +0100

    Add missing elements to Maven consumer POM
    Closes #192

--- .bach/build/build/BachBuildProgram.java
+++ .bach/build/build/BachBuildProgram.java
@@ -4,6 +4,8 @@ import com.github.sormuras.bach.Bach;
 import com.github.sormuras.bach.BuildProgram;
 import com.github.sormuras.bach.project.Project;
 import com.github.sormuras.bach.project.ProjectInfo;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class BachBuildProgram implements BuildProgram {
 
@@ -29,10 +31,47 @@ public class BachBuildProgram implements BuildProgram {
     System.clearProperty("bach.project.main.jarslug");
 
     new BachBuilder(bach, project).build();
+
+    replaceAll(
+        Path.of(".bach/workspace/deploy/maven/com.github.sormuras.bach.pom.xml"),
+        "  <dependencies>",
+        """
+          <name>Bach</name>
+          <description>🎼 Java Shell Builder</description>
+
+          <url>https://github.com/sormuras/bach</url>
+          <scm>
+            <url>https://github.com/sormuras/bach.git</url>
+          </scm>
+
+          <developers>
+            <developer>
+              <name>Christian Stein</name>
+              <id>sormuras</id>
+            </developer>
+          </developers>
+
+          <licenses>
+            <license>
+              <name>MIT License</name>
+              <url>https://opensource.org/licenses/mit-license</url>
+              <distribution>repo</distribution>
+            </license>
+          </licenses>
+
+          <dependencies>""");
   }
 
   @Override
   public String toString() {
     return "Bach's Build Program";
+  }
+
+  private static void replaceAll(Path path, String regex, String replacement) {
+    try {
+      Files.writeString(path, Files.readString(path).replaceAll(regex, replacement));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }
