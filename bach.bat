454f1ad96d570bc8125fa7033af41f24ba8fe8d6
Christian Stein <sormuras@gmail.com>
2021-01-09 08:14:57 +0100

    Fix local launchers

--- bach.bat
+++ bach.bat
@@ -9,7 +9,7 @@ GOTO END
 :INIT
 del .bach\cache\com.github.sormuras.bach@*.jar >nul 2>&1
 SETLOCAL
-IF [%2]==[] ( SET tag=early-access ) ELSE ( SET tag=%2 )
+IF [%2]==[] ( SET tag=17-ea ) ELSE ( SET tag=%2 )
 jshell -R-Dreboot -R-Dversion=%tag% https://bit.ly/bach-main-init
 ENDLOCAL
 
