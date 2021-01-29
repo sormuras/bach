c4fa2cd829100200d930c8393de6e4f48a41bf7b
Christian Stein <sormuras@gmail.com>
2021-01-29 09:45:44 +0100

    Add project creator support

--- .bach/source/Init.java
+++ .bach/source/Init.java
@@ -1,7 +1,11 @@
+import java.lang.module.ModuleDescriptor.Version;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 @SuppressWarnings("unused")
 public class Init {
@@ -17,11 +21,12 @@ public class Init {
   }
 
   public static void dir(String folder, String glob) {
+    var win = System.getProperty("os.name", "?").toLowerCase(Locale.ROOT).contains("win");
     var directory = Path.of(folder).toAbsolutePath().normalize();
     var paths = new ArrayList<Path>();
     try (var stream = Files.newDirectoryStream(directory, glob)) {
       for (var path : stream) {
-        // if (Files.isHidden(path)) continue;
+        if (win && Files.isHidden(path)) continue;
         paths.add(path);
       }
     } catch (Exception exception) {
@@ -40,15 +45,16 @@ public class Init {
     for (var path : paths) {
       var name = path.getFileName().toString();
       if (Files.isDirectory(path)) out.accept(String.format("%-15s %s", "[+]", name));
-      else try {
-        files++;
-        var size = Files.size(path);
-        bytes += size;
-        out.accept(String.format("%,15d %s", size, name));
-      } catch (Exception exception) {
-        out.accept(exception);
-        return;
-      }
+      else
+        try {
+          files++;
+          var size = Files.size(path);
+          bytes += size;
+          out.accept(String.format("%,15d %s", size, name));
+        } catch (Exception exception) {
+          out.accept(exception);
+          return;
+        }
     }
     var all = paths.size();
     if (all == 0) {
@@ -65,25 +71,32 @@ public class Init {
   }
 
   public static void tree(String folder) {
+    tree(folder, name -> name.contains("module-info"));
+  }
+
+  public static void tree(String folder, Predicate<String> fileNameFilter) {
     var directory = Path.of(folder).toAbsolutePath();
-    var files = tree(directory, "");
-    out.accept("");
+    out.accept(folder.isEmpty() ? directory : folder);
+    var files = tree(directory, "  ", fileNameFilter);
+    out.accept("  ");
     out.accept(String.format("%d file%s in tree of %s", files, files == 1 ? "" : "s", directory));
   }
 
-  private static int tree(Path directory, String indent) {
+  private static int tree(Path directory, String indent, Predicate<String> filter) {
+    var win = System.getProperty("os.name", "?").toLowerCase(Locale.ROOT).contains("win");
     var files = 0;
     try (var stream = Files.newDirectoryStream(directory, "*")) {
       for (var path : stream) {
-        if (Files.isHidden(path)) continue;
+        if (win && Files.isHidden(path)) continue;
         var name = path.getFileName().toString();
         if (Files.isDirectory(path)) {
           out.accept(indent + name + "/");
-          files += tree(path, indent + "  ");
+          if (name.equals(".git")) continue;
+          files += tree(path, indent + "  ", filter);
           continue;
         }
         files++;
-        if (name.contains("module-info")) out.accept(indent + name);
+        if (filter.test(name)) out.accept(indent + name);
       }
     } catch (Exception exception) {
       out.accept(exception);
@@ -91,22 +104,116 @@ public class Init {
     return files;
   }
 
-  public static ProjectTemplate newProject(String name) {
-    return new ProjectTemplate(Path.of(""), name);
+  public static Project project() {
+    return new Project();
   }
 
-  record ProjectTemplate(Path directory, String name) {
+  record Project(Path directory, String name, Version bach) {
 
-    void createProject() throws Exception {
-      out.accept("Create project using " + this);
+    Project() {
+      this(Path.of(""), "noname", Version.parse("17-ea"));
+    }
+
+    Project in(String directory) {
+      return new Project(Path.of(directory), name, bach);
+    }
+
+    Project name(String name) {
+      return new Project(directory, name, bach);
+    }
+
+    Project bach(String version) {
+      return new Project(directory, name, Version.parse(version));
+    }
+
+    void create() throws Exception {
+      out.accept("Create " + this);
       var base = directory.resolve(name);
       if (Files.exists(base)) {
         out.accept("Path already exists: " + base);
         return;
       }
       Files.createDirectories(base);
+      createBuildModule();
+      createCache();
+      createLaunchers();
+      out.accept("");
       out.accept("Created project " + name);
-      tree(base.toString());
+      tree(base.toString(), __ -> true);
+    }
+
+    void createBuildModule() throws Exception {
+      var base = directory.resolve(name);
+      var info = base.resolve(".bach/build/module-info.java");
+      out.accept("Create build module declaration: " + info);
+      Files.createDirectories(info.getParent());
+      Files.writeString(
+          info,
+          """
+              // @ProjectInfo()
+              module build {
+                requires com.github.sormuras.bach;
+                // provides com.github.sormuras.bach.Bach with build.CustomBach;
+              }
+              """);
+    }
+
+    void createCache() throws Exception {
+      var base = directory.resolve(name);
+      var cache = base.resolve(".bach/cache");
+      var module = "com.github.sormuras.bach";
+      var jar = module + '@' + bach + ".jar";
+      var source = "https://github.com/sormuras/bach/releases/download/" + bach + '/' + jar;
+      var target = cache.resolve(jar);
+
+      Files.createDirectories(cache);
+      try (var stream = new URL(source).openStream()) {
+        Files.copy(stream, target);
+      }
+    }
+
+    void createLaunchers() throws Exception {
+      var base = directory.resolve(name);
+
+      var bash = base.resolve("bach");
+      out.accept("Create launcher: " + bash);
+      Files.writeString(
+              bash,
+              """
+              #!/usr/bin/env bash
+
+              if [[ $1 != 'init' ]]; then
+                java --module-path .bach/cache --module com.github.sormuras.bach "$@"
+              else
+                rm -f .bach/cache/com.github.sormuras.bach@*.jar
+                jshell -R-Dreboot -R-Dversion="${2:-17-ea}" https://bit.ly/bach-main-init
+              fi
+              """)
+          .toFile()
+          .setExecutable(true);
+
+      var bat = base.resolve("bach.bat");
+      out.accept("Create launcher: " + bat);
+      Files.writeString(
+          bat,
+          """
+            @ECHO OFF
+
+            IF [%1]==[init] GOTO INIT
+
+            java --module-path .bach\\cache --module com.github.sormuras.bach %*
+
+            GOTO END
+
+            :INIT
+            del .bach\\cache\\com.github.sormuras.bach@*.jar >nul 2>&1
+            SETLOCAL
+            IF [%2]==[] ( SET tag=17-ea ) ELSE ( SET tag=%2 )
+            jshell -R-Dreboot -R-Dversion=%tag% https://bit.ly/bach-main-init
+            ENDLOCAL
+
+            :END
+            """);
     }
   }
 }
