1b65529b53a82c15fe00af5fb78aab0543d3ca30
Christian Stein <sormuras@gmail.com>
2021-02-14 07:36:50 +0100

    Print more helpful messages on init/update

--- .bach/bin/init.java
+++ .bach/bin/init.java
@@ -1,22 +1,28 @@
+import java.io.File;
 import java.lang.module.ModuleFinder;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 /** Bach's init program. */
 class init {
 
-  public static void main(String... args) throws Exception {
-    var version = args.length == 0 ? "17-ea" : args[0];
-    System.out.printf("Initialize Bach %s in %s%n", version, Path.of("").toAbsolutePath());
+  public static Path BIN = Path.of(".bach/bin");
 
-    var bin = Path.of(".bach/bin");
-    if (Files.isDirectory(bin)) {
-      var module = ModuleFinder.of(bin).find("com.github.sormuras.bach");
+  public static void main(String... args) throws Exception {
+    var update = Files.isDirectory(BIN);
+    var action = update ? "Update" : "Initialize";
+    var version = args.length == 0 ? "17-ea" : args[0];
+
+    System.out.printf("%s Bach %s in %s%n", action, version, Path.of("").toAbsolutePath());
+
+    if (update) {
+      var module = ModuleFinder.of(BIN).find("com.github.sormuras.bach");
       if (module.isPresent()) Files.delete(Path.of(module.get().location().orElseThrow()));
     } else {
-      Files.createDirectories(bin);
+      Files.createDirectories(BIN);
     }
 
     loadScript("bach").toFile().setExecutable(true);
@@ -26,7 +32,32 @@ class init {
     loadScript("init.java");
     loadModule("com.github.sormuras.bach", version);
 
-    System.out.printf("%nBach %s initialized.%n", version);
+    checkPath();
+
+    System.out.printf(
+        """
+        %sd Bach %s.
+        
+        bach boot
+          Launch JShell with Bach booted into it.
+        bach build
+          Build the current modular Java project.
+        """, action, version);
+  }
+
+  static void checkPath() {
+    var path = System.getenv("PATH");
+    if (path == null) return;
+    var target = BIN.toString();
+    var elements = List.of(path.split(File.pathSeparator));
+    for (var element : elements) if (element.strip().equals(target)) return;
+
+    System.out.printf(
+        """
+        Append `%s` to the PATH environment variable in order to call
+        Bach's launch script without using that path prefix every time.
+        """,
+        BIN);
   }
 
   static Path loadScript(String name) throws Exception {
@@ -41,7 +72,7 @@ class init {
   }
 
   static Path copy(String uri, String name) throws Exception {
-    var file = Path.of(".bach", "bin", name);
+    var file = BIN.resolve(name);
     try (var stream = new URL(uri).openStream()) {
       var size = Files.copy(stream, file, StandardCopyOption.REPLACE_EXISTING);
       System.out.printf("%,7d %s%n", size, file);
