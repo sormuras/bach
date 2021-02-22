f25c708819edaa09288e26d16bc14e8016936d75
Christian Stein <sormuras@gmail.com>
2021-02-22 15:03:56 +0100

    Add duration to string assertions

--- com.github.sormuras.bach/test/java/com/github/sormuras/bach/internal/StringsTests.java
+++ com.github.sormuras.bach/test/java/com/github/sormuras/bach/internal/StringsTests.java
@@ -3,9 +3,22 @@ package com.github.sormuras.bach.internal;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.lang.module.ModuleDescriptor.Version;
+import java.time.Duration;
 import org.junit.jupiter.api.Test;
 
 class StringsTests {
+
+  @Test
+  void durations() {
+    assertEquals("0s", Strings.toString(Duration.ZERO));
+    assertEquals("1ms", Strings.toString(Duration.ofMillis(1)));
+    assertEquals("999ms", Strings.toString(Duration.ofMillis(999)));
+    assertEquals("1s 1ms", Strings.toString(Duration.ofMillis(1001)));
+    assertEquals("1s", Strings.toString(Duration.ofSeconds(1)));
+    assertEquals("59s", Strings.toString(Duration.ofSeconds(59)));
+    assertEquals("1m", Strings.toString(Duration.ofSeconds(60)));
+    assertEquals("1m 1s", Strings.toString(Duration.ofSeconds(61)));
+  }
 
   @Test
   void toNumberAndPreRelease() {
