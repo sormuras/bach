1a2d2655ada269f0a953eaa01a5494fc82d4ebb0
Christian Stein <sormuras@gmail.com>
2021-02-25 04:43:05 +0100

    Speed up module lookup by consulting explicit functions first

--- .bach/bach.info/module-info.java
+++ .bach/bach.info/module-info.java
@@ -14,11 +14,11 @@ import com.github.sormuras.bach.project.JavaStyle;
       @External(module = "org.hamcrest", via = "org.hamcrest:hamcrest:2.2"),
     },
     lookupExternals = {
-      @Externals(name = Name.GITHUB_RELEASES, version = "*"),
-      @Externals(name = Name.SORMURAS_MODULES, version = "2021.02.24"),
       @Externals(name = Name.JAVAFX, version = "15.0.1"),
       @Externals(name = Name.JUNIT, version = "5.8.0-M1"),
       @Externals(name = Name.LWJGL, version = "3.2.3"),
+      @Externals(name = Name.SORMURAS_MODULES, version = "2021.02.24"),
+      @Externals(name = Name.GITHUB_RELEASES, version = "*"),
     })
 module bach.info {
   requires com.github.sormuras.bach;
