865c45a1c4c931dd114edd5c88d769939891ac7e
Christian Stein <sormuras@gmail.com>
2021-03-07 07:40:07 +0100

    Find first source folder that targets the specified release

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/project/SourceFolders.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/project/SourceFolders.java
@@ -3,16 +3,23 @@ package com.github.sormuras.bach.project;
 import com.github.sormuras.bach.internal.Strings;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 /** A list of source folder objects. */
 public record SourceFolders(List<SourceFolder> list) {
 
-  /** @return the first source folder in the underlying list */
+  /** {@return the first source folder in the underlying list} */
   public SourceFolder first() {
     return list.get(0);
   }
 
-  /** @return a string for {@code javac --module-source-path PATH} in module-specific form */
+  /** {@return the first source folder that targets the specified release} */
+  public Optional<SourceFolder> targets(int release) {
+    for (var folder : list) if (folder.release() == release) return Optional.of(folder);
+    return Optional.empty();
+  }
+
+  /** {@return a string for {@code javac --module-source-path PATH} in module-specific form} */
   public String toModuleSpecificSourcePath() {
     return Strings.join(toModuleSpecificSourcePaths());
   }
