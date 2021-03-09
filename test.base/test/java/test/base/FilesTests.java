19970f0845f1c8c6b4ac018c3e34ea962f77ebcf
Christian Stein <sormuras@gmail.com>
2021-03-09 05:31:53 +0100

    Add path matcher related checks

---
+++ test.base/test/java/test/base/FilesTests.java
@@ -0,0 +1,114 @@
+package test.base;
+
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class FilesTests {
+
+  record AnyPathMatcher(PathMatcher... matchers) implements Predicate<Path> {
+
+    public static AnyPathMatcher of(String... patterns) {
+      return of(Arrays.stream(patterns));
+    }
+
+    public static AnyPathMatcher ofFiles(String named, String... patterns) {
+      return of(Arrays.stream(patterns).map(string -> string + '/' + named));
+    }
+
+    public static AnyPathMatcher of(Stream<String> patterns) {
+      return new AnyPathMatcher(
+          patterns
+              .map(pattern -> pattern.indexOf(':') > 0 ? pattern : "glob:" + pattern)
+              .map(syntaxAndPattern -> FileSystems.getDefault().getPathMatcher(syntaxAndPattern))
+              .toArray(PathMatcher[]::new));
+    }
+
+    @Override
+    public boolean test(Path path) {
+      for (var matcher : matchers) if (matcher.matches(path)) return true;
+      return false;
+    }
+  }
+
+  @Nested
+  class PathMatcherTests {
+
+    @SuppressWarnings("unused")
+    static List<Path> paths() {
+      return List.of(
+          // configuration module
+          Path.of(".bach/bach.info/module-info.java"),
+          // main module
+          Path.of("com.github.sormuras.bach/main/java/module-info.java"),
+          // test modules
+          Path.of("com.github.sormuras.bach/test/java-module/module-info.java"),
+          Path.of("test.base/test/java/module-info.java"),
+          Path.of("test.integration/test/java/module-info.java"),
+          Path.of("test.projects/test/java/module-info.java"),
+          // sample project modules
+          Path.of("test.projects/JigsawQuickStartGreetings/com.greetings/module-info.java"),
+          Path.of("test.projects/JigsawQuickStartWorld/com.greetings/module-info.java"),
+          Path.of("test.projects/JigsawQuickStartWorld/org.astro/module-info.java"),
+          Path.of("test.projects/Simple/.bach/bach.info/module-info.java"),
+          Path.of("test.projects/Simple/module-info.java"),
+          Path.of("test.projects/Simplicissimus/module-info.java"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("paths")
+    void isRegularFile(Path path) {
+      assertTrue(Files.isRegularFile(path), path.toString());
+    }
+
+    @ParameterizedTest
+    @MethodSource("paths")
+    void matchesGlobAny(Path path) {
+      var matcher = path.getFileSystem().getPathMatcher("glob:**/*");
+      assertTrue(matcher.matches(path));
+    }
+
+    @ParameterizedTest
+    @MethodSource("paths")
+    void matchesRegexAny(Path path) {
+      var matcher = path.getFileSystem().getPathMatcher("regex:.*");
+      assertTrue(matcher.matches(path));
+    }
+
+    @Test
+    void findTestModulesOfBachUsingSingleGlobPattern() {
+      var matcher = FileSystems.getDefault().getPathMatcher("glob:**/test/**/module-info.java");
+      assertTestModulesOfBach(matcher::matches);
+    }
+
+    @Test
+    void findTestModulesOfBachUsingMultiPathMatcher() {
+      var matcher = AnyPathMatcher.ofFiles("module-info.java", "*/test/java", "*/test/java-module");
+      assertTestModulesOfBach(matcher);
+    }
+
+    void assertTestModulesOfBach(Predicate<Path> predicate) {
+      assertLinesMatch(
+          """
+          com.github.sormuras.bach/test/java-module/module-info.java
+          test.base/test/java/module-info.java
+          test.integration/test/java/module-info.java
+          test.projects/test/java/module-info.java
+          """
+              .lines(),
+          paths().stream().filter(predicate).map(Path::toString).map(s -> s.replace('\\', '/')));
+    }
+  }
+}
