3d7a3bd117193bfd2dbdb5860504e2bf545df8c5
Christian Stein <sormuras@gmail.com>
2021-04-29 16:36:27 +0200

    Compare programmatically-created and configured project instances

--- test.projects/test/java/test/projects/JigsawQuickStartGreetingsTests.java
+++ test.projects/test/java/test/projects/JigsawQuickStartGreetingsTests.java
@@ -1,44 +1,71 @@
 package test.projects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.github.sormuras.bach.Bach;
 import com.github.sormuras.bach.Logbook;
 import com.github.sormuras.bach.Options;
 import com.github.sormuras.bach.api.Action;
+import com.github.sormuras.bach.api.CodeSpace;
+import com.github.sormuras.bach.api.CodeSpaceMain;
+import com.github.sormuras.bach.api.CodeSpaceTest;
+import com.github.sormuras.bach.api.DeclaredModule;
+import com.github.sormuras.bach.api.DeclaredModuleFinder;
+import com.github.sormuras.bach.api.DeclaredModuleReference;
+import com.github.sormuras.bach.api.Externals;
+import com.github.sormuras.bach.api.Folders;
+import com.github.sormuras.bach.api.ModulePaths;
 import com.github.sormuras.bach.api.Option;
+import com.github.sormuras.bach.api.Project;
+import com.github.sormuras.bach.api.SourceFolder;
+import com.github.sormuras.bach.api.SourceFolders;
+import com.github.sormuras.bach.api.Spaces;
+import com.github.sormuras.bach.api.Tweaks;
+import java.lang.module.ModuleDescriptor;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 
 class JigsawQuickStartGreetingsTests {
 
+  static final String NAME = "JigsawQuickStartGreetings";
+
+  private Project expectedProject() {
+    var version = ModuleDescriptor.Version.parse("0");
+    var folders = Folders.of(Path.of("test.projects", NAME));
+    var greetings = folders.root("com.greetings");
+    var main =
+        new CodeSpaceMain(
+            DeclaredModuleFinder.of(
+                new DeclaredModule(
+                    greetings,
+                    DeclaredModuleReference.of(greetings.resolve("module-info.java")),
+                    SourceFolders.of(SourceFolder.of(greetings)),
+                    SourceFolders.of())),
+            ModulePaths.of(folders.externals()),
+            0,
+            Tweaks.of());
+    var test =
+        new CodeSpaceTest(
+            DeclaredModuleFinder.of(),
+            ModulePaths.of(folders.modules(CodeSpace.MAIN), folders.externals()),
+            Tweaks.of());
+    var spaces = Spaces.of(main, test);
+    var externals = Externals.of();
+    return new Project(NAME, version, folders, spaces, externals);
+  }
+
   @Test
   void build() {
-    var name = "JigsawQuickStartGreetings";
-    var root = Path.of("test.projects", name);
+    var root = Path.of("test.projects", NAME);
     var bach =
         Bach.of(
             Logbook.ofErrorPrinter(),
-            Options.of(name + " Options")
+            Options.of(NAME + " Options")
                 .with(Option.CHROOT, root)
-                // "--strict"
-                // "--limit-tools", "javac,jar"
-                // "--jar-with-sources"
                 .with(Option.VERBOSE)
                 .with(Action.BUILD));
 
-    assertTrue(bach.options().is(Option.VERBOSE));
-    assertEquals(root, bach.project().folders().root());
-    assertEquals(name, bach.project().name());
-
-    var main = bach.project().spaces().main();
-    assertEquals(0, main.release());
-    assertEquals(1, main.modules().size());
-    assertEquals("com.greetings", main.modules().toNames(","));
-    var test = bach.project().spaces().test();
-    assertEquals(0, test.modules().size());
-
+    assertEquals(expectedProject(), bach.project());
     assertEquals(0, bach.run(), bach.logbook().toString());
   }
 }
--- test.projects/test/java/test/projects/JigsawQuickStartWorldWithTestsTests.java
+++ test.projects/test/java/test/projects/JigsawQuickStartWorldWithTestsTests.java
@@ -1,45 +1,85 @@
 package test.projects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.github.sormuras.bach.Bach;
 import com.github.sormuras.bach.Logbook;
 import com.github.sormuras.bach.Options;
 import com.github.sormuras.bach.api.Action;
+import com.github.sormuras.bach.api.CodeSpace;
+import com.github.sormuras.bach.api.CodeSpaceMain;
+import com.github.sormuras.bach.api.CodeSpaceTest;
+import com.github.sormuras.bach.api.DeclaredModule;
+import com.github.sormuras.bach.api.DeclaredModuleFinder;
+import com.github.sormuras.bach.api.DeclaredModuleReference;
+import com.github.sormuras.bach.api.Externals;
+import com.github.sormuras.bach.api.Folders;
+import com.github.sormuras.bach.api.ModulePaths;
 import com.github.sormuras.bach.api.Option;
+import com.github.sormuras.bach.api.Project;
+import com.github.sormuras.bach.api.SourceFolder;
+import com.github.sormuras.bach.api.SourceFolders;
+import com.github.sormuras.bach.api.Spaces;
+import com.github.sormuras.bach.api.Tweaks;
+import java.lang.module.ModuleDescriptor;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 
 class JigsawQuickStartWorldWithTestsTests {
 
+  static final String NAME = "JigsawQuickStartWorldWithTests";
+
+  private Project expectedProject() {
+    var version = ModuleDescriptor.Version.parse("0");
+    var folders = Folders.of(Path.of("test.projects", NAME));
+    var greetings = folders.root("com.greetings");
+    var astro = folders.root("org.astro");
+    var tests = folders.root("test.modules");
+    var main =
+        new CodeSpaceMain(
+            DeclaredModuleFinder.of(
+                new DeclaredModule(
+                    greetings,
+                    DeclaredModuleReference.of(greetings.resolve("main/java/module-info.java")),
+                    SourceFolders.of(SourceFolder.of(greetings.resolve("main/java"))),
+                    SourceFolders.of()),
+                new DeclaredModule(
+                    astro,
+                    DeclaredModuleReference.of(astro.resolve("main/java/module-info.java")),
+                    SourceFolders.of(SourceFolder.of(astro.resolve("main/java"))),
+                    SourceFolders.of()) //
+                ),
+            ModulePaths.of(folders.externals()),
+            0,
+            Tweaks.of());
+    var test =
+        new CodeSpaceTest(
+            DeclaredModuleFinder.of(
+                new DeclaredModule(
+                    tests,
+                    DeclaredModuleReference.of(tests.resolve("test/java/module-info.java")),
+                    SourceFolders.of(SourceFolder.of(tests.resolve("test/java"))),
+                    SourceFolders.of()) //
+                ),
+            ModulePaths.of(folders.modules(CodeSpace.MAIN), folders.externals()),
+            Tweaks.of());
+    var spaces = Spaces.of(main, test);
+    var externals = Externals.of();
+    return new Project(NAME, version, folders, spaces, externals);
+  }
+
   @Test
   void build() {
-    var name = "JigsawQuickStartWorldWithTests";
-    var root = Path.of("test.projects", name);
+    var root = Path.of("test.projects", NAME);
     var bach =
         Bach.of(
             Logbook.ofErrorPrinter(),
-            Options.of(name + " Options")
+            Options.of(NAME + " Options")
                 .with(Option.CHROOT, root)
-                // "--strict"
-                // "--limit-tools", "javac,jar"
-                // "--jar-with-sources"
                 .with(Option.VERBOSE)
                 .with(Action.BUILD));
 
-    assertTrue(bach.options().is(Option.VERBOSE));
-    assertEquals(root, bach.project().folders().root());
-    assertEquals(name, bach.project().name());
-
-    var main = bach.project().spaces().main();
-    assertEquals(0, main.release());
-    assertEquals(2, main.modules().size(), main.modules().toNames(" + "));
-    assertEquals("com.greetings,org.astro", main.modules().toNames(","));
-    var test = bach.project().spaces().test();
-    assertEquals(1, test.modules().size(), test.modules().toNames(" + "));
-    assertEquals("test.modules", test.modules().toNames(","));
-
+    assertEquals(expectedProject(), bach.project());
     assertEquals(0, bach.run(), bach.logbook().toString());
   }
 }
