4334304434d0eef108a74205f80a96b868110b5d
Christian Stein <sormuras@gmail.com>
2021-05-11 04:58:11 +0200

    Implement clean action

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/core/CleanAction.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/core/CleanAction.java
@@ -1,6 +1,7 @@
 package com.github.sormuras.bach.core;
 
 import com.github.sormuras.bach.Bach;
+import com.github.sormuras.bach.internal.Paths;
 
 public class CleanAction extends BachAction {
 
@@ -8,5 +9,9 @@ public class CleanAction extends BachAction {
     super(bach);
   }
 
-  public void clean() {}
+  public void clean() {
+    var workspace = bach().project().folders().workspace();
+    bach().say("Delete Bach's workspace: %s".formatted(workspace));
+    Paths.deleteDirectories(bach().project().folders().workspace());
+  }
 }
