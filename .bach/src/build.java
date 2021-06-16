b0e006634fd9e971dc8846e8b7e5b5caea81a3f5
Christian Stein <sormuras@gmail.com>
2021-06-16 10:07:50 +0200

    Generate Maven consumer POM file

--- .bach/src/build.java
+++ .bach/src/build.java
@@ -27,6 +27,9 @@ import com.github.sormuras.bach.api.Tweak;
 import com.github.sormuras.bach.api.Tweaks;
 import com.github.sormuras.bach.locator.JUnit;
 import com.github.sormuras.bach.workflow.ExecuteTestsWorkflow;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.StringJoiner;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -41,7 +44,9 @@ class build {
   public static void main(String... args) {
     System.out.println("Build with Bach " + Bach.version());
     try {
-      bach(args).buildAndWriteLogbook();
+      var bach = bach(args);
+      bach.buildAndWriteLogbook();
+      MavenConsumerPom.generate(bach);
     } catch (Throwable throwable) {
       System.exit(-1);
     }
@@ -207,6 +212,93 @@ class build {
       public void run() {
         new BufferedReader(new InputStreamReader(stream)).lines().forEach(consumer);
       }
+    }
+  }
+
+  static class MavenConsumerPom {
+
+    static void generate(Bach bach) {
+      var module = "com.github.sormuras.bach";
+      var version = bach.project().version();
+      var jar = bach.project().folders().jar(CodeSpace.MAIN, module, version);
+
+      bach.say("Generate Maven consumer POM file");
+      try {
+        // deploy/maven/
+        var maven = Files.createDirectories(bach.project().folders().workspace("deploy", "maven"));
+        var pom = generate(maven, module, version.toString(), jar);
+        bach.say("  " + pom);
+      } catch (Exception exception) {
+        throw new RuntimeException(exception);
+      }
+    }
+
+    static Path generate(Path maven, String module, String version, Path jar) throws Exception {
+      var pom =
+          """
+          <?xml version="1.0" encoding="UTF-8"?>
+          <project xmlns="http://maven.apache.org/POM/4.0.0"
+                   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                   xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+            <modelVersion>4.0.0</modelVersion>
+  
+            <groupId>com.github.sormuras.bach</groupId>
+            <artifactId>${MODULE}</artifactId>
+            <version>${VERSION}</version>
+  
+            <name>Bach</name>
+            <description>🎼 Java Shell Builder</description>
+  
+            <url>https://github.com/sormuras/bach</url>
+            <scm>
+              <url>https://github.com/sormuras/bach.git</url>
+            </scm>
+  
+            <developers>
+              <developer>
+                <name>Christian Stein</name>
+                <id>sormuras</id>
+              </developer>
+            </developers>
+  
+            <licenses>
+              <license>
+                <name>MIT License</name>
+                <url>https://opensource.org/licenses/mit-license</url>
+                <distribution>repo</distribution>
+              </license>
+            </licenses>
+  
+            <dependencies>
+              <!-- system module: java.base -->
+              <!-- system module: java.net.http -->
+              <!-- system module: jdk.compiler -->
+              <!-- system module: jdk.crypto.ec -->
+              <!-- system module: jdk.jartool -->
+              <!-- system module: jdk.javadoc -->
+              <!-- system module: jdk.jdeps -->
+              <!-- system module: jdk.jlink -->
+            </dependencies>
+          </project>
+          """
+              .replace("${MODULE}", module)
+              .replace("${VERSION}", version);
+      // deploy/maven/${MODULE}.pom.xml
+      var pomFile = Files.writeString(maven.resolve(module + ".pom.xml"), pom);
+      // deploy/maven/empty.zip
+      // https://central.sonatype.org/pages/requirements.html#supply-javadoc-and-sources
+      var emptyZip = maven.resolve("empty.zip");
+      byte[] bytes = {0x50, 0x4B, 0x05, 0x06, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      Files.write(emptyZip, bytes);
+      // deploy/maven/${MODULE}.files
+      var files =
+          new StringJoiner(" ")
+              .add("-DpomFile=" + pomFile)
+              .add("-Dfile=" + jar)
+              .add("-Dsources=" + emptyZip)
+              .add("-Djavadoc=" + emptyZip);
+      Files.writeString(maven.resolve(module + ".files"), files.toString());
+      return pomFile;
     }
   }
 }
