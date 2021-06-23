8f982673dc6d113067b8b1b36a879c77b193e702
Christian Stein <sormuras@gmail.com>
2021-06-23 04:18:02 +0200

    Document `ModuleDescriptor`-related utilities

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/ModuleDescriptors.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/ModuleDescriptors.java
@@ -8,16 +8,26 @@ import com.sun.source.util.TaskEvent;
 import com.sun.source.util.TaskListener;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.module.InvalidModuleDescriptorException;
 import java.lang.module.ModuleDescriptor;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.spi.ToolProvider;
 
+/** {@link ModuleDescriptor}-related utilities. */
 public class ModuleDescriptors {
 
   private static final Map<Path, ModuleDescriptor> DESCRIPTORS = new ConcurrentHashMap<>();
 
+  /**
+   * Reads the source form of a module declaration from a file as a module descriptor.
+   *
+   * @param info the path to a {@code module-info.java} file to parse
+   * @return the module descriptor
+   * @implNote For the time being, only the {@code name} of a module and its {@code requires}
+   *     directives are parsed.
+   */
   public static ModuleDescriptor parse(Path info) {
     if (!Path.of("module-info.java").equals(info.getFileName()))
       throw new IllegalArgumentException("Path must end with 'module-info.java': " + info);
@@ -27,10 +37,12 @@ public class ModuleDescriptors {
     var writer = new PrintWriter(text);
     javac.run(writer, writer, "-proc:only", "-Xplugin:" + Parser.NAME, info.toString());
     var descriptor = DESCRIPTORS.get(info);
-    if (descriptor == null) throw new IllegalStateException("Descriptor not found for: " + info + "\n" + text);
+    if (descriptor == null)
+      throw new InvalidModuleDescriptorException("Descriptor is null: " + info + "\n" + text);
     return descriptor;
   }
 
+  /** A {@code javac} plug-in that transforms module tree nodes into module descriptors. */
   public record Parser() implements Plugin, TaskListener {
 
     public static final String NAME = "ModuleDescriptors.Parser";
