7519f6a6e50cbc5ec67fe9a07819e4df0fea254d
Christian Stein <sormuras@gmail.com>
2021-07-01 15:02:48 +0200

    Parse module's AST directly

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/ModuleDescriptors.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/ModuleDescriptors.java
@@ -1,29 +1,21 @@
 package com.github.sormuras.bach.internal;
 
+import com.sun.source.tree.ModuleTree;
+import com.sun.source.tree.RequiresTree;
 import com.sun.source.util.JavacTask;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UncheckedIOException;
 import java.io.Writer;
 import java.lang.module.ModuleDescriptor;
-import java.lang.module.ModuleFinder;
-import java.lang.module.ModuleReference;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
-import javax.lang.model.element.ModuleElement;
 import javax.tools.SimpleJavaFileObject;
 import javax.tools.ToolProvider;
 
 /** {@link ModuleDescriptor}-related utilities. */
 public class ModuleDescriptors {
-
-  private static final TreeSet<String> SYSTEM_MODULE_NAMES =
-      ModuleFinder.ofSystem().findAll().stream()
-          .map(ModuleReference::descriptor)
-          .map(ModuleDescriptor::name)
-          .collect(Collectors.toCollection(TreeSet::new));
 
   /**
    * Reads the source form of a module declaration from a file as a module descriptor.
@@ -43,13 +35,16 @@ public class ModuleDescriptors {
     var units = List.of(new ModuleInfoFileObject(info));
     var javacTask = (JavacTask) compiler.getTask(writer, fileManager, null, null, null, units);
 
-    var elements = javacTask.getElements();
-    elements.getModuleElement("java.base");
-    for (var module : elements.getAllModuleElements()) {
-      if (SYSTEM_MODULE_NAMES.contains(module.getQualifiedName().toString())) continue;
-      return describe(module);
+    try {
+      for (var tree : javacTask.parse()) {
+        for (var declaration : tree.getTypeDecls()) {
+          if (declaration instanceof ModuleTree module) return describe(module);
+        }
+      }
+    } catch (IOException e) {
+      throw new UncheckedIOException("Parse failed for " + info, e);
     }
-    throw new IllegalArgumentException("Module element not found for " + info);
+    throw new IllegalArgumentException("Module AST not found for " + info);
   }
 
   static class ModuleInfoFileObject extends SimpleJavaFileObject {
@@ -63,16 +58,16 @@ public class ModuleDescriptors {
     }
   }
 
-  static ModuleDescriptor describe(ModuleElement element) {
-    var name = element.getQualifiedName().toString();
-    var module = ModuleDescriptor.newModule(name);
-    for (var directive : element.getDirectives()) {
-      if (directive instanceof ModuleElement.RequiresDirective requires) {
-        var requiresModuleName = requires.getDependency().toString();
-        module.requires(requiresModuleName);
+  static ModuleDescriptor describe(ModuleTree moduleTree) {
+    var moduleName = moduleTree.getName().toString();
+    var builder = ModuleDescriptor.newModule(moduleName);
+    for (var directive : moduleTree.getDirectives()) {
+      if (directive instanceof RequiresTree requires) {
+        var requiresModuleName = requires.getModuleName().toString();
+        builder.requires(requiresModuleName);
       }
     }
-    return module.build();
+    return builder.build();
   }
 
   /** Hidden default constructor. */
