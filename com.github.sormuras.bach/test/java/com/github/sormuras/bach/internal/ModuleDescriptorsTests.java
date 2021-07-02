c483f9d0f884b3d5d5cb53ab3bdfa46587c2a1a9
Christian Stein <sormuras@gmail.com>
2021-07-02 08:32:38 +0200

    Add tests for `module-info.java` parser

--- com.github.sormuras.bach/test/java/com/github/sormuras/bach/internal/ModuleDescriptorsTests.java
+++ com.github.sormuras.bach/test/java/com/github/sormuras/bach/internal/ModuleDescriptorsTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class ModuleDescriptorsTests {
 
   @Test
-  void parse() {
+  void parseBachsMainModule() {
     var path = Path.of("com.github.sormuras.bach/main/java/module-info.java");
     var module = ModuleDescriptors.parse(path);
 
@@ -24,6 +24,73 @@ class ModuleDescriptorsTests {
             .requires("jdk.jdeps")
             .requires("jdk.jfr")
             .requires("jdk.jlink")
+            .build(),
+        module);
+  }
+
+  @Test
+  void parseBachsTestModule() {
+    var path = Path.of("com.github.sormuras.bach/test/java-module/module-info.java");
+    var module = ModuleDescriptors.parse(path);
+
+    assertEquals(
+        ModuleDescriptor.newModule("com.github.sormuras.bach")
+            .requires("java.base")
+            .requires("java.net.http")
+            .requires("jdk.compiler")
+            .requires("jdk.crypto.ec")
+            .requires("jdk.jartool")
+            .requires("jdk.javadoc")
+            .requires("jdk.jdeps")
+            .requires("jdk.jfr")
+            .requires("jdk.jlink")
+            .requires("org.junit.jupiter")
+            .requires("test.base")
+            .build(),
+        module);
+  }
+
+  @Test
+  void parseTestBaseModule() {
+    var path = Path.of("test.base/test/java/module-info.java");
+    var module = ModuleDescriptors.parse(path);
+
+    assertEquals(
+        ModuleDescriptor.newModule("test.base")
+            .requires("java.base")
+            .requires("jdk.httpserver")
+            .requires("jdk.xml.dom")
+            .requires("org.junit.jupiter")
+            .build(),
+        module);
+  }
+
+  @Test
+  void parseTestIntegrationModule() {
+    var path = Path.of("test.integration/test/java/module-info.java");
+    var module = ModuleDescriptors.parse(path);
+
+    assertEquals(
+        ModuleDescriptor.newModule("test.integration")
+            .requires("com.github.sormuras.bach")
+            .requires("java.base")
+            .requires("org.junit.jupiter")
+            .requires("test.base")
+            .build(),
+        module);
+  }
+
+  @Test
+  void parseTestProjectsModule() {
+    var path = Path.of("test.projects/test/java/module-info.java");
+    var module = ModuleDescriptors.parse(path);
+
+    assertEquals(
+        ModuleDescriptor.newModule("test.projects")
+            .requires("com.github.sormuras.bach")
+            .requires("java.base")
+            .requires("org.junit.jupiter")
+            .requires("test.base")
             .build(),
         module);
   }
--- test.integration/test/java/module-info.java
+++ test.integration/test/java/module-info.java
@@ -1,5 +1,6 @@
 open /*test*/ module test.integration {
   requires com.github.sormuras.bach;
+  requires java.base;
   requires org.junit.jupiter;
   requires test.base;
 }
