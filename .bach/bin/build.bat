b3f73686bf6f22e6a05ecd339f6bc2cfc19bd407
Christian Stein <sormuras@gmail.com>
2021-07-02 15:33:16 +0200

    Rethrow exception and pass error level value

--- .bach/bin/build.bat
+++ .bach/bin/build.bat
@@ -1,2 +1,13 @@
+@ECHO OFF
+CLS
+ECHO.
+ECHO Bootstrap...
+ECHO.
 java .bach\src\bootstrap.java
-bach .bach\src\build.java
+IF NOT ERRORLEVEL 0 ( EXIT /B %ERRORLEVEL% )
+
+ECHO.
+ECHO Build Bach with Bach...
+ECHO.
+java --module-path .bach\bin --add-modules ALL-MODULE-PATH .bach\src\build.java
+EXIT /B %ERRORLEVEL%
