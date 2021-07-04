245ef6b251eacfbd66c309d82c1145f87b36194d
Christian Stein <sormuras@gmail.com>
2021-07-04 07:58:24 +0200

    Add dedicated withers

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/call/JavacCall.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/call/JavacCall.java
@@ -1,6 +1,11 @@
 package com.github.sormuras.bach.call;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
+import java.util.function.IntSupplier;
+import java.util.stream.Collectors;
 
 public record JavacCall(List<String> arguments) implements AnyCall<JavacCall> {
 
@@ -17,5 +22,40 @@ public record JavacCall(List<String> arguments) implements AnyCall<JavacCall> {
   public JavacCall arguments(List<String> arguments) {
     if (this.arguments == arguments) return this;
     return new JavacCall(arguments);
+  }
+
+  /** @param directory Specify where to place generated class files */
+  public JavacCall withDirectoryForClasses(Path directory) {
+    return with("-d", directory);
+  }
+
+  public JavacCall withModule(List<String> modules) {
+    return with("--module", String.join(",", modules));
+  }
+
+  public JavacCall withModuleSourcePath(List<String> patterns) {
+    var joined =
+        patterns.stream()
+            .map(pattern -> pattern.replace('/', File.separatorChar))
+            .map(pattern -> pattern.replace('\\', File.separatorChar))
+            .collect(Collectors.joining(File.pathSeparator));
+    return with("--module-source-path", joined);
+  }
+
+  public JavacCall withModuleSourcePath(String module, List<Path> paths) {
+    var joined = paths.stream().map(Path::toString).collect(Collectors.joining(File.pathSeparator));
+    return with("--module-source-path", module + '=' + joined);
+  }
+
+  public JavacCall withModuleSourcePaths(Map<String, List<Path>> map) {
+    return forEach(map.entrySet(), (c, e) -> c.withModuleSourcePath(e.getKey(), e.getValue()));
+  }
+
+  public JavacCall withRelease(IntSupplier release) {
+    return withRelease(release.getAsInt());
+  }
+
+  public JavacCall withRelease(int release) {
+    return with("--release", release);
   }
 }
