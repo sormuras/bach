90420590c2ed968f530d34f4bd81d829da234e8b
Christian Stein <sormuras@gmail.com>
2021-07-07 13:32:59 +0200

    Transmute `Project` into a record

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/Project.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/Project.java
@@ -12,74 +12,62 @@ import java.nio.charset.StandardCharsets;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
-public interface Project {
+public record Project(
+    ProjectName name, ProjectVersion version, ProjectDefaults defaults, ProjectSpaces spaces) {
 
-  ProjectName name();
-
-  ProjectVersion version();
-
-  ProjectDefaults defaults();
-
-  ProjectSpaces spaces();
-
-  default String toNameAndVersion() {
+  public String toNameAndVersion() {
     return "%s %s".formatted(name().value(), version().value().toString());
   }
 
-  static NewProject newProject(String name, String version) {
-    return new NewProject(
+  public static Project of(String name, String version) {
+    return new Project(
         new ProjectName(name),
         new ProjectVersion(ModuleDescriptor.Version.parse(version)),
         new ProjectDefaults(StandardCharsets.UTF_8),
         new ProjectSpaces(new ProjectSpace("main", ""), new ProjectSpace("test", "-test")));
   }
 
-  record NewProject(
-      ProjectName name, ProjectVersion version, ProjectDefaults defaults, ProjectSpaces spaces)
-      implements Project {
+  public Project assertJDK(int feature) {
+    return assertJDK(
+        version -> version.feature() == feature,
+        "Expected JDK %d but runtime version is %s".formatted(feature, Runtime.version()));
+  }
 
-    public NewProject assertJDK(int feature) {
-      return assertJDK(
-          version -> version.feature() == feature,
-          "Expected JDK %d but runtime version is %s".formatted(feature, Runtime.version()));
-    }
+  public Project assertJDK(Predicate<Runtime.Version> predicate, String message) {
+    if (predicate.test(Runtime.version())) return this;
+    throw new AssertionError(message);
+  }
 
-    public NewProject assertJDK(Predicate<Runtime.Version> predicate, String message) {
-      if (predicate.test(Runtime.version())) return this;
-      throw new AssertionError(message);
-    }
+  public Project with(Object component) {
+    RecordComponents.of(Project.class).findUnique(component.getClass()).orElseThrow();
+    return new Project(
+        component instanceof ProjectName name ? name : name,
+        component instanceof ProjectVersion version ? version : version,
+        component instanceof ProjectDefaults defaults ? defaults : defaults,
+        component instanceof ProjectSpaces spaces ? spaces : spaces);
+  }
 
-    public NewProject with(Object component) {
-      RecordComponents.of(NewProject.class).findUnique(component.getClass()).orElseThrow();
-      return new NewProject(
-          component instanceof ProjectName name ? name : name,
-          component instanceof ProjectVersion version ? version : version,
-          component instanceof ProjectDefaults defaults ? defaults : defaults,
-          component instanceof ProjectSpaces spaces ? spaces : spaces);
-    }
+  public Project withName(String name) {
+    return with(new ProjectName(name));
+  }
 
-    public NewProject withName(String name) {
-      return with(new ProjectName(name));
-    }
+  public Project withVersion(String version) {
+    return with(new ProjectVersion(ModuleDescriptor.Version.parse(version)));
+  }
 
-    public NewProject withVersion(String version) {
-      return with(new ProjectVersion(ModuleDescriptor.Version.parse(version)));
-    }
+  public Project withDefaultSourceFileEncoding(String encoding) {
+    return withDefaultSourceFileEncoding(Charset.forName(encoding));
+  }
 
-    public NewProject withDefaultSourceFileEncoding(String encoding) {
-      return withDefaultSourceFileEncoding(Charset.forName(encoding));
-    }
+  public Project withDefaultSourceFileEncoding(Charset encoding) {
+    return with(new ProjectDefaults(encoding));
+  }
 
-    public NewProject withDefaultSourceFileEncoding(Charset encoding) {
-      return with(new ProjectDefaults(encoding));
-    }
+  public Project withMainProjectSpace(UnaryOperator<ProjectSpace> operator) {
+    return with(new ProjectSpaces(operator.apply(spaces.main()), spaces.test()));
+  }
 
-    public NewProject withMainProjectSpace(UnaryOperator<ProjectSpace> operator) {
-      return with(new ProjectSpaces(operator.apply(spaces.main()), spaces.test()));
-    }
-
-    public NewProject withTestProjectSpace(UnaryOperator<ProjectSpace> operator) {
-      return with(new ProjectSpaces(spaces.main(), operator.apply(spaces.test())));
-    }
+  public Project withTestProjectSpace(UnaryOperator<ProjectSpace> operator) {
+    return with(new ProjectSpaces(spaces.main(), operator.apply(spaces.test())));
   }
 }
