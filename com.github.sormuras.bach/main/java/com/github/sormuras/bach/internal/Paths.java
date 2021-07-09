dbf2a64c37e3309855f9c301ba44d497940ab289
Christian Stein <sormuras@gmail.com>
2021-07-09 08:37:23 +0200

    Introduce DeleteDirectoriesCall

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/Paths.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/Paths.java
@@ -2,13 +2,16 @@ package com.github.sormuras.bach.internal;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.math.BigInteger;
+import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.DigestOutputStream;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
@@ -16,6 +19,26 @@ import java.util.function.Predicate;
 
 /** {@link Path}-related utilities. */
 public class Paths {
+
+  public static Path deleteDirectories(Path directory) throws IOException {
+    return deleteDirectories(directory, __ -> true);
+  }
+
+  public static Path deleteDirectories(Path directory, Predicate<Path> filter) throws IOException {
+    // trivial case: delete existing empty directory or single file
+    try {
+      Files.deleteIfExists(directory);
+      return directory;
+    } catch (DirectoryNotEmptyException ignored) {
+      // fall-through
+    }
+    // default case: walk the tree...
+    try (var stream = Files.walk(directory)) {
+      var selected = stream.filter(filter).sorted((p, q) -> -p.compareTo(q));
+      for (var path : selected.toArray(Path[]::new)) Files.deleteIfExists(path);
+    }
+    return directory;
+  }
 
   /** Test supplied path for pointing to a Java Archive file. */
   public static boolean isJarFile(Path path) {
@@ -39,7 +62,7 @@ public class Paths {
 
   /** Walk all trees to find matching paths the given filter starting at given root path. */
   public static List<Path> find(List<Path> roots, int maxDepth, Predicate<Path> filter)
-      throws Exception {
+      throws IOException {
     var paths = new TreeSet<>(Comparator.comparing(Path::toString));
     for (var root : roots) {
       try (var stream = Files.walk(root, maxDepth)) {
@@ -50,19 +73,19 @@ public class Paths {
   }
 
   /** {@return a listing of the directory in natural order with the given filter applied} */
-  public static List<Path> list(Path directory, DirectoryStream.Filter<? super Path> filter) {
+  public static List<Path> list(Path directory, DirectoryStream.Filter<? super Path> filter)
+      throws IOException {
     if (Files.notExists(directory)) return List.of();
     var paths = new TreeSet<>(Comparator.comparing(Path::toString));
     try (var stream = Files.newDirectoryStream(directory, filter)) {
       stream.forEach(paths::add);
-    } catch (Exception e) {
-      throw new RuntimeException("Stream directory '" + directory + "' failed: " + e, e);
     }
     return List.copyOf(paths);
   }
 
   /** {@return the message digest of the given file as a hexadecimal string} */
-  public static String hash(Path file, String algorithm) throws Exception {
+  public static String hash(Path file, String algorithm)
+      throws IOException, NoSuchAlgorithmException {
     var md = MessageDigest.getInstance(algorithm);
     try (var in = new BufferedInputStream(new FileInputStream(file.toFile()));
         var out = new DigestOutputStream(OutputStream.nullOutputStream(), md)) {
