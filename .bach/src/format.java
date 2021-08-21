89116272f5ffda1f9c2c73199e6293c1aaba530c
Christian Stein <sormuras@gmail.com>
2021-08-21 18:12:37 +0200

    Introduce Bach's format program

---
+++ .bach/src/format.java
@@ -0,0 +1,9 @@
+class format {
+  public static void main(String... args) {
+    try (var bach = new com.github.sormuras.bach.Bach(args)) {
+      var files = bach.explorer().findJavaFiles();
+      bach.log("CAPTION:Format %d .java files".formatted(files.size()));
+      bach.run("format@1.11.0", format -> format.with("--replace").withAll(files));
+    }
+  }
+}
