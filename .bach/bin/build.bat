e6b83c7aaa4bab7b95f6571feef9975b7166ac4e
Christian Stein <sormuras@gmail.com>
2021-08-22 06:43:41 +0200

    Pass command-line arguments through to the build program

--- .bach/bin/build.bat
+++ .bach/bin/build.bat
@@ -9,5 +9,5 @@ IF NOT ERRORLEVEL 0 ( EXIT /B %ERRORLEVEL% )
 ECHO.
 ECHO Build Bach with Bach...
 ECHO.
-java --module-path .bach\bin --add-modules ALL-MODULE-PATH .bach\src\build.java
+java --module-path .bach\bin --add-modules ALL-MODULE-PATH .bach\src\build.java %*
 EXIT /B %ERRORLEVEL%
