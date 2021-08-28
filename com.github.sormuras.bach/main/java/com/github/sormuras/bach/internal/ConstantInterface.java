4818d29cf6640e5c3cadc2816bf18af68ad1efed
Christian Stein <sormuras@gmail.com>
2021-08-28 04:11:12 +0200

    Add internal module finder support

--- com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/ConstantInterface.java
+++ com.github.sormuras.bach/main/java/com/github/sormuras/bach/internal/ConstantInterface.java
@@ -3,6 +3,7 @@ package com.github.sormuras.bach.internal;
 record ConstantInterface()
     implements DurationSupport,
         ModuleDescriptorSupport,
+        ModuleFinderSupport,
         ModuleSupport,
         PathSupport,
         StringSupport,
