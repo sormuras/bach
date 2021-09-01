07ff0058bf60d6adfa7bf0cc025bc0dd0c6cfcf1
Christian Stein <sormuras@gmail.com>
2021-09-01 10:39:09 +0200

    Show execution of native executables via `ToolCall.process()`
    Addresses part 2. of issue #201

---
+++ .bach/src/jarsign.java
@@ -0,0 +1,10 @@
+import com.github.sormuras.bach.ToolCall;
+
+class jarsign {
+  public static void main(String... args) {
+    try (var bach = new com.github.sormuras.bach.Bach(args)) {
+      var jarsigner = bach.path().javaExecutable().resolveSibling("jarsigner");
+      bach.run(ToolCall.process(jarsigner).with("--help")).visit(bach.printer()::print);
+    }
+  }
+}
