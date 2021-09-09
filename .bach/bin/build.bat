3691897df06a1cbb635594827a3b0a5f6fb34e16
Christian Stein <sormuras@gmail.com>
2021-09-09 08:15:01 +0200

    Auto-format on build

--- .bach/bin/build.bat
+++ .bach/bin/build.bat
@@ -7,6 +7,12 @@ java .bach\src\bootstrap.java
 IF NOT ERRORLEVEL 0 ( EXIT /B %ERRORLEVEL% )
 
 ECHO.
+ECHO Format...
+ECHO.
+java --module-path .bach\bin --add-modules ALL-MODULE-PATH .bach\src\format.java
+IF NOT ERRORLEVEL 0 ( EXIT /B %ERRORLEVEL% )
+
+ECHO.
 ECHO Build Bach with Bach...
 ECHO.
 java --module-path .bach\bin --add-modules ALL-MODULE-PATH .bach\src\build.java %*
