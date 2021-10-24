b9dbe9446cee6f21dcf137de6084e52669c76f10
Christian Stein <sormuras@gmail.com>
2021-10-24 18:15:10 +0200

    Upgrade Google Java Format to 1.12.0

--- .bach/src/format.java
+++ .bach/src/format.java
@@ -1,13 +1,13 @@
 class format {
   public static void main(String... args) {
     try (var bach = new com.github.sormuras.bach.Bach(args)) {
-      if (bach.configuration().tooling().finder().find("format@1.11.0").isEmpty()) {
+      if (bach.configuration().tooling().finder().find("format@1.12.0").isEmpty()) {
         bach.logCaption("Grab external tools");
         bach.run("grab", grab -> grab.add(bach.path().root(".bach", "external.properties")));
       }
       var files = bach.explorer().findJavaFiles();
       bach.logCaption("Format %d .java files".formatted(files.size()));
-      bach.run("format@1.11.0", format -> format.add("--replace").addAll(files));
+      bach.run("format@1.12.0", format -> format.add("--replace").addAll(files));
     }
   }
 }
