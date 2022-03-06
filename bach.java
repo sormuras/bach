193c3a00560fc85f07f3cb309d337a903c4f7e40
Christian Stein <sormuras@gmail.com>
2022-03-06 17:50:33 +0100

    Create bach.java

---
+++ bach.java
@@ -0,0 +1,6 @@
+class bach {
+  public static void main(String... args) {
+    String name = args.length == 0 ? System.getProperty("user.name") : String.join(" ", args);
+    System.out.printf("Hello %s!%n", name);
+  }
+}
