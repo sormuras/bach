88e0211a6a430d5fc62f262d0a9366c179c9e99f
Christian Stein <sormuras@gmail.com>
2022-03-22 06:07:47 +0100

    Add custom container finished feed

--- test/BachTests.java
+++ test/BachTests.java
@@ -2,14 +2,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Path;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 class BachTests {
+  @RepeatedTest(2)
+  void repeated() throws Exception {
+    Thread.sleep(1000);
+  }
+
   @Test
   void test(@TempDir Path temp) {
     var bach = Bach.of(Bach.Printer.ofSilent(), "--chroot", temp.toString());
     assertEquals(temp, bach.paths().root());
     assertTrue(bach.printer().lines().isEmpty());
+  }
+
+  @RepeatedTest(3)
+  void sleeping() throws Exception {
+    Thread.sleep(1000);
   }
 }
