a3311650a32da7c552178c528460953c5efe4d49
Christian Stein <sormuras@gmail.com>
2022-03-25 18:30:57 +0100

    Add `directories` tool

--- src/Bach.java
+++ src/Bach.java
@@ -90,6 +90,7 @@ public record Bach(
                     Tool.of("build", Tool::build),
                     Tool.of("checksum", Tool::checksum),
                     Tool.of("compile", Tool::compile),
+                    new DirectoriesToolProvider(),
                     Tool.of("download", Tool::download),
                     Tool.of("info", Tool::info),
                     Tool.of("test", Tool::test)),
@@ -895,6 +896,52 @@ public record Bach(
 
     public ToolNotFoundException(String name) {
       super("Tool named `%s` not found".formatted(name));
+    }
+  }
+
+  public record DirectoriesToolProvider() implements ToolProvider {
+
+    private enum Mode {
+      CREATE,
+      CLEAN,
+      DELETE
+    }
+
+    @Override
+    public String name() {
+      return "directories";
+    }
+
+    @Override
+    public int run(PrintWriter out, PrintWriter err, String... args) {
+      if (args.length != 2) {
+        out.println("""
+          Usage: directories [create|clean|delete] PATH
+          """);
+        return 1;
+      }
+      try {
+        var mode = Mode.valueOf(args[0].toUpperCase());
+        var path = Path.of(args[1]);
+        if (mode == Mode.DELETE || mode == Mode.CLEAN) deleteDirectories(path);
+        if (mode == Mode.CREATE || mode == Mode.CLEAN) createDirectories(path);
+      } catch (Exception exception) {
+        exception.printStackTrace(err);
+        return 2;
+      }
+      return 0;
+    }
+
+    static void createDirectories(Path root) throws Exception {
+      Files.createDirectories(root);
+    }
+
+    static void deleteDirectories(Path root) throws Exception {
+      if (Files.notExists(root)) return;
+      try (var stream = Files.walk(root)) {
+        var paths = stream.sorted((p, q) -> -p.compareTo(q));
+        for (var path : paths.toArray(Path[]::new)) Files.deleteIfExists(path);
+      }
     }
   }
 
