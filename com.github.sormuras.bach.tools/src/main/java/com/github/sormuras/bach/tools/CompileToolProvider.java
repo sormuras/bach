c55eeaf09911bb92bf9406d8ffc573f951f7dd09
Christian Stein <sormuras@gmail.com>
2022-03-26 04:57:45 +0100

    Rename `compile` tool provider

--- com.github.sormuras.bach.tools/src/main/java/com/github/sormuras/bach/tools/CompileToolProvider.java
+++ com.github.sormuras.bach.tools/src/main/java/com/github/sormuras/bach/tools/CompileToolProvider.java
@@ -1,16 +1,18 @@
 package com.github.sormuras.bach.tools;
 
+import com.github.sormuras.bach.project.Project;
 import java.io.PrintWriter;
 import java.util.spi.ToolProvider;
 
-public record Compiler() implements ToolProvider {
+public record CompileToolProvider() implements ToolProvider {
   @Override
   public String name() {
-    return "bach-tool-compile";
+    return "compile";
   }
 
   @Override
   public int run(PrintWriter out, PrintWriter err, String... args) {
+    out.printf("Compiling %s...%n", new Project());
     return 0;
   }
 }
--- com.github.sormuras.bach.tools/src/main/java/module-info.java
+++ com.github.sormuras.bach.tools/src/main/java/module-info.java
@@ -1,6 +1,9 @@
+import com.github.sormuras.bach.tools.CompileToolProvider;
+import java.util.spi.ToolProvider;
+
 module com.github.sormuras.bach.tools {
   requires com.github.sormuras.bach.project;
 
-  provides java.util.spi.ToolProvider with
-      com.github.sormuras.bach.tools.Compiler;
+  provides ToolProvider with
+      CompileToolProvider;
 }
