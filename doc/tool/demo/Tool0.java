96df843f8f3df82303c6ed6a550d82e7d9e32436
Christian Stein <sormuras@gmail.com>
2022-04-10 22:36:20 +0200

    Reset setup of step zero

--- doc/tool/demo/Tool0.java
+++ doc/tool/demo/Tool0.java
@@ -3,6 +3,6 @@ import java.util.spi.ToolProvider;
 /** Use tool provider to find and run javac tool. */
 class Tool0 {
   public static void main(String... args) {
-    ToolProvider.findFirst("javac").orElseThrow().run(System.out, System.err, args);
+    ToolProvider.findFirst("javac").orElseThrow().run(System.out, System.err, "--version");
   }
 }
