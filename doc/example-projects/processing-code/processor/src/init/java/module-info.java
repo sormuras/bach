6c5463dd70dc2d033facbab10c7d6a13435c4fcb
Christian Stein <sormuras@gmail.com>
2022-04-13 18:12:23 +0200

    Add `processing-code` example project

---
+++ doc/example-projects/processing-code/processor/src/init/java/module-info.java
@@ -0,0 +1,10 @@
+/** Defines classes and interfaces of the Processor API. */
+module processor {
+  requires java.compiler;
+  requires jdk.javadoc;
+
+  provides com.sun.source.util.Plugin with
+      processor.ShowPlugin;
+  provides javax.annotation.processing.Processor with
+      processor.ShowProcessor;
+}
---
+++ doc/example-projects/processing-code/production/src/main/java/production/Main.java
@@ -0,0 +1,5 @@
+package production;
+
+class Main {
+  public static void main(String... args) {}
+}
