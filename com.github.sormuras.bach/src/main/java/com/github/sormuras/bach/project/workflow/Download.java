b49680aab3a2b8650a3afe6ea1b77579acae4c39
Christian Stein <sormuras@gmail.com>
2022-04-28 10:33:37 +0200

    Activate workflow operators

--- com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/project/workflow/Download.java
+++ com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/project/workflow/Download.java
@@ -1,37 +1,34 @@
 package com.github.sormuras.bach.project.workflow;
 
 import com.github.sormuras.bach.Bach;
+import com.github.sormuras.bach.ToolCall;
 import com.github.sormuras.bach.ToolOperator;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Download implements ToolOperator {
   @Override
   public int run(Bach bach, PrintWriter out, PrintWriter err, String... args) {
-    /*
-    var calls = new ArrayList<Tool.Call>();
-    for (var external : bach.configuration.project.tools.externals) {
-        var directory = bach.configuration.paths.root(".bach", "external-tools", external.name);
-        if (external.from.isPresent()) {
-            var source = external.from.get();
-            var begin = source.lastIndexOf('/') + 1;
-            var end = source.indexOf('#', begin);
-            var name = source.substring(begin, end != -1 ? end : source.length());
-            var target = directory.resolve(name);
-            calls.add(Tool.Call.of("load-and-verify", target, source));
-        }
-        for (var asset : external.assets) {
-            var target = directory.resolve(asset.name);
-            var source = asset.from;
-            var call = Tool.Call.of("load-and-verify", target, source);
-            if (source.startsWith("string:")) {
-                var size = source.getBytes(StandardCharsets.UTF_8).length - 7;
-                call = call.with("SIZE=" + size);
-            }
-            calls.add(call);
-        }
+
+    var calls = new ArrayList<ToolCall>();
+    for (var external : bach.configuration().project().tools().externals()) {
+      var directory = bach.configuration().paths().root(".bach", "external-tools", external.name());
+      if (external.from().isPresent()) {
+        var source = external.from().get();
+        var begin = source.lastIndexOf('/') + 1;
+        var end = source.indexOf('#', begin);
+        var name = source.substring(begin, end != -1 ? end : source.length());
+        var target = directory.resolve(name);
+        calls.add(ToolCall.of("load-and-verify", target, source));
+      }
+      for (var asset : external.assets()) {
+        var target = directory.resolve(asset.name());
+        var source = asset.from();
+        calls.add(ToolCall.of("load-and-verify", target, source));
+      }
     }
     calls.forEach(bach::run);
-    */
+
     return 0;
   }
 }
--- com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/project/workflow/Launch.java
+++ com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/project/workflow/Launch.java
@@ -1,27 +1,26 @@
 package com.github.sormuras.bach.project.workflow;
 
 import com.github.sormuras.bach.Bach;
+import com.github.sormuras.bach.ToolCall;
 import com.github.sormuras.bach.ToolOperator;
 import java.io.PrintWriter;
+import java.util.stream.Stream;
 
 public class Launch implements ToolOperator {
   @Override
   public int run(Bach bach, PrintWriter out, PrintWriter err, String... args) {
-    /*
-    var launcher = bach.configuration.project.spaces().main().launcher();
-    var paths = bach.configuration.paths;
+    var launcher = bach.configuration().project().spaces().main().launcher();
+    var paths = bach.configuration().paths();
     if (launcher.isEmpty()) {
       err.println("No launcher defined. No start.");
       return 1;
     }
-
     var java =
-        Tool.Call.of("java")
+        ToolCall.of("java")
             .with("--module-path", paths.out("main", "modules"))
             .with("--module", launcher.get())
             .with(Stream.of(args));
     bach.run(java);
-    */
     return 0;
   }
 }
