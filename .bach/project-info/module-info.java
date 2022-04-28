d9be9a76eacbaa2339a280d1c464f22ff155d245
Christian Stein <sormuras@gmail.com>
2022-04-28 09:03:02 +0200

    No imports!

--- .bach/project-info/module-info.java
+++ .bach/project-info/module-info.java
@@ -1,11 +1,8 @@
-import com.github.sormuras.bach.project.Project;
-import project.Configurator;
-
 module project {
   requires com.github.sormuras.bach;
 
-  provides Project.Configurator with
-      Configurator;
+  provides com.github.sormuras.bach.project.Project.Configurator with
+      project.Configurator;
   provides java.util.spi.ToolProvider with
       project.Hello;
 }
