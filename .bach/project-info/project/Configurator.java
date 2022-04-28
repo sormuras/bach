50b80087cdb244868bc542d152ebf620e168fbb6
Christian Stein <sormuras@gmail.com>
2022-04-28 10:32:44 +0200

    Use dedicated wither

--- .bach/project-info/project/Configurator.java
+++ .bach/project-info/project/Configurator.java
@@ -5,10 +5,6 @@ import com.github.sormuras.bach.project.Project;
 public class Configurator implements Project.Configurator {
   @Override
   public Project configure(Project project) {
-    return project.withParsingArguments(
-        """
-        --project-version
-          17-M5
-        """.lines().toList());
+    return project.withVersion("17-M5");
   }
 }
