769acff14ccda41fd35f22ee5c08b23567aedb83
Christian Stein <sormuras@gmail.com>
2022-05-02 18:22:08 +0200

    Fix error level handling on Windows

--- .bach/bin/bach.bat
+++ .bach/bin/bach.bat
@@ -3,12 +3,12 @@
 IF "%~1" == "boot" (
   REM SHIFT 1
   jshell --module-path .bach\bin --add-modules ALL-MODULE-PATH .bach\bin\bach.jshell %2 %3 %4 %5 %6 %7 %8 %9
-  EXIT /B %ERRORLEVEL%
+  EXIT /B
 )
 
 IF "%~1" == "clean" (
   IF EXIST .bach\out rmdir /S /Q .bach\out
-  EXIT /B %ERRORLEVEL%
+  EXIT /B
 )
 
 IF "%~1" == "init" (
@@ -17,20 +17,20 @@ IF "%~1" == "init" (
     EXIT /B 1
   )
   jshell -R-Dbach-version=%2 https://git.io/bach-init
-  EXIT /B %ERRORLEVEL%
+  EXIT /B
 )
 
 IF EXIST ".bach\src\%~1.java" (
   REM PROGRAM=
   REM SHIFT 1
   java --module-path .bach\bin --add-modules ALL-MODULE-PATH ".bach\src\%~1.java" %2 %3 %4 %5 %6 %7 %8 %9
-  EXIT /B %ERRORLEVEL%
+  EXIT /B
 )
 
 IF EXIST "%~1" (
   java --module-path .bach\bin --add-modules ALL-MODULE-PATH %*
-  EXIT /B %ERRORLEVEL%
+  EXIT /B
 )
 
 java --module-path .bach\bin --module com.github.sormuras.bach %*
-EXIT /B %ERRORLEVEL%
+EXIT /B
--- .bach/bin/build.bat
+++ .bach/bin/build.bat
@@ -4,16 +4,16 @@ ECHO.
 ECHO Bootstrap...
 ECHO.
 java .bach\src\bootstrap.java
-IF NOT ERRORLEVEL 0 ( EXIT /B %ERRORLEVEL% )
+IF %ERRORLEVEL% NEQ 0 EXIT /B
 
 ECHO.
 ECHO Format...
 ECHO.
 java --module-path .bach\bin --add-modules ALL-MODULE-PATH .bach\src\format.java
-IF NOT ERRORLEVEL 0 ( EXIT /B %ERRORLEVEL% )
+IF %ERRORLEVEL% NEQ 0 EXIT /B
 
 ECHO.
 ECHO Build Bach with Bach...
 ECHO.
 java --module-path .bach\bin --add-modules ALL-MODULE-PATH .bach\src\build.java %*
-EXIT /B %ERRORLEVEL%
+EXIT /B
