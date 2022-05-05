0fd0e6e53e4b0a02d4527f9eeed671f8de2e60ae
Christian Stein <sormuras@gmail.com>
2022-05-05 07:20:57 +0200

    Update project configurator

--- .bach/project-info/project/Configurator.java
+++ .bach/project-info/project/Configurator.java
@@ -1,10 +1,14 @@
 package project;
 
+import com.github.sormuras.bach.Main;
 import com.github.sormuras.bach.project.Project;
 
 public class Configurator implements Project.Configurator {
   @Override
   public Project apply(Project project) {
-    return project.withVersion("17-M5");
+    return project
+        .withTargetsJava(17)
+        .withTargetsJava(project.spaces().test(), Runtime.version().feature())
+        .withLauncher(Main.class.getCanonicalName());
   }
 }
