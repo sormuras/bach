6a186f9cf7d432eb80d7d81d3437c4b397f84e3f
Christian Stein <sormuras@gmail.com>
2022-05-09 18:26:25 +0200

    Fix launch scripts

--- .bach/bin/bach.bat
+++ .bach/bin/bach.bat
@@ -2,7 +2,7 @@
 
 IF "%~1" == "boot" (
   REM SHIFT 1
-  jshell --module-path .bach\bin --add-modules ALL-DEFAULT .bach\bin\bach.jshell %2 %3 %4 %5 %6 %7 %8 %9
+  jshell --module-path .bach\bin --add-modules ALL-MODULE-PATH,ALL-SYSTEM %~dp0\bach.jshell %2 %3 %4 %5 %6 %7 %8 %9
   EXIT /B
 )
 
@@ -20,17 +20,19 @@ IF "%~1" == "init" (
   EXIT /B
 )
 
+SET JAVA_LAUNCHER_ARGUMENTS=--module-path .bach\bin --add-modules=ALL-MODULE-PATH --add-modules ALL-DEFAULT
+
 IF EXIST ".bach\src\%~1.java" (
   REM PROGRAM=
   REM SHIFT 1
-  java --module-path .bach\bin --add-modules ALL-DEFAULT ".bach\src\%~1.java" %2 %3 %4 %5 %6 %7 %8 %9
+  java %JAVA_LAUNCHER_ARGUMENTS% ".bach\src\%~1.java" %2 %3 %4 %5 %6 %7 %8 %9
   EXIT /B
 )
 
 IF EXIST "%~1" (
-  java --module-path .bach\bin --add-modules ALL-DEFAULT %*
+  java %JAVA_LAUNCHER_ARGUMENTS% %*
   EXIT /B
 )
 
-java --module-path .bach\bin --add-modules ALL-DEFAULT --module com.github.sormuras.bach %*
+java %JAVA_LAUNCHER_ARGUMENTS% --module com.github.sormuras.bach %*
 EXIT /B
