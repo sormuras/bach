9914c3c0a741808dc4661b82adcdb4330d87dfc9
Christian Stein <sormuras@gmail.com>
2022-05-12 04:09:07 +0200

    Update launch scripts

--- .bach/bin/bach.bat
+++ .bach/bin/bach.bat
@@ -1,16 +1,5 @@
 @ECHO OFF
 
-IF "%~1" == "boot" (
-  REM SHIFT 1
-  jshell --module-path .bach\bin --add-modules ALL-MODULE-PATH,ALL-SYSTEM %~dp0\bach.jshell %2 %3 %4 %5 %6 %7 %8 %9
-  EXIT /B
-)
-
-IF "%~1" == "clean" (
-  IF EXIST .bach\out rmdir /S /Q .bach\out
-  EXIT /B
-)
-
 IF "%~1" == "init" (
   IF "%~2" == "" (
     ECHO "Usage: bach init VERSION"
@@ -20,7 +9,22 @@ IF "%~1" == "init" (
   EXIT /B
 )
 
-SET JAVA_LAUNCHER_ARGUMENTS=--module-path .bach\bin --add-modules=ALL-MODULE-PATH --add-modules ALL-DEFAULT
+IF NOT EXIST ".bach\bin\*.jar" (
+  IF NOT EXIST ".bach\bach-init.version" (
+    ECHO "Usage: bach init VERSION"
+    EXIT /B 1
+  )
+  SET /p VERSION=<.bach\bach-init.version
+  jshell -R-Dbach-version=%VERSION% https://git.io/bach-init
+)
+
+IF "%~1" == "boot" (
+  REM SHIFT 1
+  jshell --module-path .bach\bin --add-modules ALL-MODULE-PATH,ALL-SYSTEM .bach\bin\bach.jshell %2 %3 %4 %5 %6 %7 %8 %9
+  EXIT /B
+)
+
+SET JAVA_LAUNCHER_ARGUMENTS=--module-path=.bach\bin --add-modules=ALL-MODULE-PATH --add-modules=ALL-DEFAULT
 
 IF EXIST ".bach\src\%~1.java" (
   REM PROGRAM=
