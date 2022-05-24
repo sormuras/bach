2ba4d5cb951c9fb1e8b629618e896e49846c521f
Christian Stein <sormuras@gmail.com>
2022-05-24 13:10:48 +0200

    Clean up folder API

--- com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/project/Folder.java
+++ com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/project/Folder.java
@@ -9,26 +9,40 @@ import java.util.regex.Pattern;
  *
  * @param path the directory
  * @param release the Java feature release version to target, 0 for none
- * @param types the set of types associated with the directory
+ * @param type the type associated with the directory
  */
-public record Folder(Path path, int release, FolderTypes types) implements Comparable<Folder> {
+public record Folder(Path path, int release, FolderType type) implements Comparable<Folder> {
   static final Pattern RELEASE_PATTERN = Pattern.compile(".*?(\\d+)$");
 
-  static int parseReleaseNumber(String string) {
-    var matcher = RELEASE_PATTERN.matcher(string);
+  static int parseReleaseNumber(String name) {
+    var matcher = RELEASE_PATTERN.matcher(name);
     return matcher.matches() ? Integer.parseInt(matcher.group(1)) : 0;
   }
 
-  public static Folder of(Path path, FolderType... folderTypes) {
+  static FolderType parseFolderType(String name) {
+    if (name.startsWith("resources")) return FolderType.RESOURCES;
+    return FolderType.SOURCES; // includes "java[-...]" names
+  }
+
+  public static Folder of(Path path) {
     if (Files.notExists(path)) throw new IllegalArgumentException("No such path: " + path);
     if (Files.isRegularFile(path)) throw new IllegalArgumentException("Not a directory: " + path);
-    var release = parseReleaseNumber(path.getFileName().toString());
-    var types = FolderTypes.of(folderTypes);
-    return new Folder(path, release, types);
+    var name = path.getFileName().toString();
+    var release = parseReleaseNumber(name);
+    var type = parseFolderType(name);
+    return new Folder(path, release, type);
   }
 
   @Override
   public int compareTo(Folder other) {
     return release - other.release;
+  }
+
+  public boolean isSources() {
+    return type == FolderType.SOURCES;
+  }
+
+  public boolean isResources() {
+    return type == FolderType.RESOURCES;
   }
 }
--- com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/project/Folders.java
+++ com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/project/Folders.java
@@ -9,23 +9,28 @@ import java.util.List;
  * version.
  */
 public record Folders(List<Folder> list) {
+
   public static Folders of(Path info) {
     var parent = info.getParent();
     if (parent == null) return new Folders(List.of());
+
     if (!parent.getFileName().toString().startsWith("java"))
       return new Folders(List.of(Folder.of(parent)));
-    var javas = parent.getParent();
-    if (javas == null) return new Folders(List.of(Folder.of(parent)));
-    try (var stream = Files.list(javas)) {
-      return new Folders(
-          stream
-              .filter(Files::isDirectory)
-              .filter(path -> path.getFileName().toString().startsWith("java"))
-              .map(path -> Folder.of(path, FolderType.SOURCES))
-              .sorted()
-              .toList());
+
+    var grandpa = parent.getParent();
+    if (grandpa == null) return new Folders(List.of(Folder.of(parent)));
+
+    try (var stream = Files.list(grandpa)) {
+      var folders = stream.filter(Folders::isSourcesOrResourcesDirectory).map(Folder::of);
+      return new Folders(folders.sorted().toList());
     } catch (Exception exception) {
       throw new RuntimeException("Listing entries of %s failed".formatted(info));
     }
+  }
+
+  static boolean isSourcesOrResourcesDirectory(Path path) {
+    if (!Files.isDirectory(path)) return false;
+    var name = path.getFileName().toString();
+    return name.startsWith("java") || name.startsWith("resources");
   }
 }
