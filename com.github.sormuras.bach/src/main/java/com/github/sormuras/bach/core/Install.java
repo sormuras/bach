c1462c7a3a134a823445bd3c0c3705289960e851
Christian Stein <sormuras@gmail.com>
2022-07-04 07:20:04 +0200

    Support custom storages for external properties

--- com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/core/Install.java
+++ com.github.sormuras.bach/src/main/java/com/github/sormuras/bach/core/Install.java
@@ -1,6 +1,7 @@
 package com.github.sormuras.bach.core;
 
 import com.github.sormuras.bach.Bach;
+import com.github.sormuras.bach.ExternalPropertiesStorage;
 import com.github.sormuras.bach.ToolCall;
 import com.github.sormuras.bach.ToolOperator;
 import com.github.sormuras.bach.internal.ArgVester;
@@ -12,19 +13,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.StringJoiner;
 
 public class Install implements ToolOperator {
-  public record CLI(
-      String tool, Optional<String> uri, Optional<String> user, Optional<String> repo) {}
+  public record CLI(String tool, Optional<String> from) {}
 
   @Override
   public int run(Bach bach, PrintWriter out, PrintWriter err, String... args) {
     var cli = ArgVester.create(CLI.class).parse(args);
-    var target = bach.configuration().paths().externalTools(cli.tool() + ".properties");
+    var tool = cli.tool();
+
+    if (tool.isEmpty() || tool.equals("?")) {
+      var storage = ExternalPropertiesStorage.find(cli.from().orElse(""));
+      if (bach.configuration().isVerbose()) out.println("Using storage: " + storage);
+      var list = storage.map().get("external-tools");
+      if (list == null) return 0;
+      list.forEach(file -> out.println(computeCommand(cli, file)));
+      return 0;
+    }
+
+    var target = bach.configuration().paths().externalTools(tool + ".properties");
 
     if (Files.notExists(target)) {
-      var uri = cli.uri().orElse(computeUri(cli));
-      bach.run("load", target, uri);
+      bach.run("load", target, computeUri(cli));
     }
 
     generateLoadAndVerifyCalls(target).forEach(bach::run);
@@ -32,16 +43,21 @@ public class Install implements ToolOperator {
     return 0;
   }
 
+  String computeCommand(CLI cli, String file) {
+    var command = new StringJoiner(" ");
+    command.add("bach");
+    command.add(name()); // "install"
+    command.add(file);
+    cli.from().ifPresent(storage -> command.add("--from").add(storage));
+    return command.toString();
+  }
+
   String computeUri(CLI cli) {
-    return "https://github.com/"
-        + cli.user().orElse("sormuras")
-        + "/"
-        + cli.repo().orElse("bach-external-tools")
-        + "/raw/main/properties/"
-        + cli.tool().split("@")[0]
-        + "/"
-        + cli.tool()
-        + ".properties";
+    var from = cli.from().orElse("");
+    if (from.startsWith("http")) return from;
+    var storage =
+        from.isEmpty() ? ExternalPropertiesStorage.DEFAULT : ExternalPropertiesStorage.find(from);
+    return storage.uri("external-tools", cli.tool());
   }
 
   List<ToolCall> generateLoadAndVerifyCalls(Path path) {
@@ -55,6 +71,7 @@ public class Install implements ToolOperator {
     }
     var calls = new ArrayList<ToolCall>();
     for (var key : properties.stringPropertyNames()) {
+      if (key.startsWith("@")) continue;
       var source = properties.getProperty(key);
       var target = tool.resolve(key);
       calls.add(ToolCall.of("load-and-verify", target, source));
