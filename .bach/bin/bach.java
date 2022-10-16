e22f070b8055c8c41b7e99bcc39639e941d97b04
Christian Stein <sormuras@gmail.com>
2022-10-16 08:47:27 +0200

    Acquire Bach sources on-the-fly

--- .bach/bin/bach.java
+++ .bach/bin/bach.java
@@ -1,5 +1,9 @@
+import java.net.URL;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
 import java.util.spi.ToolProvider;
@@ -12,9 +16,14 @@ interface bach {
     if (code != 0) System.exit(code);
   }
 
-  static Path acquireBachSources() {
+  Path GIT_IGNORE_FILE = Path.of(".bach", ".gitignore");
+  Path BACH_VERSION_FILE = Path.of(".bach", "bach.version");
+
+  static Path acquireBachSources() throws Exception {
     var sources = Path.of(".bach/src");
-    if (!Files.isDirectory(sources)) throw new RuntimeException("No sources found: " + sources);
+    if (Files.isDirectory(sources.resolve("run.bach"))) return sources;
+    var version = System.getProperty("bach.version", readBachVersionFromFileOrElseReturnMain());
+    new Sources(version).acquire();
     return sources;
   }
 
@@ -45,9 +54,67 @@ interface bach {
     }
   }
 
+  static String readBachVersionFromFileOrElseReturnMain() throws Exception {
+    return Files.exists(BACH_VERSION_FILE) ? Files.readString(BACH_VERSION_FILE) : "main";
+  }
+
   static void run(String name, String... args) {
     var tool = ToolProvider.findFirst(name).orElseThrow();
     var code = tool.run(System.out, System.err, args);
     if (code != 0) throw new RuntimeException(name + " -> " + code);
+  }
+
+  record Sources(String version) {
+    void acquire() throws Exception {
+      var zip = Path.of(".bach/tmp/bach-archive-" + version + ".zip");
+      if (Files.notExists(zip)) {
+        Files.createDirectories(zip.getParent());
+        load("https://github.com/sormuras/bach/archive/" + version + ".zip", zip);
+      }
+      extract(zip);
+
+      //noinspection ResultOfMethodCallIgnored
+      Path.of(".bach/bin/bach").toFile().setExecutable(true, true);
+
+      if (Files.notExists(GIT_IGNORE_FILE)) Files.writeString(GIT_IGNORE_FILE, generateGitIgnore());
+      Files.writeString(BACH_VERSION_FILE, version);
+    }
+
+    String generateGitIgnore() {
+      return """
+           out/
+           *.jar
+           """;
+    }
+
+    void load(String uri, Path file) throws Exception {
+      System.out.printf("<< %s%n", uri);
+      try (var stream = new URL(uri).openStream()) {
+        var size = Files.copy(stream, file);
+        System.out.printf(">> %,7d %s%n", size, file.getFileName());
+      }
+    }
+
+    void extract(Path zip) throws Exception {
+      System.out.printf("<< %s%n", zip.toUri());
+      var files = new ArrayList<Path>();
+      try (var fs = FileSystems.newFileSystem(zip)) {
+        for (var root : fs.getRootDirectories()) {
+          try (var stream = Files.walk(root)) {
+            var list = stream.filter(Files::isRegularFile).toList();
+            for (var file : list) {
+              var string = file.toString();
+              if (string.contains(".bach/bin") || string.contains(".bach/src/run.bach")) {
+                var target = Path.of(file.subpath(1, file.getNameCount()).toString());
+                Files.createDirectories(target.getParent());
+                Files.copy(file, target, StandardCopyOption.REPLACE_EXISTING);
+                files.add(target);
+              }
+            }
+          }
+        }
+      }
+      System.out.printf(">> %d files extracted%n", files.size());
+    }
   }
 }
