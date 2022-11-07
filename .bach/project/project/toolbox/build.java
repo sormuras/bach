4ff31fb71f7019121241a346a55ba59733ff0906
Christian Stein <sormuras@gmail.com>
2022-11-07 12:20:33 +0100

    Always write logbooks on Bach's own build

--- .bach/project/project/toolbox/build.java
+++ .bach/project/project/toolbox/build.java
@@ -4,8 +4,13 @@ import run.bach.ToolOperator;
 
 public final class build implements ToolOperator {
   @Override
-  public void run(Operation operation) {
-    operation.run("project/format");
-    operation.run("run.bach/build");
+  public void run(Operation operation) throws Exception {
+    try {
+      operation.run("project/format");
+      operation.run("run.bach/build");
+    } catch (Exception exception) {
+      operation.bach().writeLogbook();
+      throw exception;
+    }
   }
 }
