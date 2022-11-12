ea1e9b0a8c5e325be8eec2d98b9c2e62d3813c5c
Christian Stein <sormuras@gmail.com>
2022-11-12 10:05:50 +0100

    Find better position for additional arguments

--- .bach/project/project/tweak/CallWithUtf8EncodingTweak.java
+++ .bach/project/project/tweak/CallWithUtf8EncodingTweak.java
@@ -7,7 +7,9 @@ public class CallWithUtf8EncodingTweak implements ToolTweak {
   @Override
   public ToolCall tweak(ToolCall call) {
     return switch (call.name()) {
-      case "javac", "jdk.compiler/javac" -> call.with(0, javac -> javac.with("-encoding", "UTF-8"));
+      case "javac", "jdk.compiler/javac" -> call.with(
+          call.arguments().indexOf("-d"), // insert at position of `-d`, pushing it to the right
+          javac -> javac.with("-encoding", "UTF-8")); // additional arguments to insert
       default -> call;
     };
   }
