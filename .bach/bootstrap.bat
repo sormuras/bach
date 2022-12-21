dbff5560ac97d1ee68f9c0adb1fabd22ca1aadf2
Christian Stein <sormuras@gmail.com>
2022-12-21 07:46:28 +0100

    Add bootstrap script

---
+++ .bach/bootstrap.bat
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal
+
+for %%i in ("%~dp0..") do set BACH_HOME=%%~fi
+set BIN=%BACH_HOME%\bin
+set SRC=%BACH_HOME%\src
+
+echo ^| BAT = %~dpnx0
+echo ^| BIN = %BIN%
+echo ^| SRC = %SRC%
+
+if "%JAVA_HOME%"=="" (
+  echo JAVA_HOME not set, trying via PATH
+  set JAVAC="javac.exe"
+) else (
+  set JAVAC="%JAVA_HOME%\bin\javac.exe"
+)
+
+@echo on
+%JAVAC% --release 17 --module run.bach,run.duke --module-version 2022-ea+src --module-source-path %SRC%\*\main\java -d %BIN%
+@echo off
+
+endlocal
+
+exit /B
