8db9237f847310ebbf5e98aedd0eb32abd97f8e3
Christian Stein <sormuras@gmail.com>
2022-12-22 16:47:33 +0100

    Try out JUnit Pioneer 2
    https://github.com/junit-pioneer/junit-pioneer/discussions/701

--- src/test.junit/test/java/module-info.java
+++ src/test.junit/test/java/module-info.java
@@ -1,4 +1,5 @@
 open /* for testing */ module test.junit {
   requires org.junit.jupiter; // for writing tests
+  requires org.junitpioneer; // for writing more tests
   requires static org.junit.platform.console; // for running tests
 }
