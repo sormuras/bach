c0ee9ed6ae67e84e85b4b3ff949e1ba95cd8ac44
Christian Stein <sormuras@gmail.com>
2024-03-21 07:52:59 +0100

    Apply Universal Permissive License https://opensource.org/license/upl

--- Tool.java
+++ Tool.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2024 Christian Stein
+ * Licensed under the Universal Permissive License v 1.0 -> https://opensource.org/license/upl
+ */
 package run.bach;
 
 import java.nio.file.Path;
--- package-info.java
+++ package-info.java
@@ -1,2 +1,7 @@
+/*
+ * Copyright (c) 2024 Christian Stein
+ * Licensed under the Universal Permissive License v 1.0 -> https://opensource.org/license/upl
+ */
+
 /** Provides classes that are fundamental to Bach. */
 package run.bach;
