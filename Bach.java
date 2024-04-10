a56a4ae06ea5d6add3cffd79cfac967a870aaea6
Christian Stein <sormuras@gmail.com>
2024-04-10 22:11:31 +0200

    Transmute `Bach` into a record

--- Bach.java
+++ Bach.java
@@ -6,13 +6,15 @@
 package run.bach;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
 import java.util.List;
 import java.util.Map;
 import run.bach.internal.ModuleSourcePathSupport;
 
 /** Defines Bach's constants and helpers. */
-public final class Bach {
+public record Bach(Folders folders) {
   /**
    * Well-known directories and files.
    *
@@ -21,8 +23,32 @@ public final class Bach {
    * @param var used to store cacheable file into, defaults to {@code ${root}.bach/var}
    */
   public record Folders(Path root, Path out, Path var) {
+    /** {@code .bach} */
     public static Folders ofCurrentWorkingDirectory() {
       return Folders.of(Path.of(""));
+    }
+
+    /** {@code C:\Users\${user.name}\.bach} */
+    public static Folders ofUserHomeDirectory() {
+      return Folders.of(Path.of(System.getProperty("user.home", "")));
+    }
+
+    /** {@code C:\Users\${user.name}\AppData\Local\Temp\.bach} */
+    public static Folders ofTemporaryDirectory() {
+      return Folders.of(Path.of(System.getProperty("java.io.tmpdir", "")));
+    }
+
+    /**
+     * {@code C:\Users\${user.name}\AppData\Local\Temp\${prefix}17762885918332141894\.bach}
+     *
+     * @see Files#createTempDirectory(String, FileAttribute[])
+     */
+    public static Folders ofTemporaryDirectory(String prefix) {
+      try {
+        return Folders.of(Files.createTempDirectory(prefix));
+      } catch (Exception exception) {
+        throw new RuntimeException(exception);
+      }
     }
 
     public static Folders of(Path root) {
@@ -33,15 +59,23 @@ public final class Bach {
     }
 
     public Path root(String first, String... more) {
-      return root.resolve(Path.of(first, more));
+      return root.resolve(first, more);
     }
 
     public Path out(String first, String... more) {
-      return out.resolve(Path.of(first, more));
+      return out.resolve(first, more);
     }
 
     public Path var(String first, String... more) {
-      return var.resolve(Path.of(first, more));
+      return var.resolve(first, more);
+    }
+
+    public Path tools() {
+      return var("cache", "tools");
+    }
+
+    public Path tools(String first, String... more) {
+      return tools().resolve(first, more);
     }
 
     public String computeModuleSourcePath(String module, String info) {
@@ -56,6 +90,4 @@ public final class Bach {
       return String.join(File.pathSeparator, ModuleSourcePathSupport.compute(modules, false));
     }
   }
-
-  private Bach() {}
 }
