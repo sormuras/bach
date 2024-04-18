c679436289dd2bbbb6ef1bd6d72727521278d447
Christian Stein <sormuras@gmail.com>
2024-04-18 16:52:50 +0200

    Update Greeter installer program

--- .bach/src/run.bach/run/Greeter.java
+++ .bach/src/run.bach/run/Greeter.java
@@ -9,33 +9,33 @@ import run.bach.ToolInstaller;
 import run.bach.ToolProgram;
 
 public record Greeter(String version) implements ToolInstaller {
-  public static void main(String... args) {
+  public static void main(String... args) throws Exception {
     var greeter = new Greeter("99");
-    greeter.install().run(); // "run.bach/greeter"
+    System.out.println("[1]");
+    greeter.install().run(); // "run.bach/greeter@99"
 
-    var folders = Bach.Folders.ofCurrentWorkingDirectory();
-    Tool.of("greeter/eager", greeter.installInto(folders.tools("greeter/eager"))).run();
-    Tool.of("greeter/inert", () -> greeter.installInto(folders.tools("greeter/inert"))).run();
+    var folders = Bach.Folders.ofTemporaryDirectory();
+    System.out.println("\n[2]");
+    Tool.of("greeter/eager", greeter.install(folders.tools("greeter/eager"))).run();
+    System.out.println("\n[3]");
+    Tool.of("greeter/inert", () -> greeter.install(folders.tools("greeter/inert"))).run();
   }
 
   @Override
-  public ToolProvider installInto(Path folder) {
-    var file = folder.resolve("Prog.java");
+  public ToolProvider install(Path into) throws Exception {
+    System.out.println(into.toUri());
+    var file = into.resolve("Prog.java");
     if (!Files.exists(file)) {
-      try {
-        Files.createDirectories(folder);
-        var text = // language=java
-            """
-            class Prog {
-              public static void main(String... args) {
-                System.out.println("Greetings!");
-              }
+      Files.createDirectories(into);
+      var text = // language=java
+          """
+          class Prog {
+            public static void main(String... args) {
+              System.out.println("Greetings!");
             }
-            """;
-        Files.writeString(file, text);
-      } catch (Exception exception) {
-        throw new RuntimeException();
-      }
+          }
+          """;
+      Files.writeString(file, text);
     }
     return ToolProgram.java(file.toString());
   }
