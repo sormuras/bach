c194d56b84cc6bdc3c8ee0d7647b0c5c7ca8d196
Christian Stein <sormuras@gmail.com>
2024-04-19 16:48:30 +0200

    Streamline tester action

--- workflow/Tester.java
+++ workflow/Tester.java
@@ -5,9 +5,12 @@
 
 package run.bach.workflow;
 
+import java.lang.annotation.Annotation;
 import java.lang.module.ModuleFinder;
+import java.nio.file.Path;
 import java.util.ServiceLoader;
 import java.util.spi.ToolProvider;
+import java.util.stream.Stream;
 import run.bach.Tool;
 import run.bach.ToolCall;
 import run.bach.internal.ModulesSupport;
@@ -15,75 +18,78 @@ import run.bach.workflow.Structure.Space;
 
 public interface Tester extends Action {
   default void test() {
+    var name = testerUsesSpaceName();
     var spaces = workflow().structure().spaces();
-    if (!spaces.names().contains("test")) return;
-    var space = spaces.space("test");
-    var tester = new SpaceTester(workflow(), space);
-    tester.testWithSpaceLauncher();
-    tester.testWithJUnitPlatform();
+    if (!spaces.names().contains(name)) return;
+    var space = spaces.space(name);
+    test(space);
   }
 
-  class SpaceTester {
-    final Workflow workflow;
-    final Space space;
+  default void test(Space space) {
+    testWithSpaceLauncher(space);
+    testWithJUnitPlatform(space);
+  }
 
-    public SpaceTester(Workflow workflow, Space space) {
-      this.workflow = workflow;
-      this.space = space;
-    }
+  default String testerUsesSpaceName() {
+    return "test";
+  }
 
-    void testWithSpaceLauncher() {
-      for (var launcher : space.launchers()) testWithSpaceLauncher(launcher);
-    }
+  default boolean testerShouldTestModuleWithJUnitPlatform(Module module) {
+    var annotations =
+        Stream.of(module.getAnnotations())
+            .map(Annotation::annotationType)
+            .map(Class::getTypeName)
+            .toList();
+    return annotations.contains("org.junit.platform.commons.annotation.Testable");
+  }
 
-    void testWithSpaceLauncher(String launcher) {
-      var folders = workflow.folders();
-      var java =
-          ToolCall.of("java")
-              .add("--module-path", space.toRuntimeSpace().toModulePath(folders).orElse("."))
-              .add("--module", launcher);
-      workflow.runner().run(java);
-    }
+  private void testWithSpaceLauncher(Space space) {
+    for (var launcher : space.launchers()) testWithSpaceLauncher(space, launcher);
+  }
 
-    void testWithJUnitPlatform() {
-      if (workflow.runner().findTool("junit").isEmpty()) return;
-      for (var module : space.modules()) {
-        testWithJUnitPlatform(module.name());
-      }
-    }
+  private void testWithSpaceLauncher(Space space, String launcher) {
+    var folders = workflow().folders();
+    var java =
+        ToolCall.of("java")
+            .add("-ea") // enable assertions
+            .add("--module-path", space.toRuntimeSpace().toModulePath(folders).orElse("."))
+            .add("--module", launcher);
+    workflow().runner().run(java);
+  }
 
-    void testWithJUnitPlatform(String name) {
-      var folders = workflow.folders();
-      var finder =
-          ModuleFinder.of(
-              folders.out("test", "modules", name + ".jar"),
-              folders.out("main", "modules"),
-              folders.out("test", "modules"),
-              folders.root("lib"));
-      var layer = ModulesSupport.buildModuleLayer(finder, name);
-      var module = layer.findModule(name).orElseThrow(AssertionError::new);
-      /*
-      var annotations =
-          Stream.of(module.getAnnotations())
-              .map(Annotation::annotationType)
-              .map(Class::getTypeName)
-              .toList();
-      if (!annotations.contains("org.junit.platform.commons.annotation.Testable")) return;
-      */
-      module.getClassLoader().setDefaultAssertionStatus(true);
-      var junit =
-          ServiceLoader.load(layer, ToolProvider.class).stream()
-              .map(ServiceLoader.Provider::get)
-              .filter(provider -> provider.name().equals("junit"))
-              .findFirst()
-              .orElseThrow();
-      workflow
-          .runner()
-          .run(
-              Tool.of(junit),
-              args ->
-                  args.add("--select-module", name)
-                      .add("--reports-dir", folders.out("test-reports", "junit-" + name)));
+  private void testWithJUnitPlatform(Space space) {
+    if (workflow().runner().findTool("junit").isEmpty()) return;
+    for (var module : space.modules()) {
+      testWithJUnitPlatform(space, module.name());
     }
+  }
+
+  private void testWithJUnitPlatform(Space space, String name) {
+    var folders = workflow().folders();
+    var finder =
+        ModuleFinder.compose(
+            ModuleFinder.of(folders.out(space.name(), "modules", name + ".jar")),
+            ModuleFinder.of(
+                space.requires().stream()
+                    .map(required -> folders.out(required, "modules"))
+                    .toArray(Path[]::new)),
+            ModuleFinder.of(folders.out(space.name(), "modules")),
+            ModuleFinder.of(folders.root("lib")));
+    var layer = ModulesSupport.buildModuleLayer(finder, name);
+    var module = layer.findModule(name).orElseThrow(AssertionError::new);
+    if (!testerShouldTestModuleWithJUnitPlatform(module)) return;
+    module.getClassLoader().setDefaultAssertionStatus(true);
+    var junit =
+        Tool.of(
+            ServiceLoader.load(layer, ToolProvider.class).stream()
+                .map(ServiceLoader.Provider::get)
+                .filter(provider -> provider.name().equals("junit"))
+                .findFirst()
+                .orElseThrow());
+    var call =
+        ToolCall.of(junit)
+            .add("--select-module", name)
+            .add("--reports-dir", folders.out("test-reports", "junit-" + name));
+    workflow().runner().run(call);
   }
 }
