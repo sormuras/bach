2e624c878d71c31760821c36ee0118df6cbfe7b1
Christian Stein <sormuras@gmail.com>
2024-04-19 18:54:45 +0200

    Fine-tune class compilation

--- .bach/src/run.bach/run/Build.java
+++ .bach/src/run.bach/run/Build.java
@@ -3,25 +3,5 @@ package run;
 class Build {
   public static void main(String... args) {
     Project.ofCurrentWorkingDirectory().build();
-    var string =
-        """
-        var classes = folders.out("classes");
-        var modules = folders.out("modules");
-        var moduleSourcePath = folders.computeModuleSourcePath("run.bach", ".bach/src/run.bach");
-
-        ToolCall.of("javac")
-            .add("--module", "run.bach")
-            .add("--module-source-path", moduleSourcePath)
-            .add("-X" + "lint:all")
-            .add("-W" + "error")
-            .add("-d", classes)
-            .run();
-
-        ToolCall.of("jar", "--create")
-            .when(Boolean.getBoolean("-Debug".substring(2)), "--verbose")
-            .add("--file", modules.resolve("run.bach.jar"))
-            .add("-C", classes.resolve("run.bach"), ".")
-            .run();
-        """;
   }
 }
--- .bach/src/run.bach/run/Project.java
+++ .bach/src/run.bach/run/Project.java
@@ -1,30 +1,34 @@
 package run;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import run.bach.Bach;
+import run.bach.ToolCall;
 import run.bach.ToolRunner;
 import run.bach.workflow.Builder;
 import run.bach.workflow.Structure;
+import run.bach.workflow.Structure.Space;
 import run.bach.workflow.Tester;
 import run.bach.workflow.Workflow;
 
-public record Project(Workflow workflow) implements Builder, Tester {
+public record Project(boolean verbose, Workflow workflow) implements Builder, Tester {
   static Project ofCurrentWorkingDirectory() {
     return new Project(
+        Boolean.getBoolean("-Debug".substring(2)),
         new Workflow(
             Bach.Folders.ofCurrentWorkingDirectory(),
             new Structure(
                 new Structure.Basics("Bach", "2024-ea"),
                 new Structure.Spaces(
-                    new Structure.Space(
+                    new Space(
                         "main",
                         22,
                         "run.bach/run.bach.Main",
                         new Structure.DeclaredModule(
                             Path.of(".bach/src/run.bach"),
                             Path.of(".bach/src/run.bach/module-info.java"))), // Space "main
-                    new Structure.Space(
+                    new Space(
                         "test",
                         List.of("main"),
                         0,
@@ -38,5 +42,29 @@ public record Project(Workflow workflow) implements Builder, Tester {
                 ),
             ToolRunner.ofSystem()) // Workflow
         ); // Project
+  }
+
+  @Override
+  public void compileClasses(Space space) {
+    Builder.super.compileClasses(space);
+    // Delete all local programs in out/main/classes/*/run.bach/run/* directory
+    if (space.name().equals("main")) {
+      var dir = classesCompilerUsesDestinationDirectory(space).resolve("run.bach", "run");
+      try (var stream = Files.newDirectoryStream(dir, Files::isRegularFile)) {
+        for (var file : stream) Files.deleteIfExists(file);
+      } catch (Exception exception) {
+        throw new RuntimeException(exception);
+      }
+    }
+  }
+
+  @Override
+  public ToolCall compileClassesWithFinalTouch(ToolCall javac, Space space) {
+    return javac.add("-X" + "lint:all").add("-W" + "error");
+  }
+
+  @Override
+  public ToolCall packageClassesCreateJarCall() {
+    return Builder.super.packageClassesCreateJarCall().when(verbose, "--verbose");
   }
 }
