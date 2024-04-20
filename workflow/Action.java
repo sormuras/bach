6dadf61048e2afca82d4c20756476fcb7d9f1695
Christian Stein <sormuras@gmail.com>
2024-04-20 06:46:10 +0200

    Support simple action logging

--- workflow/Action.java
+++ workflow/Action.java
@@ -5,6 +5,19 @@
 
 package run.bach.workflow;
 
+import java.lang.System.Logger.Level;
+
 public interface Action {
   Workflow workflow();
+
+  default void log(String message) {
+    log(Level.DEBUG, message);
+  }
+
+  default void log(Level level, String message) {
+    var severity = level.getSeverity();
+    if (severity <= Level.DEBUG.getSeverity()) return;
+    var stream = severity < Level.ERROR.getSeverity() ? System.out : System.err;
+    stream.println(message);
+  }
 }
