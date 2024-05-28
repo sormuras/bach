942ac7863228910a39bd51fb3f03135b92777019
Christian Stein <sormuras@gmail.com>
2024-05-28 12:41:09 +0200

    Add `jreleaser` submodule

--- .bach/src/run.bach/run/demo/ModuleResolverDemo.java
+++ .bach/src/run.bach/run/demo/ModuleResolverDemo.java
@@ -2,20 +2,18 @@ package run.demo;
 
 import java.lang.module.ModuleFinder;
 import java.nio.file.Path;
-
 import jdk.jfr.consumer.RecordingStream;
 import run.bach.ModuleLocator;
 import run.bach.ModuleResolver;
 import run.bach.ToolFinder;
+import run.bach.workflow.Folders;
 import run.info.bach.JavaFX;
+import run.info.org.jreleaser.JReleaser;
 import run.info.org.junit.JUnit;
 
 public class ModuleResolverDemo {
   public static void main(String... args) throws Exception {
-    var libraries = ModuleLocator.compose(
-            JUnit.modules(),
-            JavaFX.version("22.0.1")
-    );
+    var libraries = ModuleLocator.compose(JUnit.modules(), JavaFX.version("22.0.1"));
 
     var lib = Path.of("lib");
     try (var recording = new RecordingStream()) {
@@ -28,34 +26,25 @@ public class ModuleResolverDemo {
       recording.stop();
     }
 
-    //    // "jreleaser" via the tool provider SPI
-    //    var jreleaserHome =
-    //        Folders.ofCurrentWorkingDirectory().tool(JReleaser.NAME + "@" + JReleaser.VERSION);
-    //    var jreleaserResolver = ModuleResolver.ofSingleDirectory(jreleaserHome,
-    // JReleaser.MODULES);
-    //    jreleaserResolver.resolveModule("org.jreleaser.tool");
-    //    jreleaserResolver.resolveMissingModules();
+    // "jreleaser" via the tool provider SPI
+    var jreleaserHome = Folders.ofCurrentWorkingDirectory().tool("jreleaser@" + JReleaser.VERSION);
+    var jreleaserResolver = ModuleResolver.ofSingleDirectory(jreleaserHome, JReleaser.MODULES);
+    jreleaserResolver.resolveModule("org.jreleaser.tool");
+    jreleaserResolver.resolveMissingModules();
 
     var tools =
         ToolFinder.compose(
             ToolFinder.of("jar"), // provides "jar" tool
             ToolFinder.of("java"), // provides "java" tool
             ToolFinder.of(ModuleFinder.of(lib)), // provides "junit" tool
-            //            ToolFinder.of(ModuleFinder.of(jreleaserHome)), // provides "jreleaser"
-            // tool
-            ToolFinder.ofInstaller()
-                //                .withJavaApplication("demo/release@uri", JReleaser.APPLICATION)
-                //                .withJavaApplication(
-                //                    "demo/release@all", JReleaser.APPLICATION,
-                // JReleaser.APPLICATION_ASSETS)
-            );
+            ToolFinder.of(ModuleFinder.of(jreleaserHome)), // provides "jreleaser" tool
+            ToolFinder.ofInstaller().withJavaApplication("demo/release@uri", JReleaser.URI));
 
     var junit = tools.get("junit");
     junit.run("--version");
     junit.run("engines");
 
-    //    tools.get("jreleaser").run("--version");
-    //    tools.get("releaser1").run("--version");
-    //    tools.get("releaser2").run("--version");
+    tools.get("jreleaser").run("--version");
+    tools.get("release@uri").run("--version");
   }
 }
