377e8b28fec1137c07be110c4508fe1a5417cc3b
Christian Stein <sormuras@gmail.com>
2025-10-31 19:10:03 +0100

    Bump various tool versions

--- .bach/src/run.bach/run/demo/ToolVersionsDemo.java
+++ .bach/src/run.bach/run/demo/ToolVersionsDemo.java
@@ -4,7 +4,7 @@ import run.bach.*;
 import run.info.bach.*;
 
 class ToolVersionsDemo {
-  public static void main(String... args) {
+  static void main() {
     // 1-shot, tool provider
     Tool.of("jar").run("--version");
 
@@ -12,31 +12,34 @@ class ToolVersionsDemo {
     Tool.of("java").run("--version");
 
     // 1-shot, tool installer
-    Tool.of("https://github.com/rife2/bld/releases/download/2.0.1/bld-2.0.1.jar").run("version");
+    Tool.of("https://github.com/rife2/bld/releases/download/2.3.0/bld-2.3.0.jar").run("version");
     Tool.of(new Ant(), ToolInstaller.Mode.INSTALL_IMMEDIATE).run("-version");
 
     // multi-shot, tool finder
     var finder =
         ToolFinder.ofInstaller(ToolInstaller.Mode.INSTALL_IMMEDIATE)
             .with(new Ant())
-            .withJavaApplication(JResolve.ID, JResolve.URI)
+            .with(JResolve.URI)
+            .with("https://github.com/rife2/bld/releases/download/2.3.0/bld-2.3.0.jar")
             .withJavaApplication(
-                "rife2/bld@2.0.1",
-                "https://github.com/rife2/bld/releases/download/2.0.1/bld-2.0.1.jar")
-            .withJavaApplication(
-                "org.junit.platform/junit@1.11.0",
-                "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-console-standalone/1.11.0/junit-platform-console-standalone-1.11.0.jar")
+                "maven/org.junit.platform/junit@6.0.1",
+                "https://repo.maven.apache.org/maven2/org/junit/platform/junit-platform-console-standalone/6.0.1/junit-platform-console-standalone-6.0.1.jar")
+            .with("run.bach/google-java-format@1.32", new GoogleJavaFormat("1.32.0"))
             .with("run.bach/google-java-format@1.23", new GoogleJavaFormat("1.23.0"))
             .with("run.bach/google-java-format@1.19", new GoogleJavaFormat("1.19.2"))
-            .with(new Maven("3.9.9"));
+            .withJavaApplication(
+                "maven/toolbox@0.14.3",
+                "https://repo.maven.apache.org/maven2/eu/maveniverse/maven/plugins/toolbox/0.14.3/toolbox-0.14.3-cli.jar")
+            .with(new Maven("3.9.11"));
 
     var runner = ToolRunner.of(finder);
     runner.run("ant", "-version");
     runner.run("bld", "version");
     runner.run("junit", "--version");
-    runner.run("jresolve", "--version");
+    runner.run("jresolve-cli", "--version");
     runner.run("google-java-format", "--version");
     runner.run("google-java-format@1.19", "--version");
+    runner.run("toolbox", "--version");
     runner.run("maven", "--version");
   }
 }
